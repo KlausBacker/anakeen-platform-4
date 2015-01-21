5b46c93f775b7d02b82805ba0ed9d6dfebeaf45c
Éric Brison <eric.brison@anakeen.com>
2015-01-21 14:38:25 +0100

    fix(test): Add css in karma

---
+++ karma/test-css.js
@@ -0,0 +1,11 @@
+define(function(require) {
+    "use strict";
+
+    require('jquery');
+
+
+    //Modify to suit your requirements
+    $('body').append('<link rel="stylesheet" href="/dynacase/css/dcp/document/bootstrap.css" />').
+        append('<link rel="stylesheet" href="/dynacase/css/dcp/document/kendo.css" />');
+
+});
