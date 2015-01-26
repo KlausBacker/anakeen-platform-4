1284e4eaa46563fd427391eafd710b29bcd2a939
Charles Bonnissent <charles.bonnissent@anakeen.com>
2015-01-26 14:27:04 +0100

    refactor(autocomplete): handle error

--- DOCUMENT/action-autocomplete.php
+++ DOCUMENT/action-autocomplete.php
@@ -9,103 +9,115 @@ include_once ("FDL/enum_choice.php");
 use Dcp\HttpApi\V1\DocManager\DocManager;
 function autocomplete(Action & $action)
 {
-    
-    $usage = new ActionUsage($action);
-    $usage->setText("get submenu document");
-    
-    $documentId = $usage->addRequiredParameter("id", "document identifier");
-    $attrId = $usage->addRequiredParameter("attrid", "attribute identifier");
-    $fromid = $usage->addRequiredParameter("fromid", "family identifier");
-    $index = $usage->addOptionalParameter("index", -1);
-    $usage->setStrictMode(false);
-    $usage->verify();
-    $err = '';
-    if ($documentId) {
-        $doc = DocManager::getDocument($documentId);
-        $err = $doc->control("view");
-        if ($err) {
-            $action->exitError($err);
-        }
-    } else {
-        $doc = DocManager::getFamily($fromid);
-    }
-    
-    if (!$doc) {
-        $action->exitError(sprintf(___("Document \"%s\" not found ", "ddui") , $documentId));
-    }
-    
-    $oattr = $doc->getAttribute($attrId);
-    if (!$oattr) {
-        $action->exitError(sprintf(___("Attribute \"%s\" not found ", "ddui") , $attrId));
-    }
-    
-    if ($oattr->type == "docid" || $oattr->type == "account") {
-        /**
-         * @var NormalAttribute $oattr
-         */
-    }
-    $aname = $oattr->id;
-    $famid = $oattr->format;
-    
-    if (!$oattr->phpfile) {
-        // in coherence with editutil.php
-        $filter = array(); //no filter by default
-        $sfilter = '';
-        $idid = "initid"; //if there's no docrev option (or it's present but not fixed), use initid to have the latest.
-        $docrev = $oattr->getOption("docrev");
-        if ($docrev == "fixed") {
-            $idid = "id";
-        } elseif ($docrev != "latest") {
-            //if $docrev is neither fixed nor latest it should be state=...
-            //if not, we'll just ignore the option
-            $matches = array();
-            if (preg_match('/^state\(([a-zA-Z0-9_:-]+)\)/', $docrev, $matches)) {
-                $filter[] = "state='" . pg_escape_string($matches[1]) . "'";
+    $return = array(
+        "success" => true,
+        "error" => array(),
+        "data" => array()
+    );
+
+    try {
+
+        //Usage part
+        $usage = new ActionUsage($action);
+        $usage->setText("autocomplete result for ddui");
+        $documentId = $usage->addRequiredParameter("id", "document identifier");
+        $attrId = $usage->addRequiredParameter("attrid", "attribute identifier");
+        $fromid = $usage->addRequiredParameter("fromid", "family identifier");
+        $index = $usage->addOptionalParameter("index", -1);
+        $usage->setStrictMode(false);
+        $usage->verify(true);
+
+        $err = '';
+        if ($documentId !== "0") {
+            $doc = DocManager::getDocument($documentId);
+            $err = $doc->control("view");
+            if ($err) {
+                throw new Exception("Unable to view the document ".$documentId);
             }
+        } else {
+            $doc = DocManager::getFamily($fromid);
         }
-        //make $filter safe to pass in a string for getResPhpFunc.
-        if (count($filter) == 0) $sfilter = serialize($filter);
-        $oattr->phpfunc = "lfamily(D,'$famid',CT,0,$sfilter,'$idid):${aname},CT";
-        $oattr->phpfile = "fdl.php";
-    }
-    compatOriginalFormPost($action->getArgument("filter") , $action->getArgument("attributes") , $oattr->id);
-    //print_r($oattr->phpfunc);
-    $res = getResPhpFunc($doc, $oattr, $rargids, $tselect, $tval, true, $index);
-    if (!is_array($res)) {
-        if ($res == "") {
-            $res = sprintf(___("wrong return type when calling function %s\n%s", "ddui") , $oattr->phpfunc, $res);
+
+        if (!$doc) {
+            throw new Exception(sprintf(___("Document \"%s\" not found ", "ddui") , $documentId));
         }
-        $err = $res;
-    }
-    $info = array();
-    if (!$err) {
-        foreach ($res as $aResult) {
-            $title = array_shift($aResult);
-            $values = array();
-            foreach ($rargids as $k => $argName) {
-                if ($argName === "CT") {
-                    $values[$oattr->id]["displayValue"] = isset($aResult[$k]) ? $aResult[$k] : null;
-                } else {
-                    $values[strtolower($argName) ]["value"] = isset($aResult[$k]) ? $aResult[$k] : null;
+
+        $attributeObject = $doc->getAttribute($attrId);
+        if (!$attributeObject) {
+            throw new Exception(sprintf(___("Attribute \"%s\" not found ", "ddui") , $attrId));
+        }
+
+        /**
+         * @var NormalAttribute $attributeObject
+         */
+        $attributeName = $attributeObject->id;
+        $famid = $attributeObject->format;
+
+        if (!$attributeObject->phpfile) {
+            // in coherence with editutil.php
+            $filter = array(); //no filter by default
+            $serializedFilter = '';
+            $idType = "initid"; //if there's no docrev option (or it's present but not fixed), use initid to have the latest.
+            $docrev = $attributeObject->getOption("docrev");
+            if ($docrev == "fixed") {
+                $idType = "id";
+            } elseif ($docrev != "latest") {
+                //if $docrev is neither fixed nor latest it should be state=...
+                //if not, we'll just ignore the option
+                $matches = array();
+                if (preg_match('/^state\(([a-zA-Z0-9_:-]+)\)/', $docrev, $matches)) {
+                    $filter[] = "state='" . pg_escape_string($matches[1]) . "'";
                 }
             }
-            
-            $info[] = array(
-                "title" => $title,
-                "values" => $values
-            );
+            //make $filter safe to pass in a string for getResPhpFunc.
+            if (count($filter) == 0) {
+                $serializedFilter = serialize($filter);
+            }
+            $attributeObject->phpfunc = "lfamily(D,'$famid',CT,0,$serializedFilter,'$idType):${attributeName},CT";
+            $attributeObject->phpfile = "fdl.php";
         }
+        //BEWARE SET LOT OF HTTPVAR
+        //@todo rewrite this part to be less invasive
+        compatOriginalFormPost($action->getArgument("filter") , $action->getArgument("attributes") , $attributeObject->id);
+        //print_r($oattr->phpfunc);
+        $result = getResPhpFunc($doc, $attributeObject, $rargids, $tselect, $tval, true, $index);
+        if (!is_array($result)) {
+            if ($result == "") {
+                throw new Exception(sprintf(___("wrong return type when calling function %s\n%s", "ddui") , $attributeObject->phpfunc, $result));
+            }
+            $err = $result;
+        }
+        if (!$err) {
+            foreach ($result as $currentResult) {
+                $title = array_shift($currentResult);
+                $values = array();
+                foreach ($rargids as $key => $argName) {
+                    if ($argName === "CT") {
+                        $values[$attributeObject->id]["displayValue"] = isset($currentResult[$key]) ? $currentResult[$key] : null;
+                    } else {
+                        $values[strtolower($argName) ]["value"] = isset($currentResult[$key]) ? $currentResult[$key] : null;
+                    }
+                }
+
+                $return["data"][] = array(
+                    "title" => $title,
+                    "values" => $values
+                );
+            }
+        }
+
+        if (count($return["data"]) === 0) {
+            throw new Exception(___("No result found", "ddui"));
+        }
+    } catch (Exception $e) {
+        $return["success"] = false;
+        $return["error"][] = $e->getMessage();
+        unset($return["data"]);
     }
-    
-    if (count($info) === 0) {
-        $info[] = array(
-            "title" => "",
-            "error" => $err ? $err : ___("No result found", "ddui")
-        );
-    }
-    $action->lay->template = json_encode($info);
+
+    $action->lay->template = json_encode($return);
     $action->lay->noparse = true;
-    header('Content-Type: application/json');
+    header('Content-type: application/json');
 }
 
 function compatOriginalFormPost($filters, $attributes, $currentAid)
