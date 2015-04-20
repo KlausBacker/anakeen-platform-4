48876bbdd78462871dada3863423b44811505e14
Charles Bonnissent <charles.bonnissent@anakeen.com>
2015-04-20 14:41:04 +0200

    feat(test): add standard method test
    fetch, save, delete

--- DOCUMENT/IHM/test/suiteDocumentController.js
+++ DOCUMENT/IHM/test/suiteDocumentController.js
@@ -29,8 +29,19 @@ define([
 
         config = config || {};
 
-        describe("documentController : "+config.name, function ()
+        describe("documentController : " + config.name, function ()
         {
+
+            var getMockFunction = function getMockFunction()
+            {
+                var mock = {
+                    "fct": function mockFunction()
+                    {
+                    }
+                };
+                spyOn(mock, "fct");
+                return mock;
+            };
 
             beforeEach(function initSyncBeforeTest()
             {
@@ -42,6 +53,18 @@ define([
                     var documentIdentifier = model.id + model.get("viewId");
 
                     if (method === "read" && !_.isUndefined(values[documentIdentifier])) {
+                        currentValues = values[documentIdentifier];
+                        options.success(values[documentIdentifier]);
+                        return true;
+                    }
+
+                    if (method === "update" && !_.isUndefined(values[documentIdentifier])) {
+                        currentValues = values[documentIdentifier];
+                        options.success(values[documentIdentifier]);
+                        return true;
+                    }
+
+                    if (method === "delete" && !_.isUndefined(values[documentIdentifier])) {
                         currentValues = values[documentIdentifier];
                         options.success(values[documentIdentifier]);
                         return true;
@@ -78,14 +101,10 @@ define([
                     $sandox.documentController(prepareDocumentController(documentOptions));
                     $sandox.documentController("addEvent", "ready", {"once": true}, function ()
                     {
-                        var mock = {
-                            "fct": function ()
-                            {
-                            }
-                        };
-                        spyOn(mock, "fct");
+                        var mock = getMockFunction();
                         $sandox.documentController("addEvent", "ready", mock.fct);
                         $sandox.documentController("reinitDocument");
+                        //2 because auto launch ready
                         expect(mock.fct.calls.count()).toEqual(2);
                         done();
                     });
@@ -113,6 +132,134 @@ define([
 
             });
 
+            describe("fetch", function ()
+            {
+
+                it("arguments", function test_arguments(done)
+                {
+                    var $sandox = getSandbox();
+                    $sandox.documentController(prepareDocumentController(documentOptions));
+                    $sandox.documentController("addEvent", "ready", {"once" : true}, function ()
+                    {
+                        expect(function ()
+                        {
+                            $sandox.documentController("fetchDocument", null);
+                        }).toThrowError('Fetch argument must be an object {"initid":, "revision": , "viewId": }');
+                        done();
+                    });
+                });
+
+                it("without arguments", function test_without_arguments(done)
+                {
+                    var $sandox = getSandbox();
+                    $sandox.documentController(prepareDocumentController(documentOptions));
+                    $sandox.documentController("addEvent", "ready", {"once": true}, function ()
+                    {
+                        var mockReady = getMockFunction();
+                        $sandox.documentController("addEvent", "ready", mockReady.fct);
+                        $sandox.documentController("fetchDocument");
+                        //2 because auto launch ready
+                        expect(mockReady.fct.calls.count()).toEqual(2);
+                        done();
+                    });
+                });
+
+                it("events", function test_events(done)
+                {
+                    var $sandox = getSandbox();
+                    $sandox.documentController(prepareDocumentController(documentOptions));
+                    $sandox.documentController("addEvent", "ready", {"once": true}, function test_launchEvents()
+                    {
+                        var mockReady = getMockFunction(), mockBeforeClose = getMockFunction(), mockClose = getMockFunction(),
+                            mockSuccess = getMockFunction();
+                        $sandox.documentController("addEvent", "ready", mockReady.fct);
+                        $sandox.documentController("addEvent", "beforeClose", mockBeforeClose.fct);
+                        $sandox.documentController("addEvent", "close", mockClose.fct);
+                        $sandox.documentController("fetchDocument", {}, {success: mockSuccess.fct});
+                        //2 because auto launch ready
+                        expect(mockReady.fct.calls.count()).toEqual(2);
+                        expect(mockBeforeClose.fct.calls.count()).toEqual(1);
+                        expect(mockClose.fct.calls.count()).toEqual(1);
+                        expect(mockSuccess.fct.calls.count()).toEqual(1);
+                        done();
+                    });
+                });
+
+                it("change viewId", function (done)
+                {
+                    var $sandox = getSandbox(), documentOptions = prepareDocumentController(documentOptions);
+                    $sandox.documentController(documentOptions);
+                    $sandox.documentController("addEvent", "ready", {"once": true}, function ()
+                    {
+                        var mock = getMockFunction(), viewId = documentOptions.viewId === "!coreConsultation" ? "!defaultEdition" : "!coreConsultation";
+                        $sandox.documentController("addEvent", "ready", mock.fct);
+
+                        $sandox.documentController("fetchDocument", {"viewId" : viewId});
+                        //2 because auto launch ready
+                        expect(mock.fct.calls.count()).toEqual(2);
+                        expect($sandox.documentController("getProperty", "viewId"), viewId);
+                        done();
+                    });
+                });
+
+            });
+
+            describe("save", function ()
+            {
+
+                it("events", function test_events(done)
+                {
+                    var $sandox = getSandbox();
+                    $sandox.documentController(prepareDocumentController(documentOptions));
+                    $sandox.documentController("addEvent", "ready", {"once": true}, function test_launchEvents()
+                    {
+                        var mockReady = getMockFunction(), mockBeforeSave = getMockFunction(), mockClose = getMockFunction(),
+                            mockAfterSave = getMockFunction(), mockSuccess = getMockFunction();
+                        $sandox.documentController("addEvent", "ready", mockReady.fct);
+                        $sandox.documentController("addEvent", "beforeSave", mockBeforeSave.fct);
+                        $sandox.documentController("addEvent", "close", mockClose.fct);
+                        $sandox.documentController("addEvent", "afterSave", mockAfterSave.fct);
+                        $sandox.documentController("saveDocument", {success: mockSuccess.fct});
+                        //2 because auto launch ready
+                        expect(mockReady.fct.calls.count()).toEqual(2);
+                        expect(mockBeforeSave.fct.calls.count()).toEqual(1);
+                        expect(mockClose.fct.calls.count()).toEqual(1);
+                        expect(mockAfterSave.fct.calls.count()).toEqual(1);
+                        expect(mockSuccess.fct.calls.count()).toEqual(1);
+                        done();
+                    });
+                });
+
+            });
+
+            describe("delete", function ()
+            {
+
+                it("events", function test_events(done)
+                {
+                    var $sandox = getSandbox();
+                    $sandox.documentController(prepareDocumentController(documentOptions));
+                    $sandox.documentController("addEvent", "ready", {"once": true}, function test_launchEvents()
+                    {
+                        var mockReady = getMockFunction(), mockBeforeDelete = getMockFunction(), mockClose = getMockFunction(),
+                            mockAfterDelete = getMockFunction(), mockSuccess = getMockFunction();
+                        $sandox.documentController("addEvent", "ready", mockReady.fct);
+                        $sandox.documentController("addEvent", "beforeDelete", mockBeforeDelete.fct);
+                        $sandox.documentController("addEvent", "close", mockClose.fct);
+                        $sandox.documentController("addEvent", "afterDelete", mockAfterDelete.fct);
+                        $sandox.documentController("deleteDocument", {success: mockSuccess.fct});
+                        //2 because auto launch ready
+                        expect(mockReady.fct.calls.count()).toEqual(2);
+                        expect(mockBeforeDelete.fct.calls.count()).toEqual(1);
+                        expect(mockClose.fct.calls.count()).toEqual(1);
+                        expect(mockAfterDelete.fct.calls.count()).toEqual(1);
+                        expect(mockSuccess.fct.calls.count()).toEqual(1);
+                        done();
+                    });
+                });
+
+            });
+
             describe("value", function ()
             {
 
@@ -122,7 +269,8 @@ define([
                     $sandox.documentController(prepareDocumentController(config));
                     $sandox.documentController("addEvent", "ready", function ()
                     {
-                        _.each(currentValues.data.view.documentData.document.attributes, function(value, attrid) {
+                        _.each(currentValues.data.view.documentData.document.attributes, function (value, attrid)
+                        {
                             var currentValue;
                             currentValue = $sandox.documentController("getValue", attrid);
                             expect(currentValue).toEqual(value);
@@ -137,12 +285,22 @@ define([
                     $sandox.documentController(prepareDocumentController(config));
                     $sandox.documentController("addEvent", "ready", function ()
                     {
-                        _.each(currentValues.data.view.documentData.document.attributes, function (value, attrid)
-                        {
-                            var currentValue;
-                            currentValue = $sandox.documentController("getValue", attrid);
-                            expect(currentValue).toEqual(value);
-                        });
+                        $sandox.documentController("setValue", "zoo_date", {value: "12-05-1985"});
+                        expect($sandox.documentController("getValue", "zoo_date").value).toEqual("12-05-1985");
+                        done();
+                    });
+                });
+
+                it("changeEvent", function (done)
+                {
+                    var $sandox = getSandbox();
+                    $sandox.documentController(prepareDocumentController(config));
+                    var mock = getMockFunction();
+                    $sandox.documentController("addEvent", "change", mock.fct);
+                    $sandox.documentController("addEvent", "ready", function ()
+                    {
+                        $sandox.documentController("setValue", "zoo_date", {value: "12-05-1985"});
+                        expect(mock.fct.calls.count()).toEqual(1);
                         done();
                     });
                 });
