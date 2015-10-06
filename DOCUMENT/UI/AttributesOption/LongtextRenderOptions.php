41d7cb1ed874babfdb8c5bbc0cfc009aebc58e21
Éric Brison <eric.brison@anakeen.com>
2015-10-06 13:29:04 +0200

    fix(render): rename setDisplayedLineNumber to setMaxDisplayedLineNumber (refs #5778)

--- DOCUMENT/UI/AttributesOption/LongtextRenderOptions.php
+++ DOCUMENT/UI/AttributesOption/LongtextRenderOptions.php
@@ -14,12 +14,13 @@ class LongtextRenderOptions extends TextRenderOptions
     const displayedLineNumberOption = "displayedLineNumber";
     /**
      * Number of line which are displayed before a scroll bar appears
+     * The number of line text cannot up to $number
      * @note use only in edition mode
      * @param int $number ( 0 if no limit)
      * @throws Exception UI0204
      * @return $this
      */
-    public function setDisplayedLineNumber($number)
+    public function setMaxDisplayedLineNumber($number)
     {
         if (!is_int($number) || $number < 0) {
             throw new Exception("UI0204", $number);
