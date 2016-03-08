45954497bbcd5cf9e24f5c466fb89601ee701919
Charles Bonnissent <charles.bonnissent@anakeen.com>
2016-03-08 18:55:39 +0100

    feat(deps): add promise deps (refs #5746)
    Add promise packet deps
    Add promise polyfill

---
+++ Document-uis/DOCUMENT/IHM/libs/promise.js
@@ -0,0 +1,12 @@
+if (window.Promise) {
+    define([], function nativePromise() {
+        'use strict';
+        return window.Promise;
+    });
+} else {
+    define(["es6-promise"], function polifyllPromise(Promise) {
+        'use strict';
+        return Promise;
+    });
+}
+
