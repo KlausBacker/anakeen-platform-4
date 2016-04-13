7cca68d430297bd23756a08fd8bdc6cc6dec6bbb
Jérôme Augé <jerome.auge@anakeen.com>
2016-04-13 09:53:51 +0200

    refactor(license): Changed license to GNU Lesser General Public License v3.0 (refs #6224)

--- include/class/Class.Context.php
+++ include/class/Class.Context.php
@@ -2,7 +2,6 @@
 /*
  * Context Class
  * @author Anakeen
- * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License
 */
 /**
  * Util functions using php DOMDocument
--- include/class/Class.WIFF.php
+++ include/class/Class.WIFF.php
@@ -2,7 +2,6 @@
 /*
  * Web Installer for Freedom Class
  * @author Anakeen
- * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License
 */
 
 function curPageURL()
