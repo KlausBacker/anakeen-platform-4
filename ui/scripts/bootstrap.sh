c437287c87a83583ee42022030371ee9a860a706
Aurélien Brugalières <aurelien.brugalieres@gmail.com>
2017-09-14 17:06:08 +0200

    fix(style): build bootstrap v4 css

--- ui/scripts/bootstrap.sh
+++ ui/scripts/bootstrap.sh
@@ -2,12 +2,11 @@
 if [ $# -ne 2 ]; then
     echo "Usage : $0 lib_sources_dir destination_dir"
 fi
-BASE_PATH=$1
+BASE_PATH=$1/@progress/kendo-theme-bootstrap/modules
 DEST_PATH=$2/bootstrap
 mkdir -p $DEST_PATH/js
 mv $BASE_PATH/bootstrap/dist/css $DEST_PATH
-mv $BASE_PATH/bootstrap/fonts $DEST_PATH
-mv $BASE_PATH/bootstrap/js/*.js $DEST_PATH/js
+mv $BASE_PATH/bootstrap/js/dist/*.js $DEST_PATH/js
 mv $BASE_PATH/bootstrap/dist/js/bootstrap.js $DEST_PATH/js
 mv $BASE_PATH/bootstrap/dist/js/bootstrap.min.js $DEST_PATH/js
-mv $BASE_PATH/bootstrap/less $DEST_PATH
+mv $BASE_PATH/bootstrap/scss $DEST_PATH
