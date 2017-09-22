f057dbc8016beff702e1d745c985657f4a939bb6
david <david.dang@anakeen.com>
2017-09-22 11:42:53 +0200

    refactor(remove delete button from enum bool )

--- Document-uis/src/vendor/Anakeen/Families/mailTemplate/Render/TmailEdit.php
+++ Document-uis/src/vendor/Anakeen/Families/mailTemplate/Render/TmailEdit.php
@@ -17,7 +17,6 @@ class TmailEditRender extends defaultConfigEditRender
         $options->enum(myAttributes::tmail_ulink)->setDisplay('bool');
         $options->enum(myAttributes::tmail_savecopy)->displayDeleteButton(false);
         $options->enum(myAttributes::tmail_ulink)->displayDeleteButton(false);
-        $options->arrayAttribute(myAttributes::tmail_t_from)->setRowMinDefault(1);
         $options->frame(myAttributes::tmail_fr_content)->setTemplate(
             <<< 'HTML'
             <div>
@@ -27,10 +26,23 @@ class TmailEditRender extends defaultConfigEditRender
                 {{{attributes.tmail_t_attach.htmlView}}}
             </div>
             <hr>
+            <div class="panel-heading dcpArray__label dcpLabel">
+                <span class="dcp__array__caret fa fa-lg fa-caret-down" style="font-family: Helvetica.Arial.sans-serif; font-size:12px">Other</span>
+            </div>
+            <div class="dcpArray__content">
                 {{{attributes.tmail_savecopy.htmlView}}}
                 {{{attributes.tmail_ulink.htmlView}}}
+            </div>
 HTML
         );
         return $options;
+    }
+
+    public function getCssReferences(\Doc $document = null)
+    {
+        $css = parent::getCssReferences();
+        $version = \ApplicationParameterManager::getScopedParameterValue("WVERSION");
+        $css["dduiMailTemplate"] = "DOCUMENT/Layout/MailTemplate/tmail.css?ws=" . $version;
+        return $css;
     }
 }
