ad3e362c0c8bb02d877791acf9848718577803ae
david <david.dang@anakeen.com>
2017-09-22 11:40:42 +0200

    feat(MSEARCH family render)

---
+++ Document-uis/src/vendor/Anakeen/Families/msearch/Render/MSearchEdit.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * @author Anakeen
+ * @package DDUI
+*/
+
+namespace Anakeen\Ui;
+
+use \Dcp\AttributeIdentifiers\MSEARCH as myAttributes;
+
+class MSearchEditRender extends defaultConfigEditRender
+{
+    public function getOptions(\Doc $document)
+    {
+        $options = parent::getOptions($document);
+        $options->enum(myAttributes::gui_isrss)->setDisplay('bool');
+        $options->enum(myAttributes::se_memo)->setDisplay('bool');
+        $options->enum(myAttributes::gui_isrss)->displayDeleteButton(false);
+        $options->enum(myAttributes::se_memo)->displayDeleteButton(false);
+        return $options;
+    }
+}
