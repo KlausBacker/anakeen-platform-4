24488a40dea6fe31fe9bf8745dbe5216e4e2fc84
david <david.dang@anakeen.com>
2017-09-22 11:40:33 +0200

    feat (IMAGE family render)

---
+++ Document-uis/src/vendor/Anakeen/Families/image/Render/ImageAccess.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * @author Anakeen
+ * @package FDL
+*/
+
+namespace Anakeen\Ui;
+
+class ImageAccess implements \Dcp\Ui\IRenderConfigAccess
+{
+    /**
+     * @param string $mode
+     * @return \Dcp\Ui\IRenderConfig
+     */
+    public function getRenderConfig($mode, \Doc $document)
+    {
+        switch ($mode) {
+            case \Dcp\Ui\RenderConfigManager::CreateMode:
+            case \Dcp\Ui\RenderConfigManager::EditMode:
+                return new ImageEditRender();
+            case \Dcp\Ui\RenderConfigManager::ViewMode:
+                return new ImageViewRender();
+        }
+        return null;
+    }
+}
