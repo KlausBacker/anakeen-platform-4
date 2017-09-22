3249a0cfbce8a762da69d7d0a6acde73586d1c63
david <david.dang@anakeen.com>
2017-09-22 11:46:00 +0200

    refactor(remove unused cssReferences)

--- Document-uis/src/vendor/Anakeen/Families/wdoc/Render/WdocView.php
+++ Document-uis/src/vendor/Anakeen/Families/wdoc/Render/WdocView.php
@@ -15,12 +15,4 @@ class WdocViewRender extends defaultConfigViewRender
         $options = parent::getOptions($document);
         return $options;
     }
-
-    public function getCssReferences(\Doc $document = null)
-    {
-        $css = parent::getCssReferences();
-        $version = \ApplicationParameterManager::getScopedParameterValue("WVERSION");
-        $css["dduiWvdoc"] = "DOCUMENT/Layout/Wvdoc/wdoc.css?ws=" . $version;
-        return $css;
-    }
 }
