36888a7c26f7c5eac3614ba1cd235e6ccf52f94c
david <david.dang@anakeen.com>
2017-09-22 11:39:59 +0200

    feat(Dir family render)

---
+++ Document-uis/src/vendor/Anakeen/Families/dir/Render/DirEdit.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * @author Anakeen
+ * @package DDUI
+*/
+
+namespace Anakeen\Ui;
+use \Dcp\AttributeIdentifiers\DIR as myAttributes;
+
+class DirEditRender extends defaultConfigEditRender
+{
+    public function getOptions(\Doc $document)
+    {
+        $options = parent::getOptions($document);
+        $options->enum(myAttributes::gui_isrss)->setDisplay('bool');
+        $options->enum(myAttributes::gui_isrss)->displayDeleteButton(false);
+        return $options;
+    }
+}
