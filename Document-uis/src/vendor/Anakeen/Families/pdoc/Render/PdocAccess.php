64d254e41f2f6dbbc24b369419e25ffdc7123970
david <david.dang@anakeen.com>
2017-09-22 11:41:18 +0200

    feat(PDOC family render)

---
+++ Document-uis/src/vendor/Anakeen/Families/pdoc/Render/PdocAccess.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * @author Anakeen
+ * @package FDL
+*/
+
+namespace Anakeen\Ui;
+
+class PdocAccess implements \Dcp\Ui\IRenderConfigAccess
+{
+    /**
+     * @param string $mode
+     * @return \Dcp\Ui\IRenderConfig
+     */
+    public function getRenderConfig($mode, \Doc $document)
+    {
+        switch ($mode) {
+            case \Dcp\Ui\RenderConfigManager::CreateMode:
+            case \Dcp\Ui\RenderConfigManager::EditMode:
+                return new PdocEditRender();
+            case \Dcp\Ui\RenderConfigManager::ViewMode:
+                return new PdocViewRender();
+        }
+        return null;
+    }
+}
