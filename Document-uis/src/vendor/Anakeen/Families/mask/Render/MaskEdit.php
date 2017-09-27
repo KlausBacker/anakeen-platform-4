638cc3705bfc01cc023eb59a42e72523fa3169eb
david <david.dang@anakeen.com>
2017-09-27 10:35:38 +0200

    refactor(table): disable add/move/del row

--- Document-uis/src/vendor/Anakeen/Families/mask/Render/MaskEdit.php
+++ Document-uis/src/vendor/Anakeen/Families/mask/Render/MaskEdit.php
@@ -13,7 +13,9 @@ class MaskEditRender extends DefaultConfigEditRender
     public function getOptions(\Doc $document)
     {
         $options = parent::getOptions($document);
-        $options->commonOption(myAttributes::msk_needeeds)->setInputTooltip('insert Y or N');
+        $options->arrayAttribute(myAttributes::msk_t_contain)->disableRowAdd(true);
+        $options->arrayAttribute(myAttributes::msk_t_contain)->disableRowMove(true);
+        $options->arrayAttribute(myAttributes::msk_t_contain)->disableRowDel(true);
         return $options;
     }
 }
