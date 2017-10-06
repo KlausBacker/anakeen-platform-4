bcb838d17cb7d191fc15340902c926c436da7f28
Éric Brison <eric.brison@anakeen.com>
2017-10-06 10:00:56 +0200

    feat(system); new maintenance page

--- src/TOOLBOX/stop.php
+++ src/TOOLBOX/stop.php
@@ -5,44 +5,110 @@
 */
 /**
  * Display a message to advert that Dynacase Platform being to be upgraded
- *
- * @author Anakeen
- * @version $Id: stop.php,v 1.4 2007/03/06 18:57:03 eric Exp $
- * @package FDL
- * @subpackage CORE
  */
 header("HTTP/1.0 503 Service Unavailable");
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
-<html xmlns="http://www.w3.org/1999/xhtml"> 
-<HEAD>
-<TITLE>Dynacase Maintenance</TITLE>
-<style>
-body { font-family:Trebuchet MS, Helvetica, sans-serif; background-color:#CCC; padding:0px; margin:0px; color:#C1C1C1; }
-#hello { padding: 60px 40px; }
-.title { font-size: 400%; font-weight: bold; text-shadow: -1px -1px 1px #777, 1px 1px 1px #EEE; }
-.content { font-size: 200%; font-weight: bold; text-shadow: 0 1px 1px #fff; }
-.message { padding-top:40px ; font-size:120%; color : #447A25; text-shadow: 0px 1px 1px white;  }
-td { vertical-align:top;padding:20px;}
-</style>
-</HEAD>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Anakeen Platform Maintenance</title>
+    <meta charset='utf-8'/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <style type="text/css">
+
+        html {
+            padding:0;
+            margin:0;
+            height: 100vh;
+            font-size:13px;
+        }
+        body {
+            box-sizing: border-box;
+            font-family: 'Roboto', sans-serif;
+            background-color:#f5f5f5;
+
+            padding: 3rem 0;
+            margin:0;
+            min-height: 100vh;
+
+            display:flex;
+            flex-direction: column;
+        }
+        body > div{
+            text-align: center;
+        }
+        .bottom {
+            flex-grow:1;
+            padding-top: 1rem;
+            text-align: center;
+            width:100%;
+        }
+
+
+        .product {
+            color: #9ecd63;
+            font-style: italic;
+            font-size: 2rem;
+        }
+        .summary {
+            font-size: 1.1rem;
+            font-style: italic;
+            color: #484848;
+        }
+        .description {
+            font-size: 1.3rem;
+            color: #b5b5b5;
+            font-weight: bold;
+        }
+        .maintenance {
+            flex-grow:0;
+            font-size: 2rem;
+            font-weight: bold;
+            background-color:#e9f394;
+        }
+         p {
+            margin: 0.8rem 0;
+        }
+        .logo img {
+            height: 5rem;
+        }
+        .info {
+            font-size:1.4rem;
+            line-height: 1rem;
+        }
+        hr {
+            width:4rem;
+        }
+    </style>
+</head>
 
 <body>
-<div id="hello">
-    <table>
-    <tr><td><img src="Images/maintenance-symbol.png" /></td>
-    <td>
-      <div class="title">Dynacase Platform</div>
-      <div class="message">
-         <div>The system is currently unavailable due to maintenance works.</div> 
-         <div>Please come back later.</div>
-      </div>
-      <div class="message">
-         <div>Une op&eacute;ration de maintenance est en cours.</div> 
-         <div>Merci de revenir plus tard.</div>
-      </div>
-    </td></tr>
-    </table>
-</div>
-</BODY>
-</HTML>
+
+
+    <div>
+        <img src="CORE/Images/maintenance-gearing.png"/>
+    </div>
+    <div class="maintenance">
+        <p>UNDER MAINTENANCE</p>
+    </div>
+    <div class="info">
+        <p>The system is currently unavailable due to maintenance works.</p>
+        <p>Please come back later.</p>
+    </div>
+    <hr/>
+    <div class="bottom">
+        <p class="product">ANAKEEN PLATFORM</p>
+
+        <p class="summary">LOW-CODE PLATFORM FOR HTML5 BUSINESS APPS</p>
+
+        <p class="description">Build process-based applications<br/> to solve everyday business challenges</p>
+
+    </div>
+    <div class="logo">
+        <img class="logo" src="CORE/Images/anakeen-logo.svg"/>
+    </div>
+
+
+</body>
+</html>
