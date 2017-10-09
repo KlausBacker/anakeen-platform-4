3da22365942060db784a58758d969d497f647110
Aurélien Brugalières <aurelien.brugalieres@gmail.com>
2017-10-09 13:49:00 +0200

    fix(collections): remove commented code

--- Samples/BusinessApp/src/public/BUSINESS_APP/src/components/collectionsList/collections.vue
+++ Samples/BusinessApp/src/public/BUSINESS_APP/src/components/collectionsList/collections.vue
@@ -26,24 +26,7 @@
                 </div>
             </div>
 
-            <div class="documentsList__collections__list" ref="listView">
-                <!--<div v-for="c in collections">
-                    <div :class="`list-group-item documentsList__collectionCard${selectedCollection && selectedCollection.ref === c.ref ? ' documentsList__collectionCard&#45;&#45;active': ''}`" @click="onClickCollection($event, c)">
-                        <div class="list-group-item-heading documentsList__collectionCard__heading">
-                            <div class="documentsList__collectionCard__heading__content">
-                                <div class="documentsList__collectionCard__heading__content_icon">
-                                    <img :src="c.image_url" class="documentsList__collectionCard__heading__content_icon_img">
-                                </div>
-                                <div class="documentsList__collectionCard__heading__content_label">
-                                    {{c.html_label}}
-                                </div>
-                            </div>
-                        </div>
-                        <div class="list-group-item-text documentsList__collectionCard__text">
-                        </div>
-                    </div>
-                </div>-->
-            </div>
+            <div class="documentsList__collections__list" ref="listView"></div>
 
             <div class="documentsList__collections_buttons documentsList__collections_buttons--bottom">
                 <div class="documentsList__collections_button documentsList__collections_button--down">
