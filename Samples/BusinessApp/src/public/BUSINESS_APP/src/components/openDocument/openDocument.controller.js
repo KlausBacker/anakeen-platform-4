6229e5e407ca5826b36011c64f24996ad957c906
Aurélien Brugalières <aurelien.brugalieres@gmail.com>
2017-10-20 17:24:14 +0200

    feat(tabs): add paginator buttons and place tabs

--- Samples/BusinessApp/src/public/BUSINESS_APP/src/components/openDocument/openDocument.controller.js
+++ Samples/BusinessApp/src/public/BUSINESS_APP/src/components/openDocument/openDocument.controller.js
@@ -1,123 +1,211 @@
 import A4Tabs from '@/documentsTabs/documentsTabs.vue';
 import A4Document from '@~/Document/Document.vue';
-// import A4OtherTabs from '@/otherDocumentsTabs/documentsTabs.vue';
 export default {
-  components: {
-    "a4-documents-tabs": A4Tabs,
-    "a4-vue-document": A4Document,
-  },
-  mounted() {
-    document.addEventListener('DOMContentLoaded', (event) => {
-      const store = document.getElementById('a4-store');
-      if (store) {
-        store.addEventListener('store-change', (event) => {
-          const storeData = event.detail && event.detail.length ? event.detail[0] : null;
-          this.onStoreChange(storeData);
-        });
-      }
-    });
-    this.initKendo().then(() => {
-      this.sendGetRequest('sba/collections')
-        .then((response) => {
-          this.updateNewActionsItems(response.data.data.sample.collections);
-        });
-    });
-  },
-
-  data() {
-    return {
-      tabstripEl: null,
-      openedDocuments : [],
-    };
-  },
-
-  computed: {
-    emptyData() {
-      return !this.openedDocuments.length;
-    },
-    tabstrip() {
-      return this.tabstripEl ? this.tabstripEl.data('kendoTabStrip') : null;
-    }
-  },
-
-  methods: {
-    onStoreChange(storeData) {
-      if (storeData) {
-        switch (storeData.type) {
-          case 'OPEN_DOCUMENT':
-            this.openedDocuments.push(storeData.data);
-            break;
-        }
-      }
+    components: {
+        'a4-documents-tabs': A4Tabs,
+        'a4-vue-document': A4Document,
     },
 
-    initKendo() {
-      return new Promise((resolve) => {
-        this.$(this.$refs.newActionButton).kendoMenu({
-            animation: false,
-            select: this.onClickNewAction,
-            openOnClick: true,
-            dataSource: [
-                {
-                    text: 'Nouveau',
-                    cssClass: 'documentsList__openDocuments__new__menu',
-                    items: [],
-                },
-            ],
-        });
-        resolve();
-      });
+    created() {
+        this.privateScope = {
+            configureCloseTab: (tab) => {
+                this.$(tab).on('click', "[data-type='remove']", (e) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+
+                    const item = this.$(e.target).closest('.k-item');
+                    this.removeTab(item.index());
+                });
+            },
+
+            bindDocumentEvents: (tabContent) => {
+                console.log(tabContent);
+                const documentInstance = this.$(tabContent).find('a4-document');
+                if (documentInstance && documentInstance.length) {
+                    console.log(documentInstance[0]);
+                    documentInstance[0].addEventListener('actionClick', () => {
+                        console.log('coucou');
+                    });
+                }
+            },
+
+            bindTabsPaginatorEvents: (buttons) => {
+                const closeButton = buttons.filter('.documentsList__documentsTabs__tabs__paginator__close')[0];
+                this.$(closeButton).data('kendoButton').bind('click', (e) => {
+                    this.removeAllTabs();
+                });
+            },
+        };
     },
 
-    onClickNewAction(e) {
-        if (e.item.getAttribute('initid') && e.item.getAttribute('html_label')) {
-            this.openedDocuments.push({
-                title: `Création ${e.item.getAttribute('html_label')}`,
-                initid: e.item.getAttribute('initid'),
-                viewId: '!defaultCreation',
+    mounted() {
+        document.addEventListener('DOMContentLoaded', (event) => {
+            const store = document.getElementById('a4-store');
+            if (store) {
+                store.addEventListener('store-change', (event) => {
+                    const storeData = event.detail && event.detail.length ? event.detail[0] : null;
+                    this.onStoreChange(storeData);
+                });
+            }
+        });
+        this.initKendo().then(() => {
+            this.sendGetRequest('sba/collections')
+              .then((response) => {
+                this.updateNewActionsItems(response.data.data.sample.collections);
             });
-        }
-    },
-
-    updateNewActionsItems(data) {
-        const items = data.map((c) => {
-            return {
-                text: c.html_label,
-                cssClass: 'documentsList__openDocuments__new__menu__item',
-                imageAttr: {
-                    alt: 'Image',
-                    height: '16px',
-                    width: '16px',
-                },
-                imageUrl: c.image_url,
-                attr: c,
-            };
         });
-        const menu = this.$(this.$refs.newActionButton).data('kendoMenu');
-        menu.append(items, '.documentsList__openDocuments__new__menu');
     },
 
-    sendGetRequest(url) {
-      const element = this.$(this.$refs.wrapper);
-      this.$kendo.ui.progress(element, true);
-      return new Promise((resolve, reject) => {
-        this.$http.get(url)
-          .then((response) => {
-            this.$kendo.ui.progress(element, false);
-            resolve(response);
-          }).catch((error) => {
-          this.$kendo.ui.progress(element, false);
-          reject(error);
-        });
-      });
+    data() {
+        return {
+            tabstripEl: null,
+            openedDocuments: [],
+        };
     },
 
-    onDocumentActionClick(event, document, options) {
-      console.log(event, document, options);
+    computed: {
+        emptyData() {
+            return !this.openedDocuments.length;
+        },
+
+        tabstrip() {
+            return this.tabstripEl ? this.tabstripEl.data('kendoTabStrip') : null;
+        },
     },
 
-    onAttributeAnchorClick(event, document ,options) {
-      console.log(event, document, options);
-    }
-  },
+    methods: {
+        onStoreChange(storeData) {
+            if (storeData) {
+                switch (storeData.type) {
+                case 'OPEN_DOCUMENT':
+                    this.addTab(storeData.data);
+                break;
+            }
+            }
+        },
+
+        initKendo() {
+            return new Promise((resolve) => {
+                this.$(this.$refs.newActionButton).kendoMenu({
+                    animation: false,
+                    select: this.onClickNewAction,
+                    openOnClick: true,
+                    dataSource: [
+                        {
+                            text: 'Nouveau',
+                            cssClass: 'documentsList__openDocuments__new__menu',
+                            items: [],
+                        },
+                    ],
+                });
+                this.tabstripEl = this.$(this.$refs.tabstrip).kendoTabStrip({
+                    animation: false,
+                    scrollable: true,
+                });
+                const buttons = this.$(this.$refs.tabsPaginator).find('button').kendoButton();
+                this.privateScope.bindTabsPaginatorEvents(buttons);
+
+                resolve();
+            });
+        },
+
+        onClickNewAction(e) {
+            if (e.item.getAttribute('initid') && e.item.getAttribute('html_label')) {
+                this.addTab({
+                    title: `Création ${e.item.getAttribute('html_label')}`,
+                    initid: e.item.getAttribute('initid'),
+                    viewId: '!defaultCreation',
+                });
+            }
+        },
+
+        updateNewActionsItems(data) {
+            const items = data.map((c) => {
+                return {
+                    text: c.html_label,
+                    cssClass: 'documentsList__openDocuments__new__menu__item',
+                    imageAttr: {
+                        alt: 'Image',
+                        height: '16px',
+                        width: '16px',
+                    },
+                    imageUrl: c.image_url,
+                    attr: c,
+                };
+            });
+            const menu = this.$(this.$refs.newActionButton).data('kendoMenu');
+            menu.append(items, '.documentsList__openDocuments__new__menu');
+        },
+
+        sendGetRequest(url) {
+            const element = this.$(this.$refs.wrapper);
+            this.$kendo.ui.progress(element, true);
+            return new Promise((resolve, reject) => {
+                this.$http.get(url)
+                  .then((response) => {
+                    this.$kendo.ui.progress(element, false);
+                    resolve(response);
+                }).catch((error) => {
+                    this.$kendo.ui.progress(element, false);
+                    reject(error);
+                });
+            });
+        },
+
+        headerTemplate(data) {
+            const template = `<span>${data.title}</span>
+            <span class="documentsList__documentsTabs__tabs__tab__close" data-type="remove">
+                <span class="k-icon k-i-x"></span>
+            </span>`;
+
+            return template;
+
+        },
+
+        contentTemplate(data) {
+            let option = `${data.initid ? `initid="${data.initid}"` : ''}`;
+            option += `${data.viewId ? ` viewid="${data.viewId}"` : ''}`;
+            return `<a4-document ${option}></a4-document>`;
+        },
+
+        addTab(data) {
+            this.openedDocuments.push(data);
+            this.tabstrip.append({
+                text: this.headerTemplate(data),
+                encoded: false,
+                content: this.contentTemplate(data),
+            });
+            this.tabstrip.select(this.tabstrip.items().length - 1);
+            const tab = this.tabstrip.items()[this.tabstrip.items().length - 1];
+            const content = this.tabstrip.contentElement(this.tabstrip.items().length - 1);
+            this.privateScope.configureCloseTab(tab);
+            this.privateScope.bindDocumentEvents(content);
+        },
+
+        removeTab(index) {
+            this.openedDocuments.splice(index, 1);
+            const item = this.tabstrip.items()[index];
+            this.tabstrip.remove(index);
+            if (this.tabstrip.items().length > 0 && this.$(item).hasClass('k-state-active')) {
+                this.tabstrip.select(0);
+            }
+        },
+
+        removeAllTabs() {
+            this.openedDocuments = [];
+            this.tabstrip.remove('li');
+        },
+
+        onClickCloseAllTabs() {
+            this.removeAllTabs();
+        },
+
+        onDocumentActionClick(event, document, options) {
+            console.log(event, document, options);
+        },
+
+        onAttributeAnchorClick(event, document, options) {
+            console.log(event, document, options);
+        },
+    },
 };
