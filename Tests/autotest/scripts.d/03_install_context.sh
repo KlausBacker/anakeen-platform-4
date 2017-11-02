f64877f9a2c5167e8577a8542f6da001758fb4d2
Jérôme Augé <jerome.auge@anakeen.com>
2017-11-02 13:38:53 +0100

    feat(CI): Collect artifacts for GitLab's CI
    Generate JUnit output file and generate HTML view using xunit-viewer.
    
    Also:
    - Refactored monolithic `autotest.sh` main script into sub-scripts in
      the `scripts.d` directory.
    - Remove support for "override" scripts.
    - Remove use of "/autotest/share" docker's volume.

---
+++ Tests/autotest/scripts.d/03_install_context.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eo pipefail
+
+function wiff {
+    /var/www/html/dynacase-control/wiff "$@"
+}
+
+wiff repository delete --all
+wiff repository add --default anakeen_core http://eec.corp.anakeen.com/integration/repo/anakeen/a4/webinst/
+wiff repository add --default local        file:///var/www/html/repo
+wiff create context dynacase /var/www/html/dynacase
+wiff context dynacase repository enable --default
+
+wiff context dynacase module install --unattended anakeen-core-test
