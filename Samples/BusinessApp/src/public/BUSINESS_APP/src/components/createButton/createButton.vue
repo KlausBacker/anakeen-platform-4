16b90ec3f48267b6b342b3393fbbf308026a9822
Aurélien Brugalières <aurelien.brugalieres@gmail.com>
2017-11-06 10:16:34 +0100

    feat(tabs): init a new button slot for tabs

---
+++ Samples/BusinessApp/src/public/BUSINESS_APP/src/components/createButton/createButton.vue
@@ -0,0 +1,9 @@
+<template>
+    <ul class="documentsList__createButton" ref="createButton">
+    </ul>
+</template>
+<!-- CSS to this component only -->
+<style lang="scss">
+    @import "./createButton.scss";
+</style>
+<script src="./createButton.controller.js"></script>
