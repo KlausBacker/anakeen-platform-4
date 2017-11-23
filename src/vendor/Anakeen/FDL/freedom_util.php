1d2e17ab010dff15318d83f6d6b2210fa6556c63
Éric Brison <eric.brison@anakeen.com>
2017-11-23 16:27:56 +0100

    feat(deprecated): getFromId

--- src/vendor/Anakeen/FDL/freedom_util.php
+++ src/vendor/Anakeen/FDL/freedom_util.php
@@ -313,7 +313,7 @@ function getTDoc($dbaccess, $id, $sqlfilters = array() , $result = array())
     if (!($id > 0)) return false;
     $dbid = DbManager::getDbId();
     $table = "doc";
-    $fromid = getFromId($dbaccess, $id);
+    $fromid = \Dcp\Core\DocManager::getFromId($id);
     if ($fromid > 0) $table = "doc$fromid";
     else if ($fromid == - 1) $table = "docfam";
     if ($fromid == 0) return false; // no document can be found
@@ -783,7 +783,7 @@ function getRevTDoc($dbaccess, $initid, $rev)
     
     if (!($initid > 0)) return false;
     $table = "docread";
-    $fromid = getFromId($dbaccess, $initid);
+    $fromid = \Dcp\Core\DocManager::getFromId($initid);
     $sql = sprintf("select fromid from docread where initid=%d and revision=%d", $initid, $rev);
     DbManager::query( $sql, $fromid, true, true);
     if ($fromid > 0) $table = "doc$fromid";
