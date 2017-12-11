d1b7e454c894cbabe423d72c22ba4245afdf03f6
Éric Brison <eric.brison@anakeen.com>
2017-12-11 14:06:08 +0100

    feat(crontab): Enhance crontab management (refs #7045)

---
+++ src/vendor/Anakeen/WHAT/Crontab/Class.CrontabDocument.php
@@ -0,0 +1,22 @@
+<?php
+namespace Dcp;
+
+class CrontabDocument extends CrontabElement
+{
+    public function __toString()
+    {
+        $previousElement = null;
+        /* Coalesce empty text elements */
+        $elements = array_filter($this->childs, function ($currentElement) use (&$previousElement)
+        {
+            if ($previousElement !== null && is_a($previousElement, '\Dcp\CrontabTextElement') && is_a($currentElement, '\Dcp\CrontabTextElement')) {
+                if (strlen((string)$previousElement) <= 0 && strlen((string)$currentElement) <= 0) {
+                    return false;
+                }
+            }
+            $previousElement = $currentElement;
+            return true;
+        });
+        return join("\n", $elements);
+    }
+}
