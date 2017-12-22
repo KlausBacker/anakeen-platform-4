3448c9e180593b1db5f77b3763faaa8cb36c1eb4
Éric Brison <eric.brison@anakeen.com>
2017-12-22 18:15:15 +0100

    feat(test): Use ank component to test

--- Tests/src/Apps/TEST_DOCUMENT_SELENIUM/action.testrender.php
+++ Tests/src/Apps/TEST_DOCUMENT_SELENIUM/action.testrender.php
@@ -6,8 +6,13 @@ function testRender(Action $action) {
     $smartElement = \Dcp\UI\UIGetAssetPath::getSmartElement();
     $ws = \Dcp\UI\UIGetAssetPath::getWs();
 
+    $action->parent->addJsRef(\Dcp\UI\UIGetAssetPath::getJSJqueryPath());
+    $action->parent->addJsRef(\Dcp\UI\UIGetAssetPath::getJSKendoPath());
+    $action->parent->addJsRef("components/dist/ank-components.js?ws=".$ws);
+
+
     foreach ($smartElement["js"] as $currentPath) {
-        $action->parent->addJsRef($currentPath);
+      //  $action->parent->addJsRef($currentPath);
     }
     $action->parent->addJsRef("TEST_DOCUMENT_SELENIUM/dist/testrender.js?ws=".$ws);
 
