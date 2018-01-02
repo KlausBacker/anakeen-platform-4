598df0c8f038b0ae2e6b5c2d601091d9931a61e0
Aurélien Brugalières <aurelien.brugalieres@anakeen.com>
2018-01-02 08:17:58 +0100

    fix(sample): update fees smart structure stub

--- stubs/ba_fees__STUB.php
+++ stubs/ba_fees__STUB.php
@@ -21,6 +21,10 @@ namespace Dcp\AttributeIdentifiers {
 		const fee_advance='fee_advance';
 		/** [money] Total des dépenses */
 		const fee_total='fee_total';
+		/** [docid("BA_RH_DIR")] Signature du bénéficiaire */
+		const fee_usersign='fee_usersign';
+		/** [docid("BA_RH_DIR")] Signature du directeur */
+		const fee_dirsign='fee_dirsign';
 		/** [tab] Dépenses */
 		const fee_tab_all_exp='fee_tab_all_exp';
 		/** [frame] Tableau des dépenses */
@@ -41,8 +45,12 @@ namespace Dcp\AttributeIdentifiers {
 		const fee_exp_tax='fee_exp_tax';
 		/** [image] Fichier Justificatif */
 		const fee_exp_file='fee_exp_file';
-		/** [date] Date photo */
-		const fee_exp_file_date='fee_exp_file_date';
+		/** [longtext] Hash */
+		const fee_exp_file_hash='fee_exp_file_hash';
+		/** [longtext] Stamp ID */
+		const fee_exp_file_stampid='fee_exp_file_stampid';
+		/** [date] Stamp Date */
+		const fee_exp_file_stamp_date='fee_exp_file_stamp_date';
 		/** [double] Latitude */
 		const fee_exp_file_lat='fee_exp_file_lat';
 		/** [double] Longitude */
@@ -70,5 +78,11 @@ namespace Dcp\AttributeIdentifiers {
 	}
 	/** Cycle Notes de frais  */
 	class Ba_wfees extends Wdoc {
+		/** [frame] Asks */
+		const wfee_fr_ask='wfee_fr_ask';
+		/** [enum] Asks */
+		const wfee_user_valid='wfee_user_valid';
+		/** [enum] Autorisez-vous la demande de dépassement ? */
+		const wfee_exceed_decision='wfee_exceed_decision';
 	}
 }
