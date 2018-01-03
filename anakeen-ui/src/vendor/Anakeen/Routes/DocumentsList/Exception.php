3843ef0c34f857c24d13eead28aaefe0f328189e
Aurélien Brugalières <aurelien.brugalieres@anakeen.com>
2018-01-03 12:11:13 +0100

    fix(documentsList): fix documents list component, add content url prop

---
+++ anakeen-ui/src/vendor/Anakeen/Routes/DocumentsList/Exception.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace {
+    /**
+     * Errors code used
+     * @class ErrorCodeDocList
+     * @brief List all error code errors
+     * @see ErrorCodeDocList
+     */
+    class ErrorCodeDocList
+    {
+        /**
+         * @errorCode in case of family not found
+         */
+        const DOCLIST0001 = 'Error, family "%s" not found';
+
+        /**
+         * @errorCode in case of collection not found
+         */
+        const DOCLIST0002 = 'Error, collection "%s" not found';
+
+        /**
+         * @errorCode in case of element is not family or collection
+         */
+        const DOCLIST0003 = 'Error, the element "%s" is not a family or a collection';
+    }
+}
