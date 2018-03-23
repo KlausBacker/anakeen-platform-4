17b00e444d04fa5e29ea100d92953c08ced7d3ae
Éric Brison <eric.brison@anakeen.com>
2018-03-23 08:42:25 +0100

    fix(families): adjust mask family

--- src/vendor/Anakeen/FDL/Class.Mask.php
+++ src/vendor/Anakeen/FDL/Class.Mask.php
@@ -73,14 +73,14 @@ class Mask extends \Dcp\Family\Base
      * Verify if family and attributes are coherents
      * @return string error message
      */
-    protected function verifyIntegraty()
+    public function verifyIntegraty()
     {
         $mskAttrids = $this->getMultipleRawValues(myAttr::msk_attrids);
         $famid = $this->getRawValue(myAttr::msk_famid);
         if (!$famid) {
             return \ErrorCode::getError("MSK0001", $this->name);
         }
-        $fam = new_doc($this->dbaccess, $famid);
+        $fam = DocManager::getFamily($famid);
         if ($fam->doctype !== "C") {
             return \ErrorCode::getError("MSK0002", $famid, $this->name);
         }
