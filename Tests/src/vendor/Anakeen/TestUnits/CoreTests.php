4969e8bf205819f4776e171889fb7488546a977c
Éric Brison <eric.brison@anakeen.com>
2018-04-09 18:29:57 +0200

    wip : Pu all

--- Tests/src/vendor/Anakeen/TestUnits/CoreTests.php
+++ Tests/src/vendor/Anakeen/TestUnits/CoreTests.php
@@ -35,10 +35,10 @@ class CoreTests
         $suite = new FrameworkDcp('Project');
 
         Gettext::___("Hello"); // Include ___
-     //   $suite->addTest((new SuiteRoutes)());
-     //   $suite->addTest(SuiteDcp::suite());
-      //  $suite->addTest(SuiteDcpAttribute::suite());
-      //  $suite->addTest(SuiteDcpUser::suite());
+        $suite->addTest((new SuiteRoutes)());
+        $suite->addTest(SuiteDcp::suite());
+        $suite->addTest(SuiteDcpAttribute::suite());
+        $suite->addTest(SuiteDcpUser::suite());
         $suite->addTest(SuiteDcpSecurity::suite());
         // ...
         printf("\nError log in [%s], messages in [%s]\n", self::LOGFILE, self::MSGFILE);
