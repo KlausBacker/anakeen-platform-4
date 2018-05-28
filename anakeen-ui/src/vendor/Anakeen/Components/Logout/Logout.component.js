d543882b8d59d908d030d87bcda5c42fcb00e223
Aurelien Gaillard <aurelien.gaillard@anakeen.com>
2018-05-28 09:10:26 +0200

    feat(Logout):Change route

--- anakeen-ui/src/vendor/Anakeen/Components/Logout/Logout.component.js
+++ anakeen-ui/src/vendor/Anakeen/Components/Logout/Logout.component.js
@@ -15,7 +15,7 @@ export default {
             if (event.defaultPrevented) {
                 this.$emit('logoutCancelled');
             } else {
-                this.$http.delete('/components/logout/logout')
+                this.$http.delete('/components/logout/session')
                     .then(response => {
                         this.$emit('afterLogout', response.data);
                         document.location.assign(response.data.location || '/');
