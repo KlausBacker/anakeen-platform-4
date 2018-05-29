2317e302613bb41979628031352b62515d592796
Charles Bonnissent <charles.bonnissent@anakeen.com>
2018-05-29 19:03:47 +0200

    feat(user): update user tree and grid

--- admin-center/src/vendor/Anakeen/Routes/Admin/Account/Groups.php
+++ admin-center/src/vendor/Anakeen/Routes/Admin/Account/Groups.php
@@ -7,7 +7,6 @@ use Anakeen\Core\DocManager;
 
 class Groups
 {
-    private static $nbUserGroupCache = "./nbUserGroupCache.json";
 
     public function __invoke(\Slim\Http\request $request, \Slim\Http\response $response, $args) {
 
@@ -19,33 +18,27 @@ class Groups
             $searchAccount->addFilter("lastname ~* '%s' OR login ~* '%s'", preg_quote($filter));
         }
 
-        $result = [];
-
-        $cache = [];
-        if (is_file(self::$nbUserGroupCache)) {
-            $cache = json_decode(file_get_contents(self::$nbUserGroupCache), true);
-        }
-
+        $groups = [];
+        //First iteration
         foreach ($searchAccount->search() as $currentAccount) {
-            /* @var $currentAccount \Anakeen\Core\Account */
-            $revDate = DocManager::getDocumentProperties($currentAccount->fid, ["revdate"])["revdate"];
-            if (!(isset($cache[$currentAccount->id]) && $cache[$currentAccount->id]["revdate"] === $revDate)) {
-                $cache[$currentAccount->id]["revdate"] = $revDate;
-                $cache[$currentAccount->id]["nbUser"] = count($currentAccount->getGroupUserList());
+            $nbUser = 0;
+            $userList = $currentAccount->getAllMembers();
+            if (is_array($userList)) {
+                $nbUser = count($userList);
             }
-            $result[$currentAccount->id] = [
+            /* @var $currentAccount \Anakeen\Core\Account */
+            $groups[$currentAccount->id] = [
                 "login"=> $currentAccount->login,
                 "id" => $currentAccount->fid,
                 "accountId" => $currentAccount->id,
                 "parents" => $currentAccount->getMemberOf(),
                 "title" => getDocTitle($currentAccount->fid),
-                "nbUser" => $cache[$currentAccount->id]["nbUser"]
+                "nbUser" => $nbUser,
+                "items" => []
             ];
         }
 
-        file_put_contents(self::$nbUserGroupCache, json_encode($cache));
-
-        return $response->withJson($result);
+        return $response->withJson($groups);
 
     }
 }
