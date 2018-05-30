d65290c2af295c6bd88c0b445a42d2e48438a155
Aurelien Gaillard <aurelien.gaillard@anakeen.com>
2018-05-30 12:00:14 +0200

    Test page: Add Logout ant Identity component

--- Tests/src/vendor/Anakeen/Routes/UiTest/TestPage.js
+++ Tests/src/vendor/Anakeen/Routes/UiTest/TestPage.js
@@ -1,6 +1,13 @@
 import './TestPage.css';
 const _ = require('underscore');
 
+import AnkIdentity from "../../../../../../anakeen-ui/src/vendor/Anakeen/Components/Identity/Identity.component";
+import AnkLogout from "../../../../../../anakeen-ui/src/vendor/Anakeen/Components/Logout/Logout.component";
+
+export default {
+    components: {AnkIdentity, AnkLogout}
+}
+
 $(document).ready(() => {
 
 });
