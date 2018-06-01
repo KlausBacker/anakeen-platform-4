35b7bb1c65f1db6fa7bd7a3947ba96c9967b2994
Aurelien Gaillard <aurelien.gaillard@anakeen.com>
2018-06-01 17:20:55 +0200

    feat(AuthentPassword): Authent password input usable as a web component

--- anakeen-ui/src/vendor/Anakeen/Components/main.js
+++ anakeen-ui/src/vendor/Anakeen/Components/main.js
@@ -55,6 +55,7 @@ import DocumentTabs from './DocumentTabs/documentTabs.vue';
 import AnakeenLoading from './AnakeenLoading/AnakeenLoading.vue';
 import Logout from './Logout/Logout.vue';
 import Identity from './Identity/Identity.vue';
+import AuthentPassword from './Authent/AuthentPassword.vue'
 
 Vue.customElement('ank-loading', AnakeenLoading);
 Vue.customElement('ank-authent', Authent);
@@ -63,3 +64,4 @@ Vue.customElement('ank-se-list', SEList);
 Vue.customElement('ank-document-tabs', DocumentTabs);
 Vue.customElement('ank-logout', Logout);
 Vue.customElement('ank-identity', Identity);
+Vue.customElement('ank-authent-password', AuthentPassword);
