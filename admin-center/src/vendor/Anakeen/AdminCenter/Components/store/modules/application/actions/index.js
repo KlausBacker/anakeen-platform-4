8998fdd18ddb90f3736d171bb3f375251d745bf7
Aurélien Brugalières <aurelien.brugalieres@anakeen.com>
2018-06-05 10:20:59 +0200

    feat(modal): add modal component

--- admin-center/src/vendor/Anakeen/AdminCenter/Components/store/modules/application/actions/index.js
+++ admin-center/src/vendor/Anakeen/AdminCenter/Components/store/modules/application/actions/index.js
@@ -1,5 +1,8 @@
 export default {
     showMessage ({ commit }, message) {
         // Intercepted by notification component
+    },
+    showModal ({ commit }, modalConfig) {
+        // Intercepted by modal component
     }
 };
