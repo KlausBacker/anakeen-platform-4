e824f7323f395804cd44367ba92c4e4257a795b0
Aurélien Brugalières <aurelien.brugalieres@anakeen.com>
2018-06-11 17:36:31 +0200

    fix(components): fix bundled component file cache and fix test page

--- Tests/src/vendor/Anakeen/Routes/UiTest/TestPage.php
+++ Tests/src/vendor/Anakeen/Routes/UiTest/TestPage.php
@@ -10,20 +10,40 @@ class TestPage
     public function __invoke(\Slim\Http\request $request, \Slim\Http\response $response, $args)
     {
         $page = __DIR__ . "/TestPage.html";
+        $template = file_get_contents($page);
+        $version = \Dcp\Ui\UIGetAssetPath::getWs();
+        $data = [
+            "JS" => [
+                [
+                    "key" =>"jquery",
+                    "path" => \Dcp\Ui\UIGetAssetPath::getJSJqueryPath()
+                ],
+                [
+                    "key" =>"kendo",
+                    "path" => \Dcp\Ui\UIGetAssetPath::getJSKendoPath()
+                ],
+                [
+                    "key" =>"ank-components",
+                    "path" => \Dcp\Ui\UIGetAssetPath::getSmartWebComponentsPath()
+                ],
+                [
+                    "key" => "test-page",
+                    "path" =>  \Dcp\Ui\UIGetAssetPath::getCustomAssetPath("apps/uitest/dist/TestPage.js")
+                ]
+            ],
+            "CSS" => [
+                [
+                    "key" => "bootstrap",
+                    "path" => \Dcp\Ui\UIGetAssetPath::getCustomAssetPath("/css/ank/document/bootstrap.css")
+                ],
+                [
+                    "key" => "kendo",
+                    "path" => \Dcp\Ui\UIGetAssetPath::getCustomAssetPath("/css/ank/document/kendo.css")
+                ]
+            ]
+        ];
+        $mustache = new \Mustache_Engine();
 
-        $action = ContextManager::getCurrentAction();
-        $action->lay = new \Layout($page, $action);
-
-        $action->parent->addJsRef(\Dcp\Ui\UIGetAssetPath::getJSJqueryPath());
-        $action->parent->addJsRef(\Dcp\Ui\UIGetAssetPath::getJSKendoPath());
-        $action->parent->addJsRef("components/dist/ank-components.js");
-        $action->parent->addJsRef("apps/uitest/dist/TestPage.js");
-
-        $action->parent->AddCssRef("css/ank/document/bootstrap.css");
-        $action->parent->AddCssRef("css/ank/document/kendo.css");
-
-        $action->lay->set("appicon", $action->parent->icon);
-
-        return $response->write($action->lay->gen());
+        return $response->write($mustache->render($template, $data));
     }
 }
