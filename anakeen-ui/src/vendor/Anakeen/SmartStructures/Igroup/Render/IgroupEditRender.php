29807c899c1da7d57c424272badca13e4b2aec6b
Charles Bonnissent <charles.bonnissent@anakeen.com>
2018-06-12 12:20:09 +0200

    feat(groupe): add groupe handling for user

--- anakeen-ui/src/vendor/Anakeen/SmartStructures/Igroup/Render/IgroupEditRender.php
+++ anakeen-ui/src/vendor/Anakeen/SmartStructures/Igroup/Render/IgroupEditRender.php
@@ -18,13 +18,6 @@ class IgroupEditRender extends DefaultConfigEditRender
         $options = parent::getOptions($document);
         $options->enum(myAttributes::grp_hasmail)->setDisplay('bool');
         $options->enum(myAttributes::grp_hasmail)->displayDeleteButton(false);
-        $roleButton = new ButtonOptions();
-        $roleButton->url = "?app=FDL&action=OPENDOC&famid=ROLE&updateAttrid=grp_roles&autoclose=yes";
-        $roleButton->target= "_dialog";
-        $roleButton->class= "add-doc";
-        $roleButton->title= "Créer un document Rôle";
-        $roleButton->htmlContent= '<i class="fa fa-plus"></i>';
-        $options->docid(myAttributes::grp_roles)->addButton($roleButton);
         return $options;
     }
 }
