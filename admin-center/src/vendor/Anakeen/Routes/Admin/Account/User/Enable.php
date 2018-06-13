4ad13a428ec18a965b4037d99d76f3c4af5b5394
Charles Bonnissent <charles.bonnissent@anakeen.com>
2018-06-13 16:14:49 +0200

    feat(account): add account helper

---
+++ admin-center/src/vendor/Anakeen/Routes/Admin/Account/User/Enable.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Anakeen\Routes\Admin\Account\User;
+
+use Anakeen\SmartElementManager;
+
+class Enable
+{
+    /**
+     * @param \Slim\Http\request $request
+     * @param \Slim\Http\response $response
+     * @param $args
+     * @return \Slim\Http\Response
+     * @throws \Anakeen\Core\DocManager\Exception
+     */
+    public function __invoke(\Slim\Http\request $request, \Slim\Http\response $response, $args)
+    {
+        /* @var $smartElement \SmartStructure\Iuser */
+        $smartElement = SmartElementManager::getDocument($args["userSmartId"]);
+        $err = $smartElement->activateAccount();
+        if ($err) {
+            return $response->withStatus(500, $err)->write($err);
+        }
+        return $response->withStatus(200);
+    }
+
+}
