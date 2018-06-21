d1544e63c4e4699e0f10423d0975fc702fc419a9
Éric Brison <eric.brison@anakeen.com>
2018-06-21 14:24:15 +0200

    feat(cvdoc): Add constraint on render config attribute

---
+++ anakeen-ui/src/vendor/Anakeen/SmartStructures/Cvdoc/CVDocConstraint.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Anakeen\SmartStructures\Cvdoc;
+
+use Anakeen\Core\Internal\Autoloader;
+use Dcp\Ui\IRenderConfig;
+use Dcp\Ui\IRenderConfigAccess;
+
+class CVDocConstraint
+{
+    protected static function cvIsA($className, $isA)
+    {
+        if ($className) {
+            try {
+                if ($className[0] !== '\\') {
+                    $className = '\\' . $className;
+                }
+                if ($isA[0] !== '\\') {
+                    $isA = '\\' . $isA;
+                }
+
+                if (!Autoloader::classExists($className)) {
+                    return sprintf(___("Class \"%s\" not exists"), $className);
+                }
+
+
+                $a = new $className();
+                if (!is_a($a, $isA)) {
+                    return sprintf(___("Class \"%s\" not implement %s"), $className, $isA);
+                }
+            } catch (\Exception $e) {
+                return sprintf(___("Class \"%s\" problem : %s"), $className, $e->getMessage());
+            }
+        }
+        return "";
+    }
+
+    public static function isARenderConfig($className) {
+        return self::cvIsA($className,  IRenderConfig::class);
+    }
+    public static function isARenderAccess($className) {
+        return self::cvIsA($className,  IRenderConfigAccess::class);
+    }
+}
