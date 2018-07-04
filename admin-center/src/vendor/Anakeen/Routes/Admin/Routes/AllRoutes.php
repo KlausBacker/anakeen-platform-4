5d2dc038bd0595d27e4331193e1122b2583ec75f
david <david.dang@anakeen.com>
2018-07-04 17:10:05 +0200

    feat(Route): Route API

--- admin-center/src/vendor/Anakeen/Routes/Admin/Routes/AllRoutes.php
+++ admin-center/src/vendor/Anakeen/Routes/Admin/Routes/AllRoutes.php
@@ -1,7 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: david
- * Date: 03/07/18
- * Time: 14:56
- */
+
+namespace Anakeen\Routes\Admin\Routes;
+
+use Anakeen\Core\DbManager;
+use Dcp\Db\Exception;
+
+class AllRoutes
+    public function __invoke(\Slim\Http\request $request, \Slim\Http\response $response)
+    {
+        $allRoutes = new \Anakeen\Router\RoutesConfig();
+        $tabRoutes = $allRoutes->getRoutes();
+        $result = [];
+        foreach($tabRoutes as $route){
+            $formatedRoute = $this->formatRoute($route);
+            if($formatedRoute !== null){
+                $result[]=$formatedRoute;
+            }
+        }
+        return $response->withJson($this->formatTreeDataSource($result));
+    }
+}
