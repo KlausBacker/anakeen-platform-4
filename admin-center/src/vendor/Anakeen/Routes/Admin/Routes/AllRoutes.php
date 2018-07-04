97e4461a88f65f7ef17192ddec08168059a43111
david <david.dang@anakeen.com>
2018-07-04 17:11:10 +0200

    feat(Route): data fetching function

--- admin-center/src/vendor/Anakeen/Routes/Admin/Routes/AllRoutes.php
+++ admin-center/src/vendor/Anakeen/Routes/Admin/Routes/AllRoutes.php
@@ -6,6 +6,27 @@ use Anakeen\Core\DbManager;
 use Dcp\Db\Exception;
 
 class AllRoutes
+{
+    private function formatRoute($route)
+    {
+            $formatedRoute = [];
+
+            $nsName = explode('::', $route->name, 2);
+
+            $formatedRoute['nameSpace'] = $nsName[0];
+            $formatedRoute['name'] = $nsName[1];
+
+            $formatedRoute['description'] = $route->description;
+
+            $formatedRoute['method'] = $route->methods[0];
+            $formatedRoute['pattern'] = $route->pattern;
+
+            $formatedRoute['priority'] = $route->priority;
+            $formatedRoute['overrided'] = $route->override;
+
+
+            return $formatedRoute;
+    }
     public function __invoke(\Slim\Http\request $request, \Slim\Http\response $response)
     {
         $allRoutes = new \Anakeen\Router\RoutesConfig();
