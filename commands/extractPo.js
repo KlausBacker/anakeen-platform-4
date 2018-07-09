6f4c4f0cdfeaf6782e8b780d544a6fb9f39caaf8
Charles Bonnissent <charles.bonnissent@anakeen.com>
2018-07-09 11:50:31 +0200

    feat(cli): add po command

---
+++ commands/extractPo.js
@@ -0,0 +1,36 @@
+const gulp = require('gulp');
+const {po} = require("../tasks/po");
+const signale = require("signale");
+
+signale.config({
+    displayTimestamp: true,
+    displayDate: true
+});
+
+exports.desc = 'Extract the po of the module';
+exports.builder = {
+    sourcePath: {
+        defaultDescription: 'path of the info.xml',
+        alias: 's',
+        default: '.',
+        type: 'string'
+    }
+};
+
+exports.handler = function (argv) {
+    try {
+        signale.time("po");
+        po(argv);
+        const task = gulp.task('po');
+        task().then(() => {
+            signale.timeEnd("po");
+            signale.success("po done");
+        }).catch((e) => {
+            signale.timeEnd("po");
+            signale.error(e);
+        });
+    } catch (e) {
+        signale.timeEnd("po");
+        signale.error(e);
+    }
+};
--- tasks/po.js
+++ tasks/po.js
@@ -3,16 +3,13 @@ const { getPOExtractor } = require("../utils/POExtractor");
 const path = require("path");
 const signale = require('signale');
 
-exports.po = (sourcePath, targetPath = './extraction', format = 'all') => {
+exports.po = ({sourcePath, targetPath = './extraction', format = 'all'}) => {
     return gulp.task('po', async () => {
         if(sourcePath === undefined) {
         signale.error("No source path specified.");
         return;
     }
     try {
-        if(targetPath.startsWith('./')) {
-            targetPath = sourcePath + targetPath.substr(1);
-        }
         const POExtractor = await getPOExtractor(sourcePath, targetPath, format);
         gulp.src(path.join(POExtractor.extractDir, "**"))
             .pipe(gulp.dest(targetPath));
