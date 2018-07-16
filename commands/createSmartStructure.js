620ce4fd5a7df1b70d9a261fe71377f90f40ce6c
Charles Bonnissent <charles.bonnissent@anakeen.com>
2018-07-16 17:58:52 +0200

    feat(createSmartStructure): add new command createSmartStructure

---
+++ commands/createSmartStructure.js
@@ -0,0 +1,153 @@
+const gulp = require("gulp");
+const signale = require("signale");
+const fs = require("fs");
+const inquirer = require("inquirer");
+const { createSmartStructure } = require("../tasks/createSmartStructure");
+const { checkModuleName, checkVendorName } = require("../utils/checkName");
+
+signale.config({
+  displayTimestamp: true,
+  displayDate: true
+});
+
+exports.desc = "Create a smart structure";
+const builder = {
+  sourcePath: {
+    defaultDescription: "path to the module",
+    alias: "s",
+    default: ".",
+    type: "string",
+    coerce: arg => {
+      if (!fs.statSync(arg).isDirectory()) {
+        throw new Error("Unable to find the source directory " + arg);
+      }
+      return arg;
+    }
+  },
+  name: {
+    defaultDescription: "name of the smart structure",
+    alias: "n",
+    type: "string",
+    coerce: arg => {
+      if (!checkModuleName(arg)) {
+        throw new Error(
+          "SmartStructure name must be only a-zA-Z0-9_ , the current value is not valid : " +
+            arg
+        );
+      }
+      return arg;
+    }
+  },
+  vendorName: {
+    defaultDescription: "vendor name of the module",
+    alias: "v",
+    type: "string",
+    coerce: arg => {
+      if (!checkVendorName(arg)) {
+        throw new Error(
+          "Vendor name must be only a-zA-Z0-9_ , the current value is not valid : " +
+            arg
+        );
+      }
+      return arg;
+    }
+  },
+  parentName: {
+    defaultDescription: "parent name of the smartStructure",
+    alias: "p",
+    type: "string",
+    default: "",
+    coerce: arg => {
+      if (!checkVendorName(arg)) {
+        if (!arg) {
+          return arg;
+        }
+        throw new Error(
+          "parent name must be only a-zA-Z0-9_ , the current value is not valid : " +
+            arg
+        );
+      }
+      return arg;
+    }
+  },
+  smartStructurePath: {
+    defaultDescription: "path where the smart structure will be added",
+    type: "string",
+    default: "",
+    coerce: arg => {
+      if (!arg) {
+        return arg;
+      }
+      if (!fs.statSync(arg).isDirectory()) {
+        throw new Error("Unable to find the smart structure directory " + arg);
+      }
+      return arg;
+    }
+  },
+  inSelfDirectory: {
+    defaultDescription:
+      "add a directory for the new smart structure (not compatible with smartStructurePath)",
+    default: true,
+    type: "boolean"
+  },
+  withRender: {
+    defaultDescription: "add renders class",
+    default: true,
+    type: "boolean"
+  },
+  withClass: {
+    defaultDescription: "add a class",
+    default: true,
+    type: "boolean"
+  },
+  insertIntoInfo: {
+    defaultDescription: "Insert into info.xml",
+    default: true,
+    type: "boolean"
+  }
+};
+exports.builder = builder;
+
+exports.handler = async argv => {
+  if (!argv.name) {
+    // Mode question
+    argv = await inquirer.prompt(
+      Object.keys(builder).map(currentKey => {
+        const currentParam = builder[currentKey];
+        return {
+          type: currentParam.type === "boolean" ? "confirm" : "input",
+          name: currentKey,
+          message: `${currentParam.defaultDescription} : `,
+          default: currentParam.default,
+          validate: currentParam.coerce
+            ? arg => {
+                try {
+                  currentParam.coerce(arg);
+                } catch (e) {
+                  return e.message;
+                }
+                return true;
+              }
+            : () => true
+        };
+      })
+    );
+  }
+  try {
+    signale.time("createSmartStructure");
+    createSmartStructure(argv);
+    const task = gulp.task("createSmartStructure");
+    task()
+      .then(() => {
+        signale.timeEnd("createSmartStructure");
+        signale.success("createSmartStructure done");
+      })
+      .catch(e => {
+        signale.timeEnd("createSmartStructure");
+        signale.error(e);
+      });
+  } catch (e) {
+    signale.timeEnd("createSmartStructure");
+    signale.error(e);
+  }
+};
---
+++ tasks/createSmartStructure.js
@@ -0,0 +1,364 @@
+const gulp = require("gulp");
+const { getModuleInfo } = require("../utils/moduleInfo");
+const xml2js = require("xml2js");
+const fs = require("fs");
+const path = require("path");
+const appConst = require("../utils/appConst");
+
+const convertPathInPhpNamespace = ({ vendorPath, smartStructurePath }) => {
+  return path
+    .relative(vendorPath, smartStructurePath)
+    .split(path.sep)
+    .join("\\");
+};
+
+const generateSmartStructureXML = ({
+  name,
+  parentName,
+  withClass,
+  namespace
+}) => {
+  //<smart:structure-configuration name="IUSER" id="128" label="Utilisateur">
+  const structureConf = {
+    "smart:config": {
+      $: {
+        "xmlns:smart": "http://www.anakeen.com/ns/smart/1.0"
+      },
+      "smart:structure-configuration": {
+        $: {
+          name
+        },
+        "smart:icon": {
+          $: {
+            file: `${name}.png`
+          }
+        }
+      }
+    }
+  };
+  if (parentName) {
+    structureConf["smart:config"]["smart:structure-configuration"][
+      "$"
+    ].extends = parentName;
+  }
+  if (withClass) {
+    structureConf["smart:config"]["smart:structure-configuration"][
+      "smart:class"
+    ] = `${namespace}`;
+  }
+  structureConf["smart:config"]["smart:structure-configuration"][
+    "smart:fields"
+  ] = {};
+  structureConf["smart:config"]["smart:structure-configuration"][
+    "smart:hooks"
+  ] = {};
+  structureConf["smart:config"]["smart:structure-configuration"][
+    "smart:defaults"
+  ] = {};
+  return structureConf;
+};
+
+const structurePhp = ({ name, namespace }) => {
+  return `<?php
+
+namespace ${namespace};
+
+use SmartStructure\\Fields\\${name} as ${name}Fields;
+
+class ${name} extends \\SmartStructure\\${name}
+{
+
+    public function registerHooks()
+    {
+        parent::registerHooks();
+    }
+}
+`;
+};
+
+const generateRenderAccess = ({ name, namespace }) => {
+  return `<?php
+
+namespace ${namespace};
+
+class ${name}Access implements \\Dcp\\Ui\\IRenderConfigAccess
+{
+    /**
+     * @param string $mode
+     * @return \\Dcp\\Ui\\IRenderConfig
+     */
+    public function getRenderConfig($mode, \\Anakeen\\Core\\Internal\\SmartElement $document)
+    {
+        switch ($mode) {
+            case \\Dcp\\Ui\\RenderConfigManager::CreateMode:
+            case \\Dcp\\Ui\\RenderConfigManager::EditMode:
+                return new ${name}EditRender();
+            case \\Dcp\\Ui\\RenderConfigManager::ViewMode:
+                return new ${name}ViewRender();
+        }
+        return null;
+    }
+}
+`;
+};
+
+const generateRender = ({ name, namespace, type = "View" }) => {
+  return `<?php
+
+namespace ${namespace};
+
+use SmartStructure\\Fields\\${name} as ${name}Fields;
+
+class ${name}${type}Render extends \\Anakeen\\Ui\\DefaultConfig${type}Render
+{
+
+}`;
+};
+
+exports.createSmartStructure = ({
+  sourcePath,
+  name,
+  inSelfDirectory = true,
+  withRender = true,
+  withClass = true,
+  parentName = false,
+  insertIntoInfo = true,
+  smartStructurePath,
+  vendorName
+}) => {
+  return gulp.task("createSmartStructure", async () => {
+    //Get module info
+    const moduleInfo = await getModuleInfo(sourcePath);
+    //Check the path for the xml file
+    if (!vendorName) {
+      vendorName = moduleInfo.vendorName;
+    }
+    let srcPath;
+    let vendorPath;
+    let xmlPath;
+    if (!smartStructurePath && !vendorName) {
+      throw new Error(
+        "You need to specify a vendor name or smartStructurePath"
+      );
+    }
+    if (!smartStructurePath && vendorName) {
+      //Compute and test the smartStructurePath for the vendor name
+      srcPath = moduleInfo.buildInfo.buildPath.find(currentPath => {
+        //Check current path
+        const smartPath = path.join(
+          currentPath,
+          "vendor",
+          vendorName,
+          "SmartStructures"
+        );
+        try {
+          return fs.statSync(smartPath).isDirectory();
+        } catch (e) {
+          return false;
+        }
+      });
+      if (!srcPath) {
+        throw new Error(
+          `Unable to find a smartStructure path for this vendor (${vendorName}), you should create it or indicate the smartStructurePath`
+        );
+      }
+      smartStructurePath = path.join(
+        srcPath,
+        "vendor",
+        vendorName,
+        "SmartStructures"
+      );
+      vendorPath = path.join(srcPath, "vendor");
+    }
+    //Create the directory if needed
+    let directoryPromise = Promise.resolve(smartStructurePath);
+    if (inSelfDirectory) {
+      const smartStructureDirectory = path.join(smartStructurePath, name);
+      directoryPromise = new Promise((resolve, reject) => {
+        fs.mkdir(smartStructureDirectory, err => {
+          if (err) {
+            reject(err);
+          }
+          resolve(smartStructureDirectory);
+        });
+      });
+    }
+    return directoryPromise
+      .then(currentPath => {
+        return new Promise((resolve, reject) => {
+          //Build the xml
+          const builder = new xml2js.Builder();
+          const xml = builder.buildObject(
+            generateSmartStructureXML({
+              name,
+              parentName,
+              withClass,
+              namespace: convertPathInPhpNamespace({
+                vendorPath,
+                smartStructurePath: currentPath
+              })
+            })
+          );
+          //Write the xml
+          xmlPath = path.join(currentPath, `${name}SmartStructure.xml`);
+          fs.writeFile(xmlPath, xml, err => {
+            if (err) {
+              return reject(err);
+            }
+            resolve(currentPath);
+          });
+        });
+      })
+      .then(currentPath => {
+        //Write the php if needed
+        if (withClass) {
+          return new Promise((resolve, reject) => {
+            const structurePHP = structurePhp({
+              name,
+              namespace: convertPathInPhpNamespace({
+                vendorPath,
+                smartStructurePath: currentPath
+              })
+            });
+            fs.writeFile(
+              path.join(currentPath, `${name}SmartStructure.php`),
+              structurePHP,
+              err => {
+                if (err) {
+                  return reject(err);
+                }
+                resolve(currentPath);
+              }
+            );
+          });
+        }
+      })
+      .then(currentPath => {
+        //with render
+        if (withRender) {
+          //Generate the render access and two renders (one view, one edit)
+
+          return new Promise((resolve, reject) => {
+            const renderPath = path.join(currentPath, "Render");
+            let createDirPromise;
+            let renderPathExist;
+            try {
+              renderPathExist = fs.statSync(renderPath).isDirectory();
+            } catch (e) {
+              renderPathExist = false;
+            }
+            if (renderPathExist) {
+              createDirPromise = Promise.resolve();
+            } else {
+              createDirPromise = new Promise((resolve, reject) => {
+                fs.mkdir(renderPath, err => {
+                  if (err) {
+                    return reject(err);
+                  }
+                  resolve();
+                });
+              });
+            }
+            return createDirPromise.then(() => {
+              const renderAccess = generateRenderAccess({
+                name,
+                namespace: convertPathInPhpNamespace({
+                  vendorPath,
+                  smartStructurePath: renderPath
+                })
+              });
+              fs.writeFile(
+                path.join(renderPath, `${name}Access.php`),
+                renderAccess,
+                err => {
+                  if (err) {
+                    return reject(err);
+                  }
+                  const generateRenderView = generateRender({
+                    name,
+                    namespace: convertPathInPhpNamespace({
+                      vendorPath,
+                      smartStructurePath: renderPath
+                    })
+                  });
+                  fs.writeFile(
+                    path.join(renderPath, `${name}ViewRender.php`),
+                    generateRenderView,
+                    err => {
+                      if (err) {
+                        return reject(err);
+                      }
+                      const generateRenderEdit = generateRender({
+                        name,
+                        namespace: convertPathInPhpNamespace({
+                          vendorPath,
+                          smartStructurePath: renderPath
+                        }),
+                        type: "Edit"
+                      });
+                      fs.writeFile(
+                        path.join(renderPath, `${name}EditRender.php`),
+                        generateRenderEdit,
+                        err => {
+                          if (err) {
+                            return reject(err);
+                          }
+                          resolve();
+                        }
+                      );
+                    }
+                  );
+                }
+              );
+            });
+          });
+        }
+      })
+      .then(() => {
+        //Complete the info.xml if needed
+        if (insertIntoInfo) {
+          const infoXMLPath = path.join(sourcePath, appConst.infoPath);
+          const parser = new xml2js.Parser();
+          return new Promise((resolve, reject) => {
+            fs.readFile(infoXMLPath, { encoding: "utf8" }, (err, content) => {
+              if (err) {
+                return reject(err);
+              }
+              parser.parseString(content, (err, data) => {
+                if (err) {
+                  return reject(err);
+                }
+                //Add the xml to the postInstall and the postUpgrade tags
+                const postInstall = data.module["post-install"];
+                const postUpgrade = data.module["post-upgrade"];
+                postInstall[0].process.push({
+                  $: {
+                    command: `./ank.php --script=importDocuments --file=./${path.relative(
+                      srcPath,
+                      xmlPath
+                    )}`
+                  }
+                });
+                postUpgrade[0].process.push({
+                  $: {
+                    command: `./ank.php --script=importDocuments --file=./${path.relative(
+                      srcPath,
+                      xmlPath
+                    )}`
+                  }
+                });
+                const builder = new xml2js.Builder();
+                fs.writeFile(infoXMLPath, builder.buildObject(data), err => {
+                  if (err) {
+                    reject(err);
+                  }
+                  resolve();
+                });
+                //postInstall[0];
+              });
+            });
+          });
+        }
+      });
+  });
+};
