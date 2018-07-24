05cde36b48cff15fd8e4fe2717189fd3cd3b4a7c
david <david.dang@anakeen.com>
2018-07-24 10:29:22 +0200

    fix(Route): remove unused import

--- admin-center/src/vendor/Anakeen/AdminCenter/Routes/routePluginMain.js
+++ admin-center/src/vendor/Anakeen/AdminCenter/Routes/routePluginMain.js
@@ -8,7 +8,6 @@ import "@progress/kendo-ui/js/kendo.treelist";
 import "@progress/kendo-ui/js/kendo.window";
 import "@progress/kendo-ui/js/kendo.button";
 import "@progress/kendo-ui/js/kendo.dialog";
-import VueAdminSetup from "../Vue/vueCommonConfig.js";
 import installVuePlugin from "../Vue/installVuePlugin";
 
 installVuePlugin(Vue, "ank-admin-routes", RoutePlugin);
