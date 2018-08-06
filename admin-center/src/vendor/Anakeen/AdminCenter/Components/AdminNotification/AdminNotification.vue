a11048c65b917351f9f6f7063965d748dfe9b3cc
Aurelien Gaillard <aurelien.gaillard@anakeen.com>
2018-08-06 15:03:47 +0200

    feat(Notifier): Remove Admin notifier to use ank-notifier component

--- admin-center/src/vendor/Anakeen/AdminCenter/Components/AdminNotification/AdminNotification.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="admin-center-notification">
-        <div ref="notifications"></div>
-    </div>
-</template>
-<style lang="scss">
-    @import "./AdminNotification.scss";
-</style>
-<script src="./AdminNotification.controller.js"></script>
