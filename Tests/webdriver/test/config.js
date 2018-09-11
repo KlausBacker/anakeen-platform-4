87127b187d6ad5e95a0b5389ab85695c1b99ec32
Charles Bonnissent <charles.bonnissent@anakeen.com>
2018-09-11 12:24:18 +0200

    feat(autotest): add first batch

---
+++ Tests/webdriver/test/config.js
@@ -0,0 +1,46 @@
+var config = require("../wdio.conf.js").config;
+const axios = require("axios");
+
+exports.login = ({ browser, login, password }) => {
+  return new Promise((resolve, reject) => {
+    browser.url("/login/");
+    axios({
+      method: "post",
+      url: `${config.baseUrl}/api/v2/authent/sessions/${login}`,
+      data: {
+        password: password
+      }
+    })
+      .then(response => {
+        if (response.headers["set-cookie"]) {
+          const promises = [];
+          response.headers["set-cookie"].forEach(currentCookie => {
+            const cookiesElement = currentCookie.split(";");
+            const cookiePart = cookiesElement[0].split("=");
+            promises.push(
+              browser.setCookie({
+                name: cookiePart[0],
+                value: cookiePart[1]
+              })
+            );
+            Promise.all(promises)
+              .then(resolve)
+              .catch(reject);
+          });
+        } else {
+          reject("Login cookie not found");
+        }
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+};
+
+exports.setWindowSize = ({ browser, width = 1024, height = 768 }) => {
+  browser.url("/");
+  browser.setViewportSize({
+    width,
+    height
+  });
+};
