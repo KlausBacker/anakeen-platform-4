bfb179450c83bc976b50bc01334586965a0bf387
Éric Brison <eric.brison@anakeen.com>
2018-09-19 09:05:42 +0200

    fix(database): add unaccent extension

--- Tests/autotest/scripts.d/02_start_services.sh
+++ Tests/autotest/scripts.d/02_start_services.sh
@@ -5,5 +5,4 @@ set -eo pipefail
 service apache2 restart
 service postgresql restart
 
-sudo -u postgres psql "dynacase"  -c "CREATE EXTENSION IF NOT EXISTS unaccent"
-
+su - postgres -c 'psql "dynacase"  -c "CREATE EXTENSION IF NOT EXISTS unaccent"'
