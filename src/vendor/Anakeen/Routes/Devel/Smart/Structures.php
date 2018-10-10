7da3a74f59601502d001e76cc4482b3228fc6b07
Éric Brison <eric.brison@anakeen.com>
2018-10-10 11:33:22 +0200

    feat(route): add structure route

---
+++ src/vendor/Anakeen/Routes/Devel/Smart/Structures.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Anakeen\Routes\Devel\Smart;
+
+use Anakeen\Core\DbManager;
+use Anakeen\Router\ApiV2Response;
+use Anakeen\Ui\DataSource;
+
+/**
+ * Get All Structures
+ *
+ * @note    Used by route : GET /api/v2/devel/smart/structures/vendor/
+ * @note    Used by route : GET /api/v2/devel/smart/structures/all/
+ */
+class Structures
+{
+    const ENUMPAGESIZE = 50;
+    protected $target="all";
+    protected $filters = [];
+    protected $slice = self::ENUMPAGESIZE;
+    protected $offset = 0;
+
+    public function __invoke(\Slim\Http\request $request, \Slim\Http\response $response, $args)
+    {
+        $this->initParameters($request, $args);
+        return ApiV2Response::withData($response, $this->doRequest());
+    }
+
+    protected function initParameters(\Slim\Http\request $request, $args)
+    {
+        $this->target = $args["target"];
+    }
+
+    public function doRequest()
+    {
+        $data = [];
+
+        $s=new \SearchDoc("", -1);
+        if ($this->target === "vendor") {
+            $s->addFilter("atags is null or atags->>'vendor' <> 'Anakeen'");
+        }
+        $s->setOrder("name, id");
+        $s->setObjectReturn();
+        $dl=$s->search()->getDocumentList();
+        $structData=[];
+        foreach ($dl as $structure) {
+            $structData[]=[
+                "id"=>intval($structure->id),
+                "name"=>$structure->name,
+                "title"=>$structure->getTitle(),
+                "icon"=>$structure->getIcon("", 32)
+            ];
+        }
+        $data=$structData;
+        return $data;
+    }
+}
