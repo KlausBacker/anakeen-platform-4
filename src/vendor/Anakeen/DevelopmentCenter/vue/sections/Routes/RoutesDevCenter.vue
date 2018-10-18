82bcdb14ff39d53196e56d846561abee15e4e53a
david <david.dang@anakeen.com>
2018-10-18 02:00:16 +0200

    feat(Routes): add security Routes sub section

---
+++ src/vendor/Anakeen/DevelopmentCenter/vue/sections/Routes/RoutesDevCenter.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="routes-parent">
+        <nav class="routes-nav">
+            <router-link :to="{name: 'Security::Routes::RoutesAcl'}">Access Control</router-link>
+            <router-link :to="{name: 'Security::Routes::RoutesPermissions'}">Permissions</router-link>
+        </nav>
+        <router-multi-view class="routes-content"></router-multi-view>
+    </div>
+</template>
+<!-- CSS to this component only -->
+<style lang="scss">
+    @import "./RoutesDevCenter.scss";
+</style>
