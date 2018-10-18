d2a41ccfd7dcbeb93473c78d992916ff658eb940
david <david.dang@anakeen.com>
2018-10-18 14:26:35 +0200

    feat(Routes): column method width

--- src/vendor/Anakeen/DevelopmentCenter/vue/sections/Routes/RoutesAcl/RoutesAcl.vue
+++ src/vendor/Anakeen/DevelopmentCenter/vue/sections/Routes/RoutesAcl/RoutesAcl.vue
@@ -21,7 +21,7 @@
                     :sortable="true">
             <kendo-grid-column :field="'nameSpace'" :title="'<b>Namespace</b>'" :filterable-cell-operator="'contains'" :filterable-cell-show-operators="false" :filterable-cell-template="autoFilterCol"></kendo-grid-column>
             <kendo-grid-column :field="'name'" :title="'<b>Name</b>'" :property="true" :filterable-cell-operator="'contains'" :filterable-cell-show-operators="false" :filterable-cell-template="autoFilterCol"></kendo-grid-column>
-            <kendo-grid-column :field="'method'" :title="'<b>Method</b>'" :property="true" :filterable-cell-operator="'contains'" :filterable-cell-show-operators="false" :filterable-cell-template="autoFilterCol"></kendo-grid-column>
+            <kendo-grid-column :field="'method'" :title="'<b>Method</b>'" :width="'8rem'" :property="true" :filterable-cell-operator="'contains'" :filterable-cell-show-operators="false" :filterable-cell-template="autoFilterCol"></kendo-grid-column>
             <kendo-grid-column :field="'pattern'" :title="'<b>Pattern</b>'" :property="true" :filterable-cell-operator="'contains'" :filterable-cell-show-operators="false" :filterable-cell-template="autoFilterCol"></kendo-grid-column>
             <kendo-grid-column :field="'requiredAccess'" :title="'<b>Required Accesses</b>'" :property="true" :filterable-cell-operator="'contains'" :filterable-cell-show-operators="false" :template="displayMultiple" :filterable-cell-template="autoFilterCol"></kendo-grid-column>
             <kendo-grid-column :field="'decscription'" :title="'<b>Description</b>'" :property="true" :filterable-cell-operator="'contains'" :filterable-cell-show-operators="false" :filterable-cell-template="autoFilterCol"></kendo-grid-column>
