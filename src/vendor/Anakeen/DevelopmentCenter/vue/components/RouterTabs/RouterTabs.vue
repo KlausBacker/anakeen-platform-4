6b7b6cd914cfc74f831434e28a2fd2a612bb1821
Aurélien Brugalières <aurelien.brugalieres@anakeen.com>
2018-10-25 18:18:54 +0200

    fix(router): fix router link tag to preserve link navigation

--- src/vendor/Anakeen/DevelopmentCenter/vue/components/RouterTabs/RouterTabs.vue
+++ src/vendor/Anakeen/DevelopmentCenter/vue/components/RouterTabs/RouterTabs.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="router-tabs">
         <nav class="router-tabs-nav">
-            <router-link class="router-tab-nav-link" v-for="item in items" :key="`router-tab-nav-${item.name}`" tag="div" :to="{name: item.name}">{{item.label}}</router-link>
+            <router-link class="router-tab-nav-link" v-for="item in items" :key="`router-tab-nav-${item.name}`" :to="{name: item.name}">{{item.label}}</router-link>
         </nav>
         <router-multi-view class="router-tabs-content"></router-multi-view>
     </div>
