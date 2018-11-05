91f057dc19fd14b8fe30fd57824d57c9cadfe272
Aurélien Brugalières <aurelien.brugalieres@anakeen.com>
2018-11-05 15:30:16 +0100

    feat(infos): add smart structure infos tab

--- src/vendor/Anakeen/DevelopmentCenter/vue/sections/SmartStructures/Infos.vue
+++ src/vendor/Anakeen/DevelopmentCenter/vue/sections/SmartStructures/Infos.vue
@@ -1,32 +1,106 @@
 <template>
-    <div>
-        <h1>Infos for {{ssName}}</h1>
-        <smart-element ref="smartElement" :initid="ssName"></smart-element>
+    <div class="smart-structure-infos-section" v-if="isReady">
+        <h1 class="smart-structure-title"><img class="smart-structure-icon" :src="structureProperties.icon"/> {{ssName}} ({{structureProperties.title}}) Overview</h1>
+        <div class="smart-structure-infos-content">
+            <div class="smart-structure-infos-card">
+                <div class="infos-card-title">Hierarchy</div>
+                <div class="infos-card-content">
+                    <structure-hierarchy :currentStructure="ssName" :data="structureHierarchy"></structure-hierarchy>
+                </div>
+            </div>
+            <div class="smart-structure-infos-card">
+                <div class="infos-card-title">Structure</div>
+                <div class="infos-card-content">
+                    <div class="infos-card-content-item">
+                        <span class="card-content-item-label">Number of fields (Scalar fields / All fields):</span>
+                        <span class="card-content-item-value">{{structureInfo.fields.leafCount}} / {{structureInfo.fields.count}}</span>
+                    </div>
+                    <div class="infos-card-content-item">
+                        <span class="card-content-item-label">Number of parameters (Scalar parameters / All parameters): </span>
+                        <span class="card-content-item-value">{{structureInfo.parameters.leafCount}} / {{structureInfo.parameters.count}}</span>
+                    </div>
+                </div>
+            </div>
+            <div class="smart-structure-infos-card">
+                <div class="infos-card-title">Elements</div>
+                <div class="infos-card-content">
+                    <div class="infos-card-content-item">
+                        <span class="card-content-item-label">Classname :</span>
+                        <span class="card-content-item-value">{{structureDetails.className}}</span>
+                    </div>
+                    <div class="infos-card-content-item">
+                        <span class="card-content-item-label">Number of Smart Elements (with structures children): </span>
+                        <span class="card-content-item-value">{{structureInfo.elements.count}} ({{structureInfo.elements.countWithChilds}})</span>
+                    </div>
+                    <div class="infos-card-content-item">
+                        <span class="card-content-item-label">Is revisable :</span>
+                        <span class="card-content-item-value">{{structureInfo.revision.isRevisable ? "Yes" : "No"}}</span>
+                        <span class="card-content-item-value" v-if="structureInfo.revision.isRevisable">(max revision: {{structureInfo.revision.maxRevision ? structureInfo.revision.maxRevision : "&infin;"}})</span>
+                    </div>
+                </div>
+            </div>
+            <div class="smart-structure-infos-card">
+                <div class="infos-card-title">Tags</div>
+                <div class="infos-card-content">
+                    <div v-html="formatTags(structureDetails.tags)"></div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
-<script>
-    import Vue from "vue";
-    import { AnkSmartElement } from "@anakeen/ank-components";
-    Vue.use(AnkSmartElement);
-  export default {
-    name: "Infos",
-    props: ["ssName"],
-    components: {
-      "smart-element": AnkSmartElement
-    },
-    watch: {
-      ssName(newValue, oldValue) {
-        if (newValue !== oldValue) {
-          if (this.$refs.smartElement.isLoaded()) {
-            this.$refs.smartElement.fetchSmartElement({initid: newValue});
-          }
-        }
-      }
-    }
-  }
+<script src="./Infos.controller.js">
+
 </script>
 
-<style scoped>
+<style lang="scss">
+    .smart-structure-infos-section {
+        display: flex;
+        flex-direction: column;
+        flex: 1;
+        padding: 1rem;
+        .smart-structure-title {
+            display: flex;
+            align-items: center;
+            .smart-structure-icon {
+                margin-right: 1rem;
+            }
+        }
 
+        .smart-structure-infos-content {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            flex: 1;
+            overflow-y: auto;
+            .smart-structure-infos-card {
+
+                .infos-card-title {
+                    font-size: 1.5rem;
+                    margin-bottom: 1rem;
+                }
+
+                & + .smart-structure-infos-card {
+                    /*margin-left: 1rem;*/
+                }
+                margin: auto;
+                padding: .5rem;
+                border: 1px solid rgb(210, 210, 210);
+                display: inline-block;
+                box-shadow: 3px 3px rgba(210, 210, 210, 0.5);
+                height: 40%;
+                width: 45%;
+                min-width: 30%;
+
+                .infos-card-content {
+                    height: calc(100% - 2.8rem);
+                    font-size: 1.1rem;
+                    .card-content-item-label {
+                        font-weight: bold;
+                        margin-right: .5rem;
+                    }
+                }
+            }
+        }
+    }
 </style>
---
+++ src/vendor/Anakeen/DevelopmentCenter/vue/sections/SmartStructures/StructureHierarchy.vue
@@ -0,0 +1,266 @@
+<template>
+    <div class="smart-structure-hierarchy">
+        <svg class="smart-structure-hierarchy-graph" ref="treeSvg">
+            <defs>
+                <marker id="arrowhead" viewBox="0 0 10 10" refX="10" refY="5" markerWidth="10" markerHeight="10" orient="auto-start-reverse">
+                    <path d="M 0 0 L 10 5 L 0 10 z" />
+                </marker>
+            </defs>
+        </svg>
+    </div>
+</template>
+<!-- CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
+<!-- Global CSS -->
+<style lang="scss">
+    .smart-structure-hierarchy {
+        width: 100%;
+        height: 100%;
+        .smart-structure-hierarchy-graph {
+            width: 100%;
+            height: 100%;
+
+            .node rect {
+                cursor: pointer;
+                fill: #fff;
+                fill-opacity: 0.5;
+                stroke: #3182bd;
+                stroke-width: 1.5px;
+            }
+
+            .node text {
+                font: 10px sans-serif;
+                pointer-events: none;
+            }
+
+            .link {
+                fill: none;
+                stroke: #9ecae1;
+                stroke-width: 1.5px;
+            }
+
+        }
+    }
+</style>
+<script>
+    import * as d3 from "d3";
+    // Creates a curved (diagonal) path from parent to the child nodes
+    const diagonal = d3.linkHorizontal().x(d => d.y).y(d => d.x);
+    const color = (vm) => (d) => {
+      if (d.data.name === vm.currentStructure) {
+        return "#fd8d3c";
+      }
+      return "#c6dbef";
+    }
+    export default {
+        props: {
+          data: {
+            type: Array,
+            default: () => ([])
+          },
+          currentStructure: {
+            type: String,
+            required: true
+          }
+        },
+      mounted() {
+        this.initGraph(this.$refs.treeSvg);
+        if (!this.empty) {
+          this.normalizeData();
+          this.updateGraph(this.root);
+        }
+      },
+      updated() {
+        if (!this.empty) {
+          this.normalizeData();
+          this.resizeAndUpdateTree(this.$refs.treeSvg);
+        }
+      },
+      beforeDestroy() {
+        window.removeEventListener('resize', () => {
+          this.resizeAndUpdateTree(this.$refs.treeSvg);
+        });
+      },
+      data() {
+        return {
+          root: {},
+          tree: {},
+          svg: {},
+          duration: 0,
+          i: 0,
+        };
+      },
+      computed: {
+        empty() {
+          return !this.data || !this.data.length;
+        },
+        dataEmptyText() {
+          return this.emptyText || '';
+        },
+      },
+      methods: {
+        normalizeData() {
+          this.reinitNodes();
+          if (!this.empty) {
+            this.root = this.data[0];
+          }
+          this.root = d3.hierarchy(this.root, d => d.children);
+          this.root.x0 = 50;
+          this.root.y0 = 10;
+        },
+        reinitNodes() {
+          this.root = {
+            name: 'Hierarchy',
+            x0: 50,
+            y0: 10,
+            children: [],
+          };
+        },
+        initGraph(element) {
+          let { width, height } = this.$el.getBoundingClientRect();
+          console.log(width, height);
+          const margin = { top: 0, right: 90, bottom: 90, left: 90 };
+          width = width - margin.left - margin.right;
+          height = height - margin.top - margin.bottom;
+
+          // append the svg object to the body of the page
+          // appends a 'group' element to 'svg'
+          // moves the 'group' element to the top left margin
+          let translateValue = `translate(${margin.left}, 0)`;
+          this.svg = d3.select(element)
+            .attr('width', width + margin.left)
+            .attr('height', height + margin.bottom)
+            .call(d3.zoom().scaleExtent([0, 100]).on('zoom', () => {
+              this.svg.attr('transform', `translate( ${d3.event.transform.x},${d3.event.transform.y} )`);
+            }))
+            .on('dblclick.zoom', null)
+            .append('g')
+            .attr('transform', translateValue);
+
+          const realSize = {
+            width: height,
+            height: width,
+          };
+
+          // declares a tree layout and assigns the size
+          this.tree = d3.tree().size([realSize.width, realSize.height]);
+          window.addEventListener('resize', () => {
+            this.resizeAndUpdateTree(this.$refs.treeSvg);
+          });
+        },
+        updateGraph(source) {
+          // Compute the flattened node list.
+          var nodes = this.root.descendants();
+
+
+          // Compute the "layout". TODO https://github.com/d3/d3-hierarchy/issues/67
+          var index = -1;
+          this.root.eachBefore(function(n) {
+            ++index;
+            n.x = (index * 25) + 20;
+            n.y = n.depth * 50;
+          });
+
+          // Update the nodes…
+          var node = this.svg.selectAll(".node")
+            .data(nodes, function(d) {
+              this.i += 1;
+              return d.id || (d.id = this.i);
+            });
+
+          var nodeEnter = node.enter().append("g")
+            .attr("class", "node")
+            .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
+            .style("opacity", 0)
+            .on("click", this.onClickNode)
+
+          // Enter any new nodes at the parent's previous position.
+          nodeEnter.append("rect")
+            .attr("y", -20 / 2)
+            .attr("height", 20)
+            .attr("width", 150)
+            .style("fill", color(this));
+
+          nodeEnter.append("text")
+            .attr("dy", 3.5)
+            .attr("dx", 5.5)
+            .text(function(d) { return d.data.name; });
+
+          // Transition nodes to their new position.
+          nodeEnter.transition()
+            .duration(this.duration)
+            .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
+            .style("opacity", 1);
+
+          node.transition()
+            .duration(this.duration)
+            .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
+            .style("opacity", 1)
+            .select("rect")
+            .style("fill", color(this));
+
+          // Transition exiting nodes to the parent's new position.
+          node.exit().transition()
+            .duration(this.duration)
+            .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
+            .style("opacity", 0)
+            .remove();
+
+          // Update the links…
+          var link = this.svg.selectAll(".link")
+            .data(this.root.links(), function(d) { return d.target.id; });
+
+          // Enter any new links at the parent's previous position.
+          link.enter().insert("path", "g")
+            .attr("class", "link")
+            .attr("d", function(d) {
+              var o = {x: source.x0, y: source.y0};
+              return diagonal({source: o, target: o});
+            })
+            .transition()
+            .duration(this.duration)
+            .attr("d", diagonal);
+
+          // Transition links to their new position.
+          link.transition()
+            .duration(this.duration)
+            .attr("d", diagonal);
+
+          // Transition exiting nodes to the parent's new position.
+          link.exit().transition()
+            .duration(this.duration)
+            .attr("d", function(d) {
+              var o = {x: source.x, y: source.y};
+              return diagonal({source: o, target: o});
+            })
+            .remove();
+
+          // Stash the old positions for transition.
+          this.root.each(function(d) {
+            d.x0 = d.x;
+            d.y0 = d.y;
+          });
+        },
+        onClickNode(d) {
+          console.log(d);
+          this.$router.push({ name: "SmartStructures::name", params: { ssName: d.data.name }})
+        },
+        resizeAndUpdateTree(element) {
+          if (element) {
+            const { width, height } = this.$el.getBoundingClientRect();
+            const realSize = {
+              width: height,
+              height: width,
+            };
+            this.tree.size([realSize.width, realSize.height]);
+            this.svg.attr('width', width).attr('height', height);
+            if (!this.empty) {
+              this.updateGraph(this.root);
+            }
+          }
+        },
+      },
+    }
+</script>
