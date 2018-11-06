d6f6ca9bcfafa02eb6ed6e6ea9162bd5a3cf1821
david <david.dang@anakeen.com>
2018-11-06 16:55:59 +0100

    fix(treeList): remove unused watch ssName

--- src/vendor/Anakeen/DevelopmentCenter/vue/sections/Ui/Infos/Infos.controller.js
+++ src/vendor/Anakeen/DevelopmentCenter/vue/sections/Ui/Infos/Infos.controller.js
@@ -1,18 +1,6 @@
 export default {
   name: "informations",
   props: ["ssName"],
-  watch: {
-    ssName(newValue, oldValue) {
-      if (newValue !== oldValue) {
-        this.$http.get(this.url).then(response => {
-          const ui = response.data.data.ui;
-          this.mskfamid = response.data.data.msk["msk_famid"];
-          this.cvdoc = ui.ccvid || {};
-          this.renderaccess = ui.render || {};
-        });
-      }
-    }
-  },
   data() {
     return {
       cvdoc: "",
