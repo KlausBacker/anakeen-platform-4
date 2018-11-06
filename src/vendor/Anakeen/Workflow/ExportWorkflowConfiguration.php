7d9c25f07b26156c896964a8e5204d92210a8c04
Éric Brison <eric.brison@anakeen.com>
2018-11-06 14:56:29 +0100

    feat(config): continue export workflow config

--- src/vendor/Anakeen/Workflow/ExportWorkflowConfiguration.php
+++ src/vendor/Anakeen/Workflow/ExportWorkflowConfiguration.php
@@ -2,8 +2,10 @@
 
 namespace Anakeen\Workflow;
 
+use Anakeen\Core\Internal\ContextParameterManager;
 use Anakeen\Core\SEManager;
 use Anakeen\SmartStructures\Wdoc\WDocHooks;
+use SmartStructure\Fields\Mailtemplate as MailFields;
 
 class ExportWorkflowConfiguration extends \Anakeen\Core\SmartStructure\ExportConfigurationAccesses
 {
@@ -37,6 +39,7 @@ class ExportWorkflowConfiguration extends \Anakeen\Core\SmartStructure\ExportCon
 
     protected function extract($structConfig)
     {
+        $this->extractMailTemplatesData($this->domConfig);
         $this->extractAccess($this->domConfig);
         $this->extractConfig($this->domConfig);
     }
@@ -110,6 +113,171 @@ class ExportWorkflowConfiguration extends \Anakeen\Core\SmartStructure\ExportCon
         $config->appendChild($transitionsNode);
 
         $structConfig->appendChild($config);
+    }
+
+
+    protected function extractMailTemplatesData(\DOMElement $structConfig)
+    {
+        $steps = $this->workflow->getStates();
+        foreach ($steps as $step) {
+            $mails = $this->workflow->getStateMailTemplate($step);
+            foreach ($mails as $mail) {
+                $structConfig->appendChild($this->getMailTemplateData($mail));
+            }
+        }
+
+        foreach ($this->workflow->transitions as $transitionName => $transitionConfig) {
+            $mails = $this->workflow->getTransitionMailTemplates($transitionName);
+            foreach ($mails as $mail) {
+                $structConfig->appendChild($this->getMailTemplateData($mail));
+            }
+        }
+    }
+
+    protected function getMailTemplateData($name)
+    {
+        $mail = SEManager::getDocument($name);
+        $mailNode = $this->celw("mailtemplate");
+
+        $mailNode->setAttribute("name", static::getLogicalName($name));
+        $mailNode->setAttribute("label", $mail->getRawValue(MailFields::tmail_title));
+
+        $fromNode = $this->celw("from");
+
+        $fromType = $mail->getMultipleRawValues(MailFields::tmail_fromtype);
+        $from = $mail->getMultipleRawValues(MailFields::tmail_from);
+
+        if ($from) {
+            $fromNode->appendChild($this->getRecipient($fromType[0], $from[0]));
+        }
+
+        $nodeRecipients = $this->celw("recipients");
+        $recips = $mail->getMultipleRawValues(MailFields::tmail_recip);
+        $destTypes = $mail->getMultipleRawValues(MailFields::tmail_desttype);
+        $copyModes = $mail->getMultipleRawValues(MailFields::tmail_copymode);
+        foreach ($recips as $k => $recip) {
+            $nodeRecipient = $this->celw("recipient");
+            $nodeRecipient->setAttribute("dest", $copyModes[$k]);
+            $recipient = $this->getRecipient($destTypes[$k], $recip);
+            $nodeRecipient->appendChild($recipient);
+            $nodeRecipients->appendChild($nodeRecipient);
+        }
+        $nodeSubject = $this->celw("subject");
+        $nodeSubject->nodeValue = $mail->getRawValue(MailFields::tmail_subject);
+
+        $nodeSave = $this->celw("savecopy");
+        $nodeSave->nodeValue = ($mail->getRawValue(MailFields::tmail_savecopy) === "yes" ? "true" : "false");
+
+        $nodeLink = $this->celw("savecopy");
+        $nodeLink->nodeValue = ($mail->getRawValue(MailFields::tmail_ulink) === "yes" ? "true" : "false");
+
+
+        $nodeBody = $this->celw("body");
+        $nodeBody->setAttribute("content-type", "html");
+        $nodeBody->appendChild($this->dom->createCDATASection($mail->getRawValue(MailFields::tmail_body)));
+
+        $mailNode->appendChild($fromNode);
+        $mailNode->appendChild($nodeRecipients);
+        $mailNode->appendChild($nodeSubject);
+        $mailNode->appendChild($nodeSave);
+        $mailNode->appendChild($nodeLink);
+        $mailNode->appendChild($nodeBody);
+
+        $attachements = $mail->getMultipleRawValues(MailFields::tmail_attach);
+
+        if ($attachements) {
+            $attchementsNode = $this->celw("attachments");
+            foreach ($attachements as $attachement) {
+                $attchNode = $this->celw("attachment");
+                if (preg_match("/([^(]*)\((.*)\)/", $attachement, $reg)) {
+                    $value = trim($reg[1]);
+                    $label = trim($reg[2]);
+                    $attchNode->setAttribute("label", $label);
+                    $attchNode->nodeValue = $value;
+                } else {
+                    $attchNode->nodeValue = $attachement;
+                }
+                $attchementsNode->appendChild($attchNode);
+            }
+            $mailNode->appendChild($attchementsNode);
+        }
+
+
+        return $mailNode;
+    }
+
+    protected function getRecipient($type, $value)
+    {
+        $label="";
+        if (preg_match("/([^(]*)\((.*)\)/", $value, $reg)) {
+            $value = trim($reg[1]);
+            $label = trim($reg[2]);
+        }
+        switch ($type) {
+            /*
+                "F" :"Adresse fixe":
+                "A" :"Attribut texte"
+                "D" :"Attribut relation"
+                "E" :"Paramètre de famille texte"
+                "DE":"Paramètre de famille relation"
+                "P" :"Paramètres globaux"
+                "WA":"Attribut cycle"
+                "WD":"Relation cycle"
+                "WE":"Paramètre cycle"
+            */
+            case "F":
+                $node = $this->celw("address");
+                $node->nodeValue = $value;
+                break;
+            case "A":
+                $node = $this->celw("element-field-value");
+                $node->nodeValue = $value;
+                break;
+            case "D":
+                $node = $this->celw("element-account-field");
+                $node->nodeValue = $value;
+                break;
+            case "E":
+                $node = $this->celw("structure-parameter-value");
+                $node->nodeValue = $value;
+                break;
+            case "DE":
+                $node = $this->celw("structure-account-parameter");
+                $node->nodeValue = $value;
+                break;
+            case "WA":
+                $node = $this->celw("workflow-field-value");
+                $node->nodeValue = $value;
+                break;
+            case "WE":
+                $node = $this->celw("workflow-parameter-value");
+                $node->nodeValue = $value;
+                break;
+            case "WD":
+                $node = $this->celw("workflow-account-field");
+                $node->nodeValue = $value;
+                break;
+            case "P":
+                $node = $this->celw("config-parameter");
+
+                if (strpos($value, '::') === false) {
+                    $ns = ContextParameterManager::getNs($value);
+                    $pvalue = $value;
+                } else {
+                    list($ns, $pvalue) = explode("::", $value);
+                }
+                $node->setAttribute("ns", $ns);
+                $node->nodeValue = $pvalue;
+
+                break;
+            default:
+                $node = $this->celw("unknomtype");
+                $node->setAttribute("type", $type);
+        }
+        if ($label) {
+            $node->setAttribute("label", $label);
+        }
+        return $node;
     }
 
     protected function celw($name)
