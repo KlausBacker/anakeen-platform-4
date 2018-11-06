6b14c68c9684897057ab4bc279b04807c3e60d82
Aurélien Brugalières <aurelien.brugalieres@anakeen.com>
2018-11-06 09:29:35 +0100

    feat(security): add infos tab

--- src/vendor/Anakeen/DevelopmentCenter/vue/sections/SmartStructuresSecurity/subsections/Infos/Infos.controller.js
+++ src/vendor/Anakeen/DevelopmentCenter/vue/sections/SmartStructuresSecurity/subsections/Infos/Infos.controller.js
@@ -1,7 +1,58 @@
 export default {
+  name: "Infos",
+  props: ["ssName"],
+  watch: {
+    ssName(newValue, oldValue) {
+      if (newValue !== oldValue) {
+        this.fetchStructureInfos();
+      }
+    }
+  },
   data() {
     return {
-      msg: "Hello vue !"
+      isReady: false,
+      structureDetails: {}
     };
+  },
+  computed: {
+    urlInfo() {
+      return `/api/v2/devel/smart/structures/${this.ssName}/info/`;
+    },
+    structureSecurity() {
+      return this.structureDetails ? this.structureDetails.security || {} : {};
+    },
+    structureProperties() {
+      return this.structureDetails
+        ? this.structureDetails.properties || {}
+        : {};
+    },
+    structureInfo() {
+      return this.structureDetails ? this.structureDetails.info || {} : {};
+    }
+  },
+  mounted() {
+    this.fetchStructureInfos();
+  },
+  methods: {
+    fetchStructureInfos() {
+      this.$http
+        .get(this.urlInfo)
+        .then(response => {
+          this.structureDetails = response.data.data;
+          this.isReady = true;
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    },
+    formatTags(tags) {
+      if (tags) {
+        return Object.keys(tags)
+          .map(key => {
+            return `<b>${key} :</b> ${tags[key]}`;
+          })
+          .join("<br/>");
+      }
+    }
   }
 };
