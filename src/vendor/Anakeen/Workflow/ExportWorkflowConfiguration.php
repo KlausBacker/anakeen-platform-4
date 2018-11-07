f838ebc842ed1bbfa86e9ce15b321cc40c55a462
Éric Brison <eric.brison@anakeen.com>
2018-11-07 09:51:01 +0100

    feat(config): continue export workflow config

--- src/vendor/Anakeen/Workflow/ExportWorkflowConfiguration.php
+++ src/vendor/Anakeen/Workflow/ExportWorkflowConfiguration.php
@@ -4,8 +4,10 @@ namespace Anakeen\Workflow;
 
 use Anakeen\Core\Internal\ContextParameterManager;
 use Anakeen\Core\SEManager;
+use Anakeen\Core\SmartStructure\Callables\ParseFamilyMethod;
 use Anakeen\SmartStructures\Wdoc\WDocHooks;
 use SmartStructure\Fields\Mailtemplate as MailFields;
+use SmartStructure\Fields\Timer as TimerFields;
 
 class ExportWorkflowConfiguration extends \Anakeen\Core\SmartStructure\ExportConfigurationAccesses
 {
@@ -15,12 +17,17 @@ class ExportWorkflowConfiguration extends \Anakeen\Core\SmartStructure\ExportCon
     protected $workflow;
     const NSWURL = self::NSBASEURL . "workflow/1.0";
     const NSW = "workflow";
+    const NSMTURL = self::NSBASEURL . "mailtemplate/1.0";
+    const NSMT = "mail";
+    const NSTMURL = self::NSBASEURL . "timer/1.0";
+    const NSTM = "timer";
+    protected $dataSet = [];
 
     /** @noinspection PhpMissingParentConstructorInspection */
     public function __construct(WDocHooks $wfl)
     {
         $this->workflow = $wfl;
-        $this->sst = SEManager::getFamily($this->workflow->getRawValue(\SmartStructure\Fields\Wdoc::wf_fam));
+        $this->sst = SEManager::getFamily($this->workflow->getRawValue(\SmartStructure\Fields\Wdoc::wf_famid));
 
         $this->dom = new \DOMDocument("1.0", "UTF-8");
         $this->dom->formatOutput = true;
@@ -28,20 +35,28 @@ class ExportWorkflowConfiguration extends \Anakeen\Core\SmartStructure\ExportCon
         $this->domConfig->setAttribute("xmlns:" . self::NSW, self::NSWURL);
         $this->dom->appendChild($this->domConfig);
 
-        $structConfig = $this->cel("structure-configuration");
-        $structConfig->setAttribute("name", $this->sst->name);
-        if ($this->sst->id < 1000) {
-            $structConfig->setAttribute("id", $this->sst->id);
-        }
 
-        $this->extract($structConfig);
+        $this->extractWorkflow();
+
+        if ($this->sst && $this->sst->wid == $this->workflow->id) {
+            $structConfig = $this->cel("structure-configuration");
+            $structConfig->setAttribute("name", $this->sst->name);
+            if ($this->sst->id < 1000) {
+                $structConfig->setAttribute("id", $this->sst->id);
+            }
+            $defaultWorkflowNode = $this->cel("default-workflow");
+            $defaultWorkflowNode->setAttribute("ref", self::getLogicalName($this->workflow->id));
+            $structConfig->appendChild($defaultWorkflowNode);
+            $this->domConfig->appendChild($structConfig);
+        }
     }
 
-    protected function extract($structConfig)
+    protected function extractWorkflow()
     {
         $this->extractMailTemplatesData($this->domConfig);
-        $this->extractAccess($this->domConfig);
+        $this->extractTimersData($this->domConfig);
         $this->extractConfig($this->domConfig);
+        $this->extractAccess($this->domConfig);
     }
 
     protected function extractAccess(\DOMElement $structConfig)
@@ -57,6 +72,15 @@ class ExportWorkflowConfiguration extends \Anakeen\Core\SmartStructure\ExportCon
     {
         $config = $this->celw("config");
 
+        $config->setAttribute("name", self::getLogicalName($this->workflow->id));
+        $config->setAttribute("label", $this->workflow->getRawValue(\SmartStructure\Fields\Wdoc::ba_title));
+
+        $desc = $this->workflow->getRawValue(\SmartStructure\Fields\Wdoc::wf_desc);
+        if ($desc) {
+            $nodeDesc = $this->celw("description");
+            $nodeDesc->appendChild($this->dom->createCDATASection($desc));
+            $config->appendChild($nodeDesc);
+        }
         $stepsNode = $this->celw("steps");
         $steps = $this->workflow->getStates();
         foreach ($steps as $step) {
@@ -122,27 +146,35 @@ class ExportWorkflowConfiguration extends \Anakeen\Core\SmartStructure\ExportCon
         foreach ($steps as $step) {
             $mails = $this->workflow->getStateMailTemplate($step);
             foreach ($mails as $mail) {
-                $structConfig->appendChild($this->getMailTemplateData($mail));
+                if (!$this->dataSet[$mail]) {
+                    $structConfig->appendChild($this->getMailTemplateData($mail));
+                }
             }
         }
 
         foreach ($this->workflow->transitions as $transitionName => $transitionConfig) {
             $mails = $this->workflow->getTransitionMailTemplates($transitionName);
             foreach ($mails as $mail) {
-                $structConfig->appendChild($this->getMailTemplateData($mail));
+                if (!$this->dataSet[$mail]) {
+                    $structConfig->appendChild($this->getMailTemplateData($mail));
+                }
             }
         }
+
+        $this->domConfig->setAttribute("xmlns:" . self::NSMT, self::NSMTURL);
     }
 
     protected function getMailTemplateData($name)
     {
+        $this->dataSet[$name] = true;
         $mail = SEManager::getDocument($name);
-        $mailNode = $this->celw("mailtemplate");
+        $mailNode = $this->celmail("mailtemplate");
 
         $mailNode->setAttribute("name", static::getLogicalName($name));
         $mailNode->setAttribute("label", $mail->getRawValue(MailFields::tmail_title));
+        $mailNode->setAttribute("structure", static::getLogicalName($mail->getRawValue(MailFields::tmail_family)));
 
-        $fromNode = $this->celw("from");
+        $fromNode = $this->celmail("from");
 
         $fromType = $mail->getMultipleRawValues(MailFields::tmail_fromtype);
         $from = $mail->getMultipleRawValues(MailFields::tmail_from);
@@ -151,28 +183,28 @@ class ExportWorkflowConfiguration extends \Anakeen\Core\SmartStructure\ExportCon
             $fromNode->appendChild($this->getRecipient($fromType[0], $from[0]));
         }
 
-        $nodeRecipients = $this->celw("recipients");
+        $nodeRecipients = $this->celmail("recipients");
         $recips = $mail->getMultipleRawValues(MailFields::tmail_recip);
         $destTypes = $mail->getMultipleRawValues(MailFields::tmail_desttype);
         $copyModes = $mail->getMultipleRawValues(MailFields::tmail_copymode);
         foreach ($recips as $k => $recip) {
-            $nodeRecipient = $this->celw("recipient");
+            $nodeRecipient = $this->celmail("recipient");
             $nodeRecipient->setAttribute("dest", $copyModes[$k]);
             $recipient = $this->getRecipient($destTypes[$k], $recip);
             $nodeRecipient->appendChild($recipient);
             $nodeRecipients->appendChild($nodeRecipient);
         }
-        $nodeSubject = $this->celw("subject");
+        $nodeSubject = $this->celmail("subject");
         $nodeSubject->nodeValue = $mail->getRawValue(MailFields::tmail_subject);
 
-        $nodeSave = $this->celw("savecopy");
+        $nodeSave = $this->celmail("savecopy");
         $nodeSave->nodeValue = ($mail->getRawValue(MailFields::tmail_savecopy) === "yes" ? "true" : "false");
 
-        $nodeLink = $this->celw("savecopy");
+        $nodeLink = $this->celmail("savecopy");
         $nodeLink->nodeValue = ($mail->getRawValue(MailFields::tmail_ulink) === "yes" ? "true" : "false");
 
 
-        $nodeBody = $this->celw("body");
+        $nodeBody = $this->celmail("body");
         $nodeBody->setAttribute("content-type", "html");
         $nodeBody->appendChild($this->dom->createCDATASection($mail->getRawValue(MailFields::tmail_body)));
 
@@ -186,9 +218,9 @@ class ExportWorkflowConfiguration extends \Anakeen\Core\SmartStructure\ExportCon
         $attachements = $mail->getMultipleRawValues(MailFields::tmail_attach);
 
         if ($attachements) {
-            $attchementsNode = $this->celw("attachments");
+            $attchementsNode = $this->celmail("attachments");
             foreach ($attachements as $attachement) {
-                $attchNode = $this->celw("attachment");
+                $attchNode = $this->celmail("attachment");
                 if (preg_match("/([^(]*)\((.*)\)/", $attachement, $reg)) {
                     $value = trim($reg[1]);
                     $label = trim($reg[2]);
@@ -206,9 +238,115 @@ class ExportWorkflowConfiguration extends \Anakeen\Core\SmartStructure\ExportCon
         return $mailNode;
     }
 
+
+    protected function extractTimersData(\DOMElement $structConfig)
+    {
+        $steps = $this->workflow->getStates();
+        foreach ($steps as $step) {
+            $timer = $this->workflow->getStateTimers($step);
+            if ($timer) {
+                if (!$this->dataSet[$timer]) {
+                    $structConfig->appendChild($this->getTimerData($timer));
+                }
+            }
+        }
+
+        foreach ($this->workflow->transitions as $transitionName => $transitionConfig) {
+            $timers = $this->workflow->getTransitionTimers($transitionName);
+            foreach ($timers as $timer) {
+                if (!$this->dataSet[$timer["id"]]) {
+                    $structConfig->appendChild($this->getTimerData($timer["id"]));
+                }
+            }
+        }
+        $this->domConfig->setAttribute("xmlns:" . self::NSTM, self::NSTMURL);
+    }
+
+    protected function getTimerData($name)
+    {
+        $this->dataSet[$name] = true;
+        $timer = SEManager::getDocument($name);
+        $timerNode = $this->celtimer("timer");
+
+        $timerNode->setAttribute("name", static::getLogicalName($name));
+        $timerNode->setAttribute("label", $timer->getRawValue(TimerFields::tm_title));
+
+        $timerNode->setAttribute("structure", static::getLogicalName($timer->getRawValue(TimerFields::tm_family)));
+        $timerNode->setAttribute("workflow", static::getLogicalName($timer->getRawValue(TimerFields::tm_workflow)));
+
+        $dateRef = $timer->getRawValue(TimerFields::tm_dyndate);
+        $dateNode = $this->celtimer("field-date-reference");
+        if ($dateRef) {
+            $dateNode->setAttribute("ref", $dateRef);
+        }
+        $timerNode->appendChild($dateNode);
+        $deltaDay = $timer->getRawValue(TimerFields::tm_refdaydelta);
+        $deltaHourDay = $timer->getRawValue(TimerFields::tm_refhourdelta);
+        if ($deltaDay || $deltaHourDay) {
+            $delay = sprintf("%d days %d hours", $deltaDay, $deltaHourDay);
+            $dateNode->setAttribute("delta", $delay);
+        }
+
+        $tasks = $timer->getAttributeValue(TimerFields::tm_t_config);
+        $tasksNode = $this->celtimer("tasks");
+
+        /*  [1] => Array
+        (
+            [tm_delay] => 2
+            [tm_hdelay] => 9
+            [tm_iteration] => 1
+            [tm_tmail] => Array
+                (
+                )
+
+            [tm_state] => e_ccfd_sl_validee_directeur
+            [tm_method] => ::sayHello(2)
+        )*/
+
+        foreach ($tasks as $task) {
+            $taskNode = $this->celtimer("task");
+            $delay = sprintf("%d days %d hours", $task[TimerFields::tm_delay], $task[TimerFields::tm_hdelay]);
+            $taskNode->setAttribute("delta", $delay);
+            if ($task[TimerFields::tm_state]) {
+                $singleTask = $this->celtimer("setstate");
+                $singleTask->setAttribute("state", $task[TimerFields::tm_state]);
+                $taskNode->appendChild($singleTask);
+            }
+            if ($task[TimerFields::tm_tmail]) {
+                foreach ($task[TimerFields::tm_tmail] as $mail) {
+                    $singleTask = $this->celtimer("sendmail");
+                    $singleTask->setAttribute("ref", self::getLogicalName($mail));
+                    $taskNode->appendChild($singleTask);
+                }
+            }
+            if ($task[TimerFields::tm_method]) {
+                $singleTask = $this->celtimer("process");
+                $method = new ParseFamilyMethod();
+                $method->parse($task[TimerFields::tm_method]);
+
+                $pcNode = $this->celtimer("process-callable");
+                $pcNode->setAttribute("function", sprintf("%s::%s", $method->className, $method->methodName));
+                $singleTask->appendChild($pcNode);
+
+                foreach ($method->inputs as $input) {
+                    $argNode = $this->celtimer("process-argument");
+                    $argNode->nodeValue = $input->name;
+                    $argNode->setAttribute("type", $input->type === "string" ? "string" : "field");
+                    $singleTask->appendChild($argNode);
+                }
+
+                $taskNode->appendChild($singleTask);
+            }
+            $tasksNode->appendChild($taskNode);
+        }
+        $timerNode->appendChild($tasksNode);
+
+        return $timerNode;
+    }
+
     protected function getRecipient($type, $value)
     {
-        $label="";
+        $label = "";
         if (preg_match("/([^(]*)\((.*)\)/", $value, $reg)) {
             $value = trim($reg[1]);
             $label = trim($reg[2]);
@@ -226,39 +364,39 @@ class ExportWorkflowConfiguration extends \Anakeen\Core\SmartStructure\ExportCon
                 "WE":"Paramètre cycle"
             */
             case "F":
-                $node = $this->celw("address");
+                $node = $this->celmail("address");
                 $node->nodeValue = $value;
                 break;
             case "A":
-                $node = $this->celw("element-field-value");
+                $node = $this->celmail("element-field-value");
                 $node->nodeValue = $value;
                 break;
             case "D":
-                $node = $this->celw("element-account-field");
+                $node = $this->celmail("element-account-field");
                 $node->nodeValue = $value;
                 break;
             case "E":
-                $node = $this->celw("structure-parameter-value");
+                $node = $this->celmail("structure-parameter-value");
                 $node->nodeValue = $value;
                 break;
             case "DE":
-                $node = $this->celw("structure-account-parameter");
+                $node = $this->celmail("structure-account-parameter");
                 $node->nodeValue = $value;
                 break;
             case "WA":
-                $node = $this->celw("workflow-field-value");
+                $node = $this->celmail("workflow-field-value");
                 $node->nodeValue = $value;
                 break;
             case "WE":
-                $node = $this->celw("workflow-parameter-value");
+                $node = $this->celmail("workflow-parameter-value");
                 $node->nodeValue = $value;
                 break;
             case "WD":
-                $node = $this->celw("workflow-account-field");
+                $node = $this->celmail("workflow-account-field");
                 $node->nodeValue = $value;
                 break;
             case "P":
-                $node = $this->celw("config-parameter");
+                $node = $this->celmail("config-parameter");
 
                 if (strpos($value, '::') === false) {
                     $ns = ContextParameterManager::getNs($value);
@@ -271,7 +409,7 @@ class ExportWorkflowConfiguration extends \Anakeen\Core\SmartStructure\ExportCon
 
                 break;
             default:
-                $node = $this->celw("unknomtype");
+                $node = $this->celmail("unknomtype");
                 $node->setAttribute("type", $type);
         }
         if ($label) {
@@ -283,5 +421,15 @@ class ExportWorkflowConfiguration extends \Anakeen\Core\SmartStructure\ExportCon
     protected function celw($name)
     {
         return $this->dom->createElementNS(self::NSWURL, self::NSW . ":" . $name);
+    }
+
+    protected function celmail($name)
+    {
+        return $this->dom->createElementNS(self::NSMTURL, self::NSMT . ":" . $name);
+    }
+
+    protected function celtimer($name)
+    {
+        return $this->dom->createElementNS(self::NSTMURL, self::NSTM . ":" . $name);
     }
 }
