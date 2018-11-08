41910667f8b9142fb27527526f1cd985e2c50a35
david <david.dang@anakeen.com>
2018-11-08 15:03:48 +0100

    feat(treelist): component

---
+++ src/vendor/Anakeen/DevelopmentCenter/vue/components/SSTreeList/SSTreeList.component.js
@@ -0,0 +1,146 @@
+import Vue from "vue";
+import "@progress/kendo-ui";
+import { DataSourceInstaller } from "@progress/kendo-datasource-vue-wrapper";
+import {
+  TreeList,
+  TreeListInstaller
+} from "@progress/kendo-treelist-vue-wrapper";
+
+Vue.use(DataSourceInstaller);
+Vue.use(TreeListInstaller);
+
+export default {
+  name: "ss-treelist",
+  props: {
+    ssName: {
+      type: String,
+      default: () => ""
+    },
+    items: {
+      type: Array,
+      default: () => []
+    },
+    url: {
+      type: String,
+      default: () => ""
+    },
+    getValues: {
+      default: () => {
+        return {};
+      }
+    },
+    columnTemplate: {
+      default: () => {
+        return {};
+      }
+    }
+  },
+  components: {
+    "kendo-treelist": TreeList
+  },
+  data() {
+    return {
+      remoteDataSource: ""
+    };
+  },
+  mounted() {
+    this.remoteDataSource = new kendo.data.TreeListDataSource({
+      transport: {
+        read: options => {
+          this.$http
+            .get(this.url)
+            .then(options.success)
+            .catch(options.error);
+        }
+      },
+      schema: {
+        data: response => {
+          return this.getValues(response.data.data);
+        },
+        model: {
+          id: "id",
+          parentId: "parentId",
+          expanded: true
+        }
+      }
+    });
+    $(window).resize(() => {
+      if (this.$refs.ssTreelist) {
+        this.$refs.ssTreelist.kendoWidget().resize();
+      }
+    });
+  },
+  methods: {
+    onDataBound(e) {
+      let tree = e.sender;
+      this.removeRowClassName(tree);
+      this.addRowClassName(tree);
+      tree.autoFitColumn(1);
+    },
+    onExpand(e) {
+      let tree = e.sender;
+      this.addRowClassName(tree);
+    },
+    onCollapse(e) {
+      let tree = e.sender;
+      this.addRowClassName(tree);
+    },
+    addRowClassName(tree) {
+      let items = tree.items();
+      window.setTimeout(() => {
+        const that = this;
+        items.each(function addTypeClass() {
+          let dataItem = tree.dataItem(this);
+          if (dataItem.type) {
+            $(this).addClass(" attr-type--" + dataItem.type);
+          }
+          if (
+            dataItem.structure !== that.ssName &&
+            that.$route.name === "SmartStructures::structure"
+          ) {
+            $(this).addClass(" is-herited");
+          }
+          if (
+            dataItem.declaration === "overrided" &&
+            that.$route.name === "SmartStructures::structure"
+          ) {
+            $(this).addClass(" is-overrided");
+          }
+        });
+      }, 1);
+    },
+    removeRowClassName(tree) {
+      let items = tree.items();
+      window.setTimeout(() => {
+        items.each(function removeTypeClass() {
+          let dataItem = tree.dataItem(this);
+          if (
+            $(this)
+              .attr("class")
+              .includes(" attr-type--")
+          ) {
+            $(this).removeClass(" attr-type--" + dataItem.type);
+          }
+          if (
+            $(this)
+              .attr("class")
+              .includes(" is-herited")
+          ) {
+            $(this).removeClass(" is-herited");
+          }
+          if (
+            $(this)
+              .attr("class")
+              .includes(" is-overrided")
+          ) {
+            $(this).removeClass(" is-overrided");
+          }
+        });
+      }, 1);
+    },
+    refreshTree() {
+      this.$refs.ssTreelist.kendoWidget().dataSource.filter({});
+      this.$refs.ssTreelist.kendoWidget().dataSource.read();
+    }
+  }
+};
---
+++ src/vendor/Anakeen/DevelopmentCenter/vue/components/SSTreeList/SSTreeList.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="ss-treelist">
+        <div>
+            <kendo-toolbar class="ss-treelist-toolbar">
+                <kendo-toolbar-item type="button" icon="refresh" @click="refreshTree"></kendo-toolbar-item>
+            </kendo-toolbar>
+        </div>
+        <kendo-treelist ref="ssTreelist"
+                        class="ss-treelist-tree"
+                        :data-source="remoteDataSource"
+                        :height="'100%'"
+                        :filterable="{ extra: false}"
+                        :resizable="true"
+                        :sortable="true"
+                        :dataBound="onDataBound"
+                        :expand="onExpand"
+                        :collapse="onCollapse"
+                        :columnMenu="true"
+                        :messages="{ noRows: 'There are no data for this Smart Structure...' }">
+            <kendo-treelist-column v-for="item in items" :template="columnTemplate(`${item.name}`)" :field="`${item.name}`" :title="`${item.label}`" :hidden="item.hidden"></kendo-treelist-column>
+        </kendo-treelist>
+    </div>
+</template>
+<style lang="scss">
+    @import "./SSTreeList.scss";
+</style>
+<script src="./SSTreeList.component.js"></script>
