a99eda698378d721a6907525db3544ab00554d50
Éric Brison <eric.brison@anakeen.com>
2018-11-08 15:57:55 +0100

    feat(config): continue export workflow config

--- src/vendor/Anakeen/Workflow/ExportWorkflowConfiguration.php
+++ src/vendor/Anakeen/Workflow/ExportWorkflowConfiguration.php
@@ -6,10 +6,11 @@ use Anakeen\Core\Internal\ContextParameterManager;
 use Anakeen\Core\SEManager;
 use Anakeen\Core\SmartStructure\Callables\ParseFamilyMethod;
 use Anakeen\SmartStructures\Wdoc\WDocHooks;
+use Anakeen\Ui\ExportRenderAccessConfiguration;
 use SmartStructure\Fields\Mailtemplate as MailFields;
 use SmartStructure\Fields\Timer as TimerFields;
 
-class ExportWorkflowConfiguration extends \Anakeen\Core\SmartStructure\ExportConfigurationAccesses
+class ExportWorkflowConfiguration extends ExportRenderAccessConfiguration
 {
     /**
      * @var WDocHooks
@@ -21,6 +22,21 @@ class ExportWorkflowConfiguration extends \Anakeen\Core\SmartStructure\ExportCon
     const NSMT = "mail";
     const NSTMURL = self::NSBASEURL . "timer/1.0";
     const NSTM = "timer";
+
+    const X_MAILTEMPLATEDATA = 1;
+    const X_TIMERDATA = 2;
+    const X_MAILTEMPLATEACCESS = 4;
+    const X_TIMERACCESS = 8;
+    const X_CONFIG = 16;
+    const X_UIDATA = 32;
+    const X_UIACCESS = 64;
+    const X_UIDATAACCESS = 128;
+    const X_UICONFIG = 256;
+    const X_CONFIGACCESS = 512;
+    const X_WFLACCESS = 1024;
+    const X_ALL = -1;
+
+
     protected $dataSet = [];
 
     /** @noinspection PhpMissingParentConstructorInspection */
@@ -34,10 +50,66 @@ class ExportWorkflowConfiguration extends \Anakeen\Core\SmartStructure\ExportCon
         $this->domConfig = $this->cel("config");
         $this->domConfig->setAttribute("xmlns:" . self::NSW, self::NSWURL);
         $this->dom->appendChild($this->domConfig);
+    }
+
+    public function extractWorkflow($type)
+    {
+        if ($type & self::X_MAILTEMPLATEDATA) {
+            // Mail Templates definition used in workflow
+            $this->extractMailTemplatesData($this->domConfig);
+        }
+
+        if ($type & self::X_TIMERDATA) {
+            // Timers definition used in workflow
+            $this->extractTimersData($this->domConfig);
+        }
 
 
-        $this->extractWorkflow();
+        if ($type & self::X_CONFIG) {
+            // References to mailtemplates and timers by steps
+            $this->extractConfig($this->domConfig);
+        }
 
+        if ($type & self::X_UIDATA) {
+            //  Ui : Cv and mask definition used in workflow
+            $this->extractUiData($this->domConfig);
+        }
+
+        if ($type & self::X_UIDATAACCESS) {
+            // Security (Ui) : Cv accesses and mask accesses used in workflow
+            $this->extractUiDataAccesses($this->domConfig);
+        }
+        if ($type & self::X_UICONFIG) {
+            // Ui : References to cv and mask by steps
+            $this->extractUiConfig($this->domConfig);
+        }
+        if ($type & self::X_CONFIGACCESS) {
+            // Security (Ref) : References to profil and field accesses by steps
+            $this->extractAccessConfig($this->domConfig);
+        }
+        if ($type & self::X_WFLACCESS) {
+            // Security (Workflow): Transition Accesses
+            $this->extractAccess($this->domConfig);
+        }
+
+        if ($type & self::X_MAILTEMPLATEACCESS) {
+            // Mail Templates accesses used in workflow
+            $this->extractMailTemplatesDataAccess($this->domConfig);
+        }
+
+        if ($type & self::X_TIMERACCESS) {
+            // Timers accesses used in workflow
+            $this->extractTimersDataAccesses($this->domConfig);
+        }
+
+        if ($type & self::X_CONFIG) {
+            // Reference default workflow on structure
+            $this->extractDefaultWorkflow();
+        }
+    }
+
+    protected function extractDefaultWorkflow()
+    {
         if ($this->sst && $this->sst->wid == $this->workflow->id) {
             $structConfig = $this->cel("structure-configuration");
             $structConfig->setAttribute("name", $this->sst->name);
@@ -48,30 +120,127 @@ class ExportWorkflowConfiguration extends \Anakeen\Core\SmartStructure\ExportCon
             $defaultWorkflowNode->setAttribute("ref", self::getLogicalName($this->workflow->id));
             $structConfig->appendChild($defaultWorkflowNode);
 
-            $this->domConfig->appendChild($this->dom->createComment("Default workflow for the structure"));
+            $this->setComment("Default workflow for the structure");
             $this->domConfig->appendChild($structConfig);
         }
-    }
-
-    protected function extractWorkflow()
-    {
-        $this->extractMailTemplatesData($this->domConfig);
-        $this->extractTimersData($this->domConfig);
-        $this->extractConfig($this->domConfig);
-        $this->extractAccessConfig($this->domConfig);
-        $this->extractAccess($this->domConfig);
     }
 
     protected function extractAccess(\DOMElement $structConfig)
     {
         if ($this->workflow->profid) {
-            $structConfig->appendChild($this->dom->createComment("Access of workflow configuration"));
+            $this->setComment("Access of workflow configuration", $structConfig);
             $accessControl = $this->setAccess($this->workflow->profid, "basic");
             $structConfig->appendChild($accessControl);
-            $structConfig->appendChild($this->dom->createComment("Access of workflow transitions"));
+            $this->setComment("Access of workflow transitions", $structConfig);
             $accessControl = $this->setAccess($this->workflow->profid, "extended");
             $structConfig->appendChild($accessControl);
         }
+    }
+
+    protected function extractUiDataAccesses(\DOMElement $structConfig)
+    {
+        return $this->extractUiData($structConfig, "accesses");
+    }
+
+    protected function extractUiData(\DOMElement $structConfig, $extractPart = "config")
+    {
+        $this->domConfig->setAttribute("xmlns:" . self::NSUI, self::NSUIURL);
+        $config = $this->celw("config");
+
+        $config->setAttribute("name", self::getLogicalName($this->workflow->id));
+        $config->setAttribute("structure", self::getLogicalName($this->workflow->getRawValue(\SmartStructure\Fields\Wdoc::wf_famid)));
+
+        if ($extractPart === "accesses") {
+            $this->setComment("Accesses of elements (cvdoc, masks) used in workflow");
+        } else {
+            $this->setComment("Element (cvdoc, mask) data used in workflow");
+        }
+
+        $steps = $this->workflow->getStates();
+        foreach ($steps as $step) {
+            $stepNode = $this->celw("step");
+            $stepNode->setAttribute("ref", $step);
+
+            $maskId = $this->workflow->getStateMask($step);
+            if ($maskId) {
+                $mask = SEManager::getDocument($maskId);
+                /** @var \SmartStructure\Mask $mask */
+                $maskNode = null;
+                if ($extractPart === "accesses") {
+                    $this->setAccessProfile($mask);
+                } else {
+                    $maskNode = $this->extractMaskData($mask);
+                }
+                if ($maskNode) {
+                    $structConfig->appendChild($maskNode);
+                }
+            }
+            $cvId = $this->workflow->getStateViewControl($step);
+            if ($cvId) {
+                $cvdoc = SEManager::getDocument($cvId);
+                /** @var \SmartStructure\Cvdoc $cvdoc */
+                if ($extractPart === "accesses") {
+                    $cvAccessNode = $this->extractCvdocDataAccess($cvdoc);
+                    if ($cvAccessNode) {
+                        $structConfig->appendChild($cvAccessNode);
+                    }
+                    $cvNode = $this->setAccess($cvdoc->id);
+                } else {
+                    $cvNode = $this->extractCvdocData($cvdoc);
+                }
+                if ($cvNode) {
+                    $structConfig->appendChild($cvNode);
+                }
+            }
+        }
+
+        $structConfig->appendChild($config);
+    }
+
+    protected function extractUiConfig(\DOMElement $structConfig)
+    {
+        $config = $this->celw("config");
+
+        $config->setAttribute("name", self::getLogicalName($this->workflow->id));
+        $config->setAttribute("structure", self::getLogicalName($this->workflow->getRawValue(\SmartStructure\Fields\Wdoc::wf_famid)));
+
+
+        $stepsNode = $this->celw("steps");
+        $steps = $this->workflow->getStates();
+        foreach ($steps as $step) {
+            $stepNode = $this->celw("step");
+            $stepNode->setAttribute("ref", $step);
+
+            $color = $this->workflow->getColor($step);
+            if ($color) {
+                $colorNode = $this->celw("color");
+                $colorNode->nodeValue = $color;
+                $stepNode->appendChild($colorNode);
+            }
+            $stepsNode->appendChild($stepNode);
+
+            $maskId = $this->workflow->getStateMask($step);
+            if ($maskId) {
+                $maskNode = $this->celw("mask");
+                $maskNode->setAttribute("ref", static::getLogicalName($maskId));
+                $stepNode->appendChild($maskNode);
+            }
+            $stepsNode->appendChild($stepNode);
+
+            $cvId = $this->workflow->getStateViewControl($step);
+            if ($cvId) {
+                $cvNode = $this->celw("view-control");
+                $cvNode->setAttribute("ref", static::getLogicalName($cvId));
+                $stepNode->appendChild($cvNode);
+            }
+            $stepsNode->appendChild($stepNode);
+        }
+
+        $this->setComment("Elements (color, cvdoc, masks) user interface referenced in workflow");
+        $config->appendChild($stepsNode);
+
+
+        $structConfig->appendChild($config);
     }
 
     protected function extractAccessConfig(\DOMElement $structConfig)
@@ -105,14 +274,13 @@ class ExportWorkflowConfiguration extends \Anakeen\Core\SmartStructure\ExportCon
             $stepsNode->appendChild($stepNode);
         }
 
-        $config->appendChild($this->dom->createComment("Step Element Accesses of workflow"));
+        $this->setComment("Step Element Accesses of workflow: profile and field accesses reference");
         $config->appendChild($stepsNode);
-
-
 
 
         $structConfig->appendChild($config);
     }
+
     protected function extractConfig(\DOMElement $structConfig)
     {
         $config = $this->celw("config");
@@ -182,14 +350,14 @@ class ExportWorkflowConfiguration extends \Anakeen\Core\SmartStructure\ExportCon
         }
         $config->appendChild($transitionsNode);
 
+        $this->setComment("Timer and Mail templates workflow references", $structConfig);
         $structConfig->appendChild($config);
     }
 
 
     protected function extractMailTemplatesData(\DOMElement $structConfig)
     {
-
-        $structConfig->appendChild($this->dom->createComment("Definition of mail templates used by workflow"));
+        $this->setComment("Definition of mail templates used by workflow", $structConfig);
         $steps = $this->workflow->getStates();
         foreach ($steps as $step) {
             $mails = $this->workflow->getStateMailTemplate($step);
@@ -205,6 +373,33 @@ class ExportWorkflowConfiguration extends \Anakeen\Core\SmartStructure\ExportCon
             foreach ($mails as $mail) {
                 if (empty($this->dataSet[$mail])) {
                     $structConfig->appendChild($this->getMailTemplateData($mail));
+                }
+            }
+        }
+
+        $this->domConfig->setAttribute("xmlns:" . self::NSMT, self::NSMTURL);
+    }
+
+    protected function extractMailTemplatesDataAccess(\DOMElement $structConfig)
+    {
+        $this->setComment("Accesses of mail templates used by workflow", $structConfig);
+        $steps = $this->workflow->getStates();
+        foreach ($steps as $step) {
+            $mails = $this->workflow->getStateMailTemplate($step);
+            foreach ($mails as $mail) {
+                $elt = SEManager::getDocument($mail);
+                if ($elt) {
+                    $this->setAccessProfile($elt);
+                }
+            }
+        }
+
+        foreach ($this->workflow->transitions as $transitionName => $transitionConfig) {
+            $mails = $this->workflow->getTransitionMailTemplates($transitionName);
+            foreach ($mails as $mail) {
+                $elt = SEManager::getDocument($mail);
+                if ($elt) {
+                    $this->setAccessProfile($elt);
                 }
             }
         }
@@ -289,7 +484,7 @@ class ExportWorkflowConfiguration extends \Anakeen\Core\SmartStructure\ExportCon
 
     protected function extractTimersData(\DOMElement $structConfig)
     {
-        $structConfig->appendChild($this->dom->createComment("Definition of timers used by workflow"));
+        $this->setComment("Definition of timers used by workflow", $structConfig);
 
         $steps = $this->workflow->getStates();
         foreach ($steps as $step) {
@@ -306,6 +501,34 @@ class ExportWorkflowConfiguration extends \Anakeen\Core\SmartStructure\ExportCon
             foreach ($timers as $timer) {
                 if (empty($this->dataSet[$timer["id"]])) {
                     $structConfig->appendChild($this->getTimerData($timer["id"]));
+                }
+            }
+        }
+        $this->domConfig->setAttribute("xmlns:" . self::NSTM, self::NSTMURL);
+    }
+
+
+    protected function extractTimersDataAccesses(\DOMElement $structConfig)
+    {
+        $this->setComment("Accesses of timers used by workflow", $structConfig);
+
+        $steps = $this->workflow->getStates();
+        foreach ($steps as $step) {
+            $timer = $this->workflow->getStateTimers($step);
+            if ($timer) {
+                $elt = SEManager::getDocument($timer);
+                if ($elt) {
+                    $this->setAccessProfile($elt);
+                }
+            }
+        }
+
+        foreach ($this->workflow->transitions as $transitionName => $transitionConfig) {
+            $timers = $this->workflow->getTransitionTimers($transitionName);
+            foreach ($timers as $timer) {
+                $elt = SEManager::getDocument($timer["id"]);
+                if ($elt) {
+                    $this->setAccessProfile($elt);
                 }
             }
         }
