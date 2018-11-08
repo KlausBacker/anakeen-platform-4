b515458a3276bd582f55deb23779b3f18c42071e
david <david.dang@anakeen.com>
2018-11-08 09:19:54 +0100

    fix(UI Views): fix columns name and position

--- src/vendor/Anakeen/DevelopmentCenter/vue/sections/Ui/ViewsConfiguration/ViewsConf.vue
+++ src/vendor/Anakeen/DevelopmentCenter/vue/sections/Ui/ViewsConfiguration/ViewsConf.vue
@@ -18,15 +18,15 @@
                     :filterable-mode="'row'"
                     :filterable-extra="false"
                     :sortable="true">
-            <kendo-grid-column :field="'cvId'" :title="'<b>Logical Name</b>'":filterable-cell-operator="'contains'" :filterable-cell-show-operators="false" :filterable-cell-template="autoFilterCol"></kendo-grid-column>
-            <kendo-grid-column :field="'cvStructure'" :title="'<b>View Name</b>'"  :filterable-cell-operator="'contains'" :filterable-cell-show-operators="false" :filterable-cell-template="autoFilterCol"></kendo-grid-column>
-            <kendo-grid-column :field="'viewId'" :title="'<b>Label</b>'"  :filterable-cell-operator="'contains'" :filterable-cell-show-operators="false" :filterable-cell-template="autoFilterCol"></kendo-grid-column>
-            <kendo-grid-column :field="'maskId'" :title="'<b>Mask</b>'"  :filterable-cell-show-operators="false"></kendo-grid-column>
-            <kendo-grid-column :field="'order'" :title="'<b>Order</b>'"  :filterable-cell-show-operators="false"></kendo-grid-column>
-            <kendo-grid-column :field="'renderConfigClass'" :title="'<b>Render Class</b>'"  :filterable-cell-show-operators="false"></kendo-grid-column>
-            <kendo-grid-column :field="'menuList'" :title="'<b>Menu List Label</b>'"  :filterable-cell-show-operators="false"></kendo-grid-column>
-            <kendo-grid-column :field="'viewMode'" :title="'<b>View Mode</b>'"  :filterable-cell-operator="'contains'" :filterable-cell-show-operators="false" :filterable-cell-template="autoFilterCol"></kendo-grid-column>
-            <kendo-grid-column :field="'displayed'" :title="'<b>Disabled</b>'"  :filterable-cell-show-operators="false"></kendo-grid-column>
+            <kendo-grid-column :field="'cvId'" :title="'<b>Logical Name</b>'" :filterable-cell-operator="'contains'" :filterable-cell-show-operators="false" :filterable-cell-template="autoFilterCol"></kendo-grid-column>
+            <kendo-grid-column :field="'viewId'" :title="'<b>View Name</b>'" :filterable-cell-operator="'contains'" :filterable-cell-show-operators="false" :filterable-cell-template="autoFilterCol"></kendo-grid-column>
+            <kendo-grid-column :field="'cvStructure'" :title="'<b>Structure</b>'" :filterable-cell-operator="'contains'" :filterable-cell-show-operators="false" :filterable-cell-template="autoFilterCol"></kendo-grid-column>
+            <kendo-grid-column :field="'maskId'" :title="'<b>Mask</b>'" :filterable-cell-operator="'contains'" :filterable-cell-show-operators="false" :filterable-cell-template="autoFilterCol"></kendo-grid-column>
+            <kendo-grid-column :field="'order'" :title="'<b>Order</b>'" :filterable-cell-operator="'contains'" :filterable-cell-show-operators="false" :filterable-cell-template="autoFilterCol"></kendo-grid-column>
+            <kendo-grid-column :field="'viewMode'" :title="'<b>View Mode</b>'" :filterable-cell-operator="'contains'" :filterable-cell-show-operators="false" :filterable-cell-template="autoFilterCol"></kendo-grid-column>
+            <kendo-grid-column :field="'renderConfigClass'" :title="'<b>Render Class</b>'" :filterable-cell-operator="'contains'" :filterable-cell-show-operators="false" :filterable-cell-template="autoFilterCol"></kendo-grid-column>
+            <kendo-grid-column :field="'menuList'" :title="'<b>Menu List Label</b>'" :filterable-cell-operator="'contains'" :filterable-cell-show-operators="false" :filterable-cell-template="autoFilterCol"></kendo-grid-column>
+            <kendo-grid-column :field="'displayed'" :title="'<b>Displayed</b>'" :filterable-cell-operator="'contains'" :filterable-cell-show-operators="false" :filterable-cell-template="autoFilterCol"></kendo-grid-column>
         </kendo-grid>
     </div>
 </template>
