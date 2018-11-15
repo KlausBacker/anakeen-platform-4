4f59d2825cf486fcb0481ed5cd2aad8093399238
david <david.dang@anakeen.com>
2018-11-15 17:05:18 +0100

    feat(SS with associated WFL): SS-List displays SS which have an associated WFL

---
+++ src/vendor/Anakeen/Routes/Devel/Workflow/StructureWorkflow.php
@@ -0,0 +1,72 @@
+<?php
+
+
+namespace Anakeen\Routes\Devel\Workflow;
+
+use Anakeen\Core\SEManager;
+use Anakeen\Router\ApiV2Response;
+use Anakeen\Search\SearchElements;
+
+class StructureWorkflow
+{
+    protected $target="all";
+    public function __invoke(\Slim\Http\request $request, \Slim\Http\response $response, $args)
+    {
+        $this->initParameters($request, $args);
+        return ApiV2Response::withData($response, $this->doRequest());
+    }
+
+    protected function initParameters(\Slim\Http\request $request, $args)
+    {
+        $this->target = $args["target"];
+        $this->filters = $request->getParam("filter", []);
+    }
+
+    protected function filterRequest(\SearchDoc $searchDoc)
+    {
+        if (!empty($this->filters)) {
+            if (!empty($this->filters["logic"]) && !empty($this->filters["filters"])) {
+                $filters = $this->filters["filters"];
+                $logic = sprintf(" %s ", strtoupper($this->filters["logic"]));
+                $filterSql = implode($logic, array_map(function ($filter) {
+                    return sprintf("%s ~* '%s'", pg_escape_string($filter["field"]), pg_escape_string($filter["value"]));
+                }, $filters));
+                $searchDoc->addFilter($filterSql);
+            }
+        }
+    }
+
+    public function doRequest()
+    {
+        $result = $this->getWflSSList();
+        return $result;
+    }
+
+    protected function getWflSSList()
+    {
+        $s = new SearchElements(\SmartStructure\Wdoc::familyName);
+        $wdocList = $s->search()->getResults();
+        $data = [];
+        foreach ($wdocList as $wdoc) {
+            $structure = SEManager::getFamily($wdoc->getAttributeValue("wf_famid"));
+            $wfData = [
+                "id"=>intval($structure->id),
+                "name"=>$structure->name,
+                "title"=>$structure->getTitle(),
+                "icon"=>$structure->getIcon("", 32),
+                "wfl" => [
+                    "wflId" => $wdoc->name ?: $wdoc->id,
+                    "title" => $wdoc->title,
+                    "baTitle" => $wdoc->getAttributeValue("ba_title"),
+                    "wfDesc" => $wdoc->getAttributeValue("wf_desc"),
+                    "wfFamid" => $wdoc->getAttributeValue("wf_famid"),
+                    "wfFam" => $wdoc->getAttributeValue("wf_fam"),
+                    "dpdocFamid" => $wdoc->getAttributeValue("dpdoc_famid"),
+                    "dpdocFam" => $wdoc->getAttributeValue("dpdoc_fam")
+                ]
+            ];
+            $data[] = $wfData;
+        }
+        return $data;
+    }
+}
