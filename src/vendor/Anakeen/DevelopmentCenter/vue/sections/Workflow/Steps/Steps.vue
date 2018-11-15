b8991cc18d95772f1679ef80aff1926094a57769
david <david.dang@anakeen.com>
2018-11-15 17:07:52 +0100

    feat(WFL>steps): WIP Steps grid

---
+++ src/vendor/Anakeen/DevelopmentCenter/vue/sections/Workflow/Steps/Steps.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="steps-grid-parent">
+        <div>
+            <kendo-toolbar class="steps-grid-toolbar">
+                <kendo-toolbar-item type="button" icon="refresh" @click="refreshSteps"></kendo-toolbar-item>
+            </kendo-toolbar>
+        </div>
+        <kendo-datasource ref="stepsGrid"
+                          :transport-read="getSteps"
+                          :schema-data="parseStepsData">
+        </kendo-datasource>
+        <kendo-grid ref="stepsGridContent" class="steps-grid-content"
+                    :data-source-ref="'stepsGrid'"
+                    :sortable="true">
+            <kendo-grid-column :field="'id'" :title="'<b>Step</b>'":filterable-cell-operator="'contains'" :filterable-cell-show-operators="false" :filterable-cell-template="autoFilterCol"></kendo-grid-column>
+            <kendo-grid-column :field="'color'" :title="'<b>Color</b>'"  :filterable-cell-operator="'contains'" :filterable-cell-show-operators="false" :filterable-cell-template="autoFilterCol"></kendo-grid-column>
+            <kendo-grid-column :field="'label'" :title="'<b>Activity Label</b>'"  :filterable-cell-operator="'contains'" :filterable-cell-show-operators="false" :filterable-cell-template="autoFilterCol"></kendo-grid-column>
+            <kendo-grid-column :field="'param.mailTemplate'" :title="'<b>Mail Template</b>'"  :filterable-cell-operator="'contains'" :filterable-cell-show-operators="false" :filterable-cell-template="autoFilterCol"></kendo-grid-column>
+            <kendo-grid-column :field="'param.timer'" :title="'<b>Timers</b>'" :filterable-cell-template="disabledFilter" :filterable-cell-show-operators="false"></kendo-grid-column>
+            <kendo-grid-column :field="'viewcontrol'" :title="'<b>View Control</b>'" :filterable-cell-template="disabledFilter" :filterable-cell-show-operators="false"></kendo-grid-column>
+            <kendo-grid-column :field="'mask'" :title="'<b>Masks</b>'" :filterable-cell-template="disabledFilter" :filterable-cell-show-operators="false"></kendo-grid-column>
+            <kendo-grid-column :field="'profil'" :title="'<b>PDOC</b>'" :filterable-cell-template="disabledFilter" :filterable-cell-show-operators="false"></kendo-grid-column>
+            <!--<kendo-grid-column :field="'security.seAccess'" :title="'<b>Smart Field Access</b>'" :filterable-cell-template="disabledFilter" :filterable-cell-show-operators="false"></kendo-grid-column>-->
+        </kendo-grid>
+    </div>
+</template>
+<!-- CSS to this component only -->
+<style lang="scss">
+    @import "./Steps.scss";
+</style>
+<script src="./Steps.controller.js"></script>
