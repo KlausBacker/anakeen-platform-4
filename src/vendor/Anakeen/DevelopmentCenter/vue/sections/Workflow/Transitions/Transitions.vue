e3507368d0a4a296b7b76d8a020e1fcfc382f3da
david <david.dang@anakeen.com>
2018-11-16 17:04:15 +0100

    fix(wfl>transitions): add template to transitions data cell

--- src/vendor/Anakeen/DevelopmentCenter/vue/sections/Workflow/Transitions/Transitions.vue
+++ src/vendor/Anakeen/DevelopmentCenter/vue/sections/Workflow/Transitions/Transitions.vue
@@ -8,10 +8,10 @@
                     :data-source-ref="'transitionsGrid'"
                     :sortable="true">
             <kendo-grid-column :field="'id'" :title="'<b>Transition</b>'"></kendo-grid-column>
-            <kendo-grid-column :field="'mail'" :title="'<b>Mail Template</b>'"></kendo-grid-column>
-            <kendo-grid-column :field="'colatileTimers'" :title="'<b>Volatile Timers</b>'" :template="displayMultiple"></kendo-grid-column>
-            <kendo-grid-column :field="'persistentTimers'" :title="'<b>Persistent Timers</b>'" :template="displayMultiple"></kendo-grid-column>
-            <kendo-grid-column :field="'unAttachTimers'" :title="'<b>Timers to unattach</b>'" :template="displayMultiple"></kendo-grid-column>
+            <kendo-grid-column :field="'mailtemplates'" :title="'<b>Mail Template</b>'" :template="displayMultiple('mailtemplates')"></kendo-grid-column>
+            <kendo-grid-column :field="'volatileTimers'" :title="'<b>Volatile Timers</b>'" :template="displayMultiple('volatileTimers')"></kendo-grid-column>
+            <kendo-grid-column :field="'persistentTimers'" :title="'<b>Persistent Timers</b>'" :template="displayMultiple('persistentTimers')"></kendo-grid-column>
+            <kendo-grid-column :field="'unAttachTimers'" :title="'<b>Timers to unattach</b>'" :template="displayMultiple('unAttachTimers')"></kendo-grid-column>
         </kendo-grid>
     </div>
 </template>
