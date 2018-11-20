e73097df48c3071fb394e375310f6cf6a7256c32
david <david.dang@anakeen.com>
2018-11-20 17:11:19 +0100

    feat(wfl > permissions): add permissions grid

---
+++ src/vendor/Anakeen/DevelopmentCenter/vue/sections/Workflow/Permissions/Permissions.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="permissions-grid-parent">
+        <profile-view ref="permGrid" :profileId="wflName" onlyExtendedAcls="true" class="permissions-grid-content"></profile-view>
+    </div>
+</template>
+<style lang="scss">
+    .permissions-grid-parent {
+        flex: 1;
+        min-height: 200px;
+        overflow: hidden;
+    }
+
+    .permissions-grid-content {
+        width: 100%;
+        height: 100%;
+
+    }
+</style>
+<script>
+  import ProfileView from "../../../components/profile/profile.vue";
+  export default {
+    components: {
+      "profile-view": ProfileView
+    },
+    props: ["wflName"],
+    mounted() {
+      $(window).resize(() => {
+        if (this.$refs.permGrid.$el.kendoWidget()) {
+          this.$refs.permGrid.$el.kendoWidget().resize();
+        }
+      });
+      const options = this.$refs.permGrid.$el.kendoWidget().getOptions();
+    }
+  }
+</script>
