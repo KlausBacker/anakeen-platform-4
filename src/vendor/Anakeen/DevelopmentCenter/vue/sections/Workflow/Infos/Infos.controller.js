9336225ac488825996b3f1449fad337258f9c9dd
david <david.dang@anakeen.com>
2018-11-21 11:47:28 +0100

    fix(wfl::infos): retrieve right url for graph svg

--- src/vendor/Anakeen/DevelopmentCenter/vue/sections/Workflow/Infos/Infos.controller.js
+++ src/vendor/Anakeen/DevelopmentCenter/vue/sections/Workflow/Infos/Infos.controller.js
@@ -3,21 +3,20 @@ export default {
   props: ["wflName"],
   data() {
     return {
-      wflData: {}
+      wflData: {},
+      wflGraph: ""
     };
-  },
-  computed: {
-    graphUrl() {
-      return `/api/v2/devel/ui/workflows/image/${
-        this.wflName
-      }/sizes/24x24.svg?inline=true`;
-    }
   },
   mounted() {
     this.$http
       .get(`/api/v2/devel/smart/workflows/${this.wflName}`)
       .then(response => {
         this.wflData = response.data.data.properties;
+      });
+    this.$http
+      .get(`/api/v2/devel/ui/workflows/image/${this.wflName}/sizes/24x24.svg`)
+      .then(response => {
+        this.wflGraph = response.data;
       });
   }
 };
