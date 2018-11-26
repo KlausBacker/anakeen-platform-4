ef43b751781a1695d841eef68dae6b935cd52955
david <david.dang@anakeen.com>
2018-11-26 17:27:52 +0100

    fix(ss: param): add parameters' defaut values

---
+++ src/vendor/Anakeen/DevelopmentCenter/vue/sections/SmartStructures/Parameters/ParametersDefaultValue.vue
@@ -0,0 +1,99 @@
+<template>
+    <ss-treelist ref="paramDefVamList" :items="getItems(this.columnSizeTab)" :url="url" :getValues="getValues"
+                 :columnTemplate="columnTemplate" :messages="messages"></ss-treelist>
+</template>
+<script>
+  import Vue from "vue";
+  import SsTreelist from "../../../components/SSTreeList/SSTreeList.vue";
+
+  Vue.use(SsTreelist.name, SsTreelist);
+
+  export default {
+    components: {SsTreelist},
+    props: ["ssName"],
+    data() {
+      return {
+        columnSizeTab: [],
+        items: [
+          {name: "id", label: "Identification", hidden: false},
+          {name: "type", label: "Type", hidden: false},
+          {name: "labeltext", label: "Label", hidden: false},
+          {name: "config", label: "Configuration", hidden: false, width: "15rem"},
+          {name: "value", label: "Value", hidden: false, width: "15rem"},
+        ],
+        url: `/api/v2/devel/smart/structures/${this.ssName}/defaults/`,
+        getValues(response) {
+          const items = response.defaultValues;
+          const fields = Object.keys(items).map(item => {
+            return {
+              idVal: item,
+              config: items[item].config,
+              type: items[item].type,
+              value: items[item].value
+            };
+          });
+          fields.forEach(items2 => {
+            Object.keys(response.parameterFields).forEach(items => {
+              if (items2.type === "parameter") {
+                if (items2.idVal === response.parameterFields[items].id) {
+                  response.parameterFields[items].config = items2.config;
+                  response.parameterFields[items].value = items2.value;
+                }
+              }
+            });
+          });
+          return response.parameterFields;
+        },
+        getItems(tab) {
+          Object.keys(this.items).forEach(item => {
+            if (tab) {
+              tab.forEach(it => {
+                if (this.items[item].name === it.field && it.width) {
+
+                  this.items[item]["width"] = it.width + "px";
+                } else if (!this.items[item]["width"]) {
+                  this.items[item]["width"] = "10rem";
+                }
+              });
+            }
+          });
+          return this.items;
+        },
+        columnTemplate(colId) {
+          return dataItem => {
+            if (dataItem[colId] === null || dataItem[colId] === undefined) {
+              return "";
+            }
+            if (dataItem[colId] instanceof Object && colId === "value") {
+              if (dataItem[colId].length > 1) {
+                let str = "";
+                Object.keys(dataItem[colId].toJSON()).forEach(item => {
+                  str += "<li>" + dataItem[colId][item] + "</li>";
+                });
+                return str;
+              } else {
+                return dataItem[colId][0] ? dataItem[colId][0] : "";
+              }
+            }
+            return dataItem[colId];
+          };
+        },
+        messages: "There are no default parameter values for this Smart Structure..."
+      }
+    },
+    created() {
+      this.columnSizeTab = JSON.parse(
+        window.localStorage.getItem(
+          "param-list-default-column-size-conf-" + this.ssName
+        ));
+    },
+    mounted() {
+      this.$refs.paramDefValList.$refs.ssTreelist.kendoWidget().bind("columnResize", (e) => {
+        window.localStorage.setItem(
+          "param-list-default-column-size-conf-" + this.ssName,
+          JSON.stringify(this.$refs.paramDefValList.onColumnResize(e))
+        );
+      });
+    }
+  }
+</script>
