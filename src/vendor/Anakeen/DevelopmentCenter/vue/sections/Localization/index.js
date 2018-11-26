ba606549f7484f2230ad4a7f005ea44c728d91c6
Aurélien Brugalières <aurelien.brugalieres@anakeen.com>
2018-11-26 16:04:32 +0100

    feat(localization): add localization section

---
+++ src/vendor/Anakeen/DevelopmentCenter/vue/sections/Localization/index.js
@@ -0,0 +1,11 @@
+import LocalizationContent from "./Localization/Localization.vue";
+
+export default {
+  name: "Localization",
+  path: "localization",
+  order: 7,
+  meta: {
+    label: "Localization"
+  },
+  component: LocalizationContent
+};
