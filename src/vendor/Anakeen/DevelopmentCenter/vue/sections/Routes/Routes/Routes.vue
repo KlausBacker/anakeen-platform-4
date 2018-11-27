56f4fa4d55e323d27a66afec5baf3cf58f3e6cae
david <david.dang@anakeen.com>
2018-11-27 17:21:37 +0100

    feat(applicable middlewares): add links from route to middlwares to display applicable ones

--- src/vendor/Anakeen/DevelopmentCenter/vue/sections/Routes/Routes/Routes.vue
+++ src/vendor/Anakeen/DevelopmentCenter/vue/sections/Routes/Routes/Routes.vue
@@ -23,21 +23,47 @@
           {name: "override", label: "Overrided", hidden: false, width: "8rem"},
           {name: "rowLevel", label: "rowLevel", hidden: true}
         ],
-        sort: [{ field: "rowLevel", dir: "asc" },{ field: "name", dir: "asc"}],
+        sort: [{field: "rowLevel", dir: "asc"}, {field: "name", dir: "asc"}],
         url: `/api/v2/devel/routes/all/`,
         getValues(response) {
           return response;
         },
         columnTemplate(colId) {
           return dataItem => {
-            if (dataItem[colId]) {
-              return dataItem[colId];
-            } else {
-              return '';
+            if (dataItem[colId] === null || dataItem[colId] === undefined) {
+              return "";
             }
-          }
+            switch (colId) {
+              case "pattern":
+                const data = dataItem[colId].split(",");
+                let str = "";
+                let name;
+                if (dataItem["rowLevel"] === 2) {
+                  if (data.length > 1) {
+                    name = `${dataItem["parentName"]}::${dataItem["name"]}`;
+                    data.forEach(d => {
+                      str += `<li><a data-role="develRouterLink" href="/devel/routes/middlewares/?filter=${d}&name=${name}">${d}</a></li>`;
+                    });
+                  } else {
+                    name = `${dataItem["parentName"]}::${dataItem["name"]}`;
+                    str = `<a data-role="develRouterLink" href="/devel/routes/middlewares/?filter=${dataItem[colId]}&name=${name}">${dataItem[colId]}</a>`;
+                  }
+                } else {
+                  if (data.length > 1) {
+                    data.forEach(d => {
+                      str += `<li><a data-role="develRouterLink" href="/devel/routes/middlewares/?filter=${d}&name=${dataItem["name"]}">${d}</a></li>`;
+                    });
+                  } else {
+                    str = `<a data-role="develRouterLink" href="/devel/routes/middlewares/?filter=${dataItem[colId]}&name=${dataItem["name"]}">${dataItem[colId]}</a>`;
+                  }
+                }
+                return str;
+              default:
+                return dataItem[colId];
+            }
+          };
         }
-      }
+      };
     }
   }
 </script>
--- src/vendor/Anakeen/Routes/Devel/Routes/ApplicMiddlewares.php
+++ src/vendor/Anakeen/Routes/Devel/Routes/ApplicMiddlewares.php
@@ -1,34 +1,76 @@
 <?php
 
+
 namespace Anakeen\Routes\Devel\Routes;
 
-use Anakeen\Core\SEManager;
 use Anakeen\Router\ApiV2Response;
 
-class Routes
+class ApplicMiddlewares
 {
     protected $currentNameSpace = null;
     protected $currentName = null;
-    public function __invoke(\Slim\Http\request $request, \Slim\Http\response $response)
+    /**
+     * @param \Slim\Http\request $request
+     * @param \Slim\Http\response $response
+     * @param $args
+     * @return \Slim\Http\response
+     * @throws \Dcp\Core\Exception
+     */
+    public function __invoke(\Slim\Http\request $request, \Slim\Http\response $response, $args)
     {
+        $routeParser = new \FastRoute\RouteParser\Std();
         $allRoutes = new \Anakeen\Router\RouterManager();
         $tabRoutes = $allRoutes->getRoutes();
+        $allMiddlewares = new \Anakeen\Router\RouterManager();
+        $tabMiddlewares = $allMiddlewares->getMiddlewares();
         $result = [];
         foreach ($tabRoutes as $route) {
-            $formatedRoute = $this->formatRoute($route);
-            if ($formatedRoute !== null) {
-                $result[] = $formatedRoute;
+            if (strcmp($route->name, $args['name']) === 0) {
+                foreach ($tabMiddlewares as $middleware) {
+                    $path = $this->getMiddlewarePath($routeParser->parse($middleware->pattern));
+                    if (is_array($route->pattern)) {
+                        foreach ($route->pattern as $item) {
+                            if (preg_match_all('/\\' . $path . '/', $item) === 1) {
+                                array_push($result, $this->formatRoute($middleware, $item));
+                            }
+                        }
+                    } else {
+                        if (preg_match_all('/\\' . $path . '/', $route->pattern) === 1) {
+                            array_push($result, $this->formatRoute($middleware, $route->pattern));
+                        }
+                    }
+                }
             }
         }
         return ApiV2Response::withData($response, $this->formatTreeDataSource($result));
     }
+
+    /**
+     * @param $middleware
+     * @return string
+     * Retrieve regex from middleware pattern
+     */
+    private function getMiddlewarePath($middleware)
+    {
+        $path = '';
+        $index = 0;
+        foreach ($middleware[1] as $item) {
+            if (is_string($item)) {
+                $path .= $item[$index++];
+                continue;
+            }
+            $path .= $item[$index++];
+        }
+        return $path;
+    }
+
     /**
      * @param $route
      * @return array
      * @throws \Dcp\Core\Exception
      * Retrieve dataSource from RoutesConfig
      */
-    private function formatRoute(\Anakeen\Router\Config\RouterInfo $route)
+    private function formatRoute(\Anakeen\Router\Config\RouterInfo $route, $pattern)
     {
         $formatedRoute = [];
         $nsName = explode('::', $route->name, 2);
@@ -42,19 +84,12 @@ class Routes
         $formatedRoute['description'] = $route->description;
 
         $formatedRoute['method'] = $route->methods[0];
-        $formatedRoute['pattern'] = is_array($route->pattern) ? implode(",", $route->pattern) : $route->pattern;
+        $formatedRoute['pattern'] = is_array($route->pattern) ? implode("\n", $route->pattern) : $route->pattern;
         $formatedRoute['priority'] = $route->priority;
-        $formatedRoute['override'] = $route->override;
-        $formatedRoute['active'] = $route->isActive();
 
         return $formatedRoute;
     }
 
-    /**
-     * @param $routes
-     * @return array
-     * reformat treeDataSource to correspond treeList content
-     */
     private function formatTreeDataSource($routes)
     {
         $route = $routes;
@@ -95,8 +130,7 @@ class Routes
                     $newId = $currentId++;
                     array_push($tree, ['id' => $newId, 'parentId' => $this->currentNameSpace, 'name' => $item['name'],
                         'description' => $item['description'], 'priority' => $item['priority'],
-                        'method' => $item['method'], 'pattern' => $item['pattern'],
-                        'override' => $item['override'], 'rowLevel' => 2, 'active' => $item['active'], 'parentName' => $item["nameSpace"]]);
+                        'method' => $item['method'], 'pattern' => $item['pattern'], 'rowLevel' => 2]);
                     $nameTab[$item['nameSpace']][$item['name']] = $newId;
                 }
             } else {
