ca558761da3e2ac9ad86a1a1cd9c9aa5fec4769f
Éric Brison <eric.brison@anakeen.com>
2018-11-28 18:42:43 +0100

    fix(test): correct suite

--- Tests/src/vendor/Anakeen/Pu/Config/SuiteConfig.php
+++ Tests/src/vendor/Anakeen/Pu/Config/SuiteConfig.php
@@ -16,7 +16,7 @@ class SuiteConfig
     {
         $suite = new FrameworkDcp();
 
-        $suite->addTestSuite(\Anakeen\Pu\Config\PuStructWorkflow::class);
+        $suite->addTestSuite(\Anakeen\Pu\Config\PuStructBasic::class);
 
         return $suite;
     }
