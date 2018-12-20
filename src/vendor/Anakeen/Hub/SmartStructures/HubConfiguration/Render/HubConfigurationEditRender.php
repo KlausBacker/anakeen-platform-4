b48cbfb478bda9027ac45bb012f7759d2b15d545
david <david.dang@anakeen.com>
2018-12-20 17:25:16 +0100

    feat(render): fields placement in element

--- src/vendor/Anakeen/Hub/SmartStructures/HubConfiguration/Render/HubConfigurationEditRender.php
+++ src/vendor/Anakeen/Hub/SmartStructures/HubConfiguration/Render/HubConfigurationEditRender.php
@@ -11,16 +11,46 @@ class HubConfigurationEditRender extends \Anakeen\Ui\DefaultConfigEditRender
     public function getOptions(\Anakeen\Core\Internal\SmartElement $document): RenderOptions
     {
         $options = parent::getOptions($document);
+
+        $break2 = "33%";
+        $break3 = "50%";
         $options->arrayAttribute(HubConfigurationFields::hub_titles)->setRowMinLimit(1);
+        $options->frame(HubConfigurationFields::hub_component_parameters)->setResponsiveColumns(
+            [
+                ["number" => 2, "minWidth" => $break2, "grow" => false],
+                ["number" => 3, "minWidth" => $break2, "grow" => false]
+            ]
+        );
+        $options->frame(HubConfigurationFields::hub_slot_parameters)->setResponsiveColumns(
+            [
+                ["number" => 2, "minWidth" => $break2, "grow" => false],
+                ["number" => 3, "minWidth" => $break2, "grow" => false]
+            ]
+        );
+        $options->frame(HubConfigurationFields::hub_config)->setResponsiveColumns(
+            [
+                ["number" => 2, "minWidth" => $break2, "grow" => false]
+            ]
+        );
+        $options->frame(HubConfigurationFields::hub_titles)->setResponsiveColumns(
+            [
+                ["number" => 2, "minWidth" => $break3, "grow" => true]
+            ]
+        );
         return $options;
     }
 
     public function getVisibilities(\Anakeen\Core\Internal\SmartElement $document, \SmartStructure\Mask $mask = null): RenderAttributeVisibilities
     {
         $visibilities = parent::getVisibilities($document, $mask);
-        $visibilities->setVisibility(HubConfigurationFields::hub_language_code, \Dcp\Ui\RenderAttributeVisibilities::HiddenVisibility);
-        $visibilities->setVisibility(HubConfigurationFields::hub_font_name, \Dcp\Ui\RenderAttributeVisibilities::HiddenVisibility);
-        $visibilities->setVisibility(HubConfigurationFields::hub_icon_name, \Dcp\Ui\RenderAttributeVisibilities::HiddenVisibility);
+        $visibilities->setVisibility(HubConfigurationFields::hub_language_code, RenderAttributeVisibilities::HiddenVisibility);
         return $visibilities;
+    }
+
+    public function getCssReferences(\Anakeen\Core\Internal\SmartElement $document = null)
+    {
+        $parent = parent::getCssReferences($document);
+        $parent["hubConfiguration"] = __DIR__."/References/HubConfiguration.css";
+        return $parent;
     }
 }
--- src/vendor/Anakeen/Hub/SmartStructures/HubConfiguration/Render/HubConfigurationViewRender.php
+++ src/vendor/Anakeen/Hub/SmartStructures/HubConfiguration/Render/HubConfigurationViewRender.php
@@ -4,6 +4,7 @@ namespace Anakeen\Hub\SmartStructures\HubConfiguration\Render;
 
 use Dcp\Ui\RenderAttributeVisibilities;
 use Dcp\Ui\RenderOptions;
+use SmartStructure\Fields\Hubconfiguration as HubConfigurationFields;
 
 class HubConfigurationViewRender extends \Anakeen\Ui\DefaultConfigViewRender
 {
@@ -11,8 +12,34 @@ class HubConfigurationViewRender extends \Anakeen\Ui\DefaultConfigViewRender
     {
         $visibilities = parent::getVisibilities($document, $mask);
         $visibilities->setVisibility("hub_language_code", \Dcp\Ui\RenderAttributeVisibilities::HiddenVisibility);
-        $visibilities->setVisibility("hub_font_name", \Dcp\Ui\RenderAttributeVisibilities::HiddenVisibility);
-        $visibilities->setVisibility("hub_icon_name", \Dcp\Ui\RenderAttributeVisibilities::HiddenVisibility);
         return $visibilities;
+    }
+
+    public function getOptions(\Anakeen\Core\Internal\SmartElement $document): RenderOptions
+    {
+        $options = parent::getOptions($document);
+
+
+        $break2 = "33%";
+        $break3 = "50%";
+        $options->arrayAttribute(HubConfigurationFields::hub_titles)->setRowMinLimit(1);
+        $options->frame(HubConfigurationFields::hub_component_parameters)->setResponsiveColumns(
+            [
+                ["number" => 2, "minWidth" => $break2, "maxWidth" => $break3],
+                ["number" => 3, "minWidth" => $break2, "grow" => false]
+            ]
+        );
+        $options->frame(HubConfigurationFields::hub_slot_parameters)->setResponsiveColumns(
+            [
+                ["number" => 2, "minWidth" => $break2, "maxWidth" => $break3],
+                ["number" => 3, "minWidth" => $break2, "grow" => false]
+            ]
+        );
+        $options->frame(HubConfigurationFields::hub_config)->setResponsiveColumns(
+            [
+                ["number" => 2, "minWidth" => $break2, "grow" => false]
+            ]
+        );
+        return $options;
     }
 }
