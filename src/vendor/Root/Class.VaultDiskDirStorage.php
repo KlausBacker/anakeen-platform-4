81ab994792a39a3de5853003a5ef36a672799702
Éric Brison <eric.brison@anakeen.com>
2019-01-02 15:10:21 +0100

    fix(vault): Correct detect free directory

--- src/vendor/Root/Class.VaultDiskDirStorage.php
+++ src/vendor/Root/Class.VaultDiskDirStorage.php
@@ -5,7 +5,7 @@
 */
 /**
  * Directories for vault files
- * @author Anakeen
+ * @author  Anakeen
  * @package FDL
  */
 // ---------------------------------------------------------------
@@ -17,6 +17,7 @@
 
 define("VAULT_MAXENTRIESBYDIR", 1000);
 define("VAULT_MAXDIRBYDIR", 100);
+
 class VaultDiskDirStorage extends \Anakeen\Core\Internal\DbObj
 {
     public $fields = array(
@@ -32,7 +33,7 @@ class VaultDiskDirStorage extends \Anakeen\Core\Internal\DbObj
     public $id_dir;
     public $id_fs;
     public $l_path;
-    
+
     public $dbtable = "vaultdiskdirstorage";
     public $order_by = "";
     public $seq = "seq_id_vaultdiskdirstorage";
@@ -48,12 +49,12 @@ class VaultDiskDirStorage extends \Anakeen\Core\Internal\DbObj
            create sequence seq_id_vaultdiskdirstorage start 10;
            CREATE INDEX vault_isfullstorage on vaultdiskdirstorage (isfull);
 SQL;
-    
+
     public $specific;
     public $isfull;
     public $size;
     protected $dirsToClose = [];
-    // --------------------------------------------------------------------
+
 
     /**
      * return name of next directory
@@ -61,8 +62,8 @@ SQL;
      * 1/10 => 2/1
      * 1/2  = 1/3
      *
-     * @param string   $d path  to file
-     * @param int      $max
+     * @param string $d path  to file
+     * @param int    $max
      *
      * @return string
      */
@@ -76,7 +77,6 @@ SQL;
             $td[$lastkey]++;
         } else {
             $good = false;
-            ;
             $key = $lastkey;
             while (($key >= 0) && (!$good)) {
                 $prev = intval(prev($td));
@@ -95,37 +95,42 @@ SQL;
         }
         return implode('/', $td);
     }
-    
+
     public function complete()
     {
         $this->isfull = ($this->isfull === 't');
     }
-    // --------------------------------------------------------------------
-    public function SetFreeDir($fs)
+
+
+    public function setFreeDir($fs)
     {
-        // --------------------------------------------------------------------
         $query = new \Anakeen\Core\Internal\QueryDb($this->dbaccess, $this->dbtable);
         $id_fs = $fs["id_fs"];
         $query->basic_elem->sup_where = array(
             "id_fs=" . $id_fs,
-            "not isfull"
+            "not isfull or isfull is null"
         );
         $query->order_by = "id_dir";
         // Lock directory : force each process to use its proper dir
-        $sql = sprintf("select * from %s where id_fs=%d and not isfull and pg_try_advisory_xact_lock(id_dir, %d) order by id_fs limit 1 for update;", pg_escape_identifier($this->dbtable), $id_fs, unpack("i", "VLCK") [1]);
-        
+        $sql = sprintf(
+            "select * from %s where id_fs=%d and not isfull and pg_try_advisory_xact_lock(id_dir, %d) order by id_fs limit 1 for update;",
+            pg_escape_identifier($this->dbtable),
+            $id_fs,
+            unpack("i", "VLCK") [1]
+        );
+
         $err = "";
         $dirs = $query->Query(0, 0, "TABLE", $sql);
-        
+
         $this->dirsToClose = [];
         if ($query->nb > 0) {
             $needNewOneDir = true;
             foreach ($dirs as $dir) {
                 $this->select($dir["id_dir"]);
-                
+
                 $sql = sprintf("SELECT count(*) FROM vaultdiskstorage WHERE id_dir=%d", $this->id_dir);
                 $t = $query->Query(0, 0, "TABLE", $sql);
-                
+
                 $count = intval($t[0]["count"]);
                 if ($count >= (VAULT_MAXENTRIESBYDIR - 1)) {
                     $this->dirsToClose[] = $this->id_dir;
@@ -146,7 +151,7 @@ SQL;
         }
         return $err;
     }
-    
+
     public function closeDir()
     {
         $err = '';
@@ -159,14 +164,14 @@ SQL;
                     $this->select($dirid);
                     $this->isfull = 't';
                     $this->size = $t[0]["sum"];
-                    $err.= $this->modify();
+                    $err .= $this->modify();
                 }
             }
         }
         $this->dirsToClose = [];
         return $err;
     }
-    
+
     protected function createDirectory($fs)
     {
         $id_fs = $fs["id_fs"];
@@ -175,14 +180,14 @@ SQL;
         $lpath = $t[0]["l_path"];
         $npath = $this->nextdir($lpath);
         $rpath = $fs["r_path"];
-        
+
         $absDir = sprintf("%s/%s", $rpath, $npath);
-        
+
         while (is_dir($absDir)) {
             $npath = $this->nextdir($npath);
             $absDir = sprintf("%s/%s", $rpath, $npath);
         }
-        
+
         $this->id_dir = "";
         $this->id_fs = $id_fs;
         $this->l_path = $npath;
@@ -200,10 +205,10 @@ SQL;
         }
         return $err;
     }
-    // --------------------------------------------------------------------
-    public function PreInsert()
+
+
+    public function preInsert()
     {
-        // --------------------------------------------------------------------
         if ($this->Exists($this->l_path, $this->id_fs)) {
             return (_("Directory already exists"));
         }
@@ -212,10 +217,11 @@ SQL;
         $this->id_dir = $arr["nextval"];
         return '';
     }
-    // --------------------------------------------------------------------
-    public function Exists($path, $id_fs)
+
+
+    public function exists($path, $id_fs)
     {
-        // --------------------------------------------------------------------
+
         $query = new \Anakeen\Core\Internal\QueryDb($this->dbaccess, $this->dbtable);
         $query->basic_elem->sup_where = array(
             "l_path='" . $path . "'",
@@ -224,8 +230,9 @@ SQL;
         $query->Query(0, 0, "TABLE");
         return ($query->nb > 0);
     }
-    // --------------------------------------------------------------------
-    public function DelEntry()
+
+
+    public function delEntry()
     {
         if ($this->isfull) {
             $this->isfull = false;
