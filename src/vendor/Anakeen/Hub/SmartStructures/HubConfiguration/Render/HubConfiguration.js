b04ae404451a83d7641886c1f2d1ec4f3746b76b
david <david.dang@anakeen.com>
2019-01-11 10:57:54 +0100

    feat(hubConfiguration): add event listeners for hub configuration smart elements

--- src/vendor/Anakeen/Hub/SmartStructures/HubConfiguration/Render/HubConfiguration.js
+++ src/vendor/Anakeen/Hub/SmartStructures/HubConfiguration/Render/HubConfiguration.js
@@ -1,9 +1,5 @@
 import "./HubConfiguration.css";
 
-require("@fonticonpicker/fonticonpicker")($);
-import "@fonticonpicker/fonticonpicker/dist/css/base/jquery.fonticonpicker.min.css";
-import "@fonticonpicker/fonticonpicker/dist/css/themes/bootstrap-theme/jquery.fonticonpicker.bootstrap.min.css";
-
 window.dcp.document.documentController(
   "addEventListener",
   "attributeReady",
@@ -13,37 +9,29 @@ window.dcp.document.documentController(
       return documentObject.renderMode === "view";
     },
     attributeCheck: attributeObject => {
-      return attributeObject.id === "hub_icon";
+      return attributeObject.id === "hub_icon_enum";
     }
   },
-  (event, documentObject, attributeObject, $el) => {
-    $el[0].lastElementChild.firstElementChild.innerHTML =
-      $el[0].lastElementChild.firstElementChild.textContent;
-  }
-);
-
-window.dcp.document.documentController(
-  "addEventListener",
-  "attributeReady",
-  {
-    name: "hub:configuration:edit:ready",
-    documentCheck: documentObject => {
-      return documentObject.renderMode === "edit";
-    },
-    attributeCheck: attributeObject => {
-      return attributeObject.id === "hub_icon";
+  function() {
+    switch ($(this).documentController("getValue", "hub_icon_enum").value) {
+      case "HTML":
+        console.log(
+          $(this).documentController("getValue", "hub_icon_text").value
+        );
+        break;
+      case "IMAGE":
+        console.log(
+          $(this).documentController("getValue", "hub_icon_image").value
+        );
+        break;
+      case "FONT":
+        console.log(
+          $(this).documentController("getValue", "hub_icon_font").value
+        );
+        break;
+      default:
+        break;
     }
-  },
-  function(event, documentObject, attributeObject, $el) {
-    $el.find(".iconPicker").fontIconPicker({
-      theme: "fip-bootstrap",
-      useAttribute: true,
-      iconGenerator: icon => {
-        return `<i class="fa fa-${icon}"></i>`;
-      },
-      convertToHex: false
-    });
-    $el.find(".iconPicker").on("change", () => {});
   }
 );
 
@@ -61,5 +49,127 @@ window.dcp.document.documentController(
       initid: currentDocumentObject.id,
       viewId: "!defaultConsultation"
     });
+  }
+);
+
+window.dcp.document.documentController(
+  "addEventListener",
+  "change",
+  {
+    name: "icon:mode:changed",
+    documentCheck: documentObject => {
+      return documentObject.renderMode === "edit";
+    },
+    attributeCheck: attributeObject => {
+      return attributeObject.id === "hub_icon_enum";
+    }
+  },
+  function(event, documentObject, attributeObject, $el) {
+    switch ($el.current.value) {
+      case "IMAGE":
+        $(".dcpAttribute[data-attrid='hub_icon_font']")[0].style.display =
+          "none";
+        $(".dcpAttribute[data-attrid='hub_icon_text']")[0].style.display =
+          "none";
+        $(".dcpAttribute[data-attrid='hub_icon_image']")[0].style.display =
+          "inline";
+        break;
+      case "FONT":
+        $(".dcpAttribute[data-attrid='hub_icon_image']")[0].style.display =
+          "none";
+        $(".dcpAttribute[data-attrid='hub_icon_text']")[0].style.display =
+          "none";
+        $(".dcpAttribute[data-attrid='hub_icon_font']")[0].style.display =
+          "inline";
+        break;
+      case "HTML":
+        $(".dcpAttribute[data-attrid='hub_icon_font']")[0].style.display =
+          "none";
+        $(".dcpAttribute[data-attrid='hub_icon_image']")[0].style.display =
+          "none";
+        $(".dcpAttribute[data-attrid='hub_icon_text']")[0].style.display =
+          "inline";
+        break;
+      default:
+        break;
+    }
+  }
+);
+
+window.dcp.document.documentController(
+  "addEventListener",
+  "attributeReady",
+  {
+    name: "icon:image:ready",
+    documentCheck: documentObject => {
+      return (
+        documentObject.renderMode === "edit" ||
+        documentObject.renderMode === "view"
+      );
+    },
+    attributeCheck: attributeObject => {
+      return attributeObject.id === "hub_icon_image";
+    }
+  },
+  function(event, documentObject, attributeObject, $el) {
+    if (
+      $(this).documentController("getValue", "hub_icon_enum").value !== "IMAGE"
+    ) {
+      $el[0].style.display = "none";
+    } else {
+      $el[0].style.display = "inline";
+    }
+  }
+);
+
+window.dcp.document.documentController(
+  "addEventListener",
+  "attributeReady",
+  {
+    name: "icon:text:ready",
+    documentCheck: documentObject => {
+      return (
+        documentObject.renderMode === "edit" ||
+        documentObject.renderMode === "view"
+      );
+    },
+    attributeCheck: attributeObject => {
+      return attributeObject.id === "hub_icon_text";
+    }
+  },
+  function(event, documentObject, attributeObject, $el) {
+    if (
+      $(this).documentController("getValue", "hub_icon_enum").value !== "HTML"
+    ) {
+      $el[0].style.display = "none";
+    } else {
+      $el[0].style.display = "inline";
+    }
+  }
+);
+
+window.dcp.document.documentController(
+  "addEventListener",
+  "attributeReady",
+  {
+    name: "icon:font:ready",
+    documentCheck: documentObject => {
+      return (
+        documentObject.renderMode === "edit" ||
+        documentObject.renderMode === "view"
+      );
+    },
+    attributeCheck: attributeObject => {
+      return attributeObject.id === "hub_icon_font";
+    }
+  },
+  function(event, documentObject, attributeObject, $el) {
+    if (
+      $(this).documentController("getValue", "hub_icon_enum").value !== "FONT"
+    ) {
+      $el[0].style.display = "none";
+    } else {
+      $el[0].style.display = "inline";
+    }
   }
 );
