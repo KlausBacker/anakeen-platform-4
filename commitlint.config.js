6e5ce163338684f64c86aa31520d4d40908a3a60
Charles Bonnissent <charles.bonnissent@anakeen.com>
2019-01-21 15:09:53 +0100

    build: add semver and commit lint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,11 @@
 const Configuration = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "scope-enum": [2, "always", ["config", "route", "i18n", "script"]]
+    "scope-enum": [
+      2,
+      "always",
+      ["config", "route", "i18n", "script", "component"]
+    ]
   }
 };
 
