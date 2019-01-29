f190efaa5c51b0a32adf5e2c201e711ef6176a8a
david <david.dang@anakeen.com>
2019-01-29 16:33:17 +0100

    feat(script): check if language is not null

---
+++ src/vendor/Anakeen/Hub/SmartStructures/HubConfiguration/HubConfigurationChecking/HubConfigurationChecking.php
@@ -0,0 +1,10 @@
+<?php
+namespace Anakeen\Hub\SmartStructures\HubConfiguration\HubConfigurationChecking;
+
+class HubConfigurationChecking
+{
+    public static function checkTitle($title,$language)
+    {
+        return $title && $language;
+    }
+}
