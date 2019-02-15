3cf044d2fba049e71ee14cc4c7a59dfd4d612efd
david <david.dang@anakeen.com>
2019-02-15 13:54:33 +0100

    fix(component): fix label for new organization refs #6

--- src/vendor/Anakeen/Hub/SmartStructures/HubConfigurationLabel/HubConfigurationLabelBehavior.php
+++ src/vendor/Anakeen/Hub/SmartStructures/HubConfigurationLabel/HubConfigurationLabelBehavior.php
@@ -4,11 +4,26 @@ namespace Anakeen\Hub\SmartStructures\HubConfigurationLabel;
 
 use SmartStructure\Fields\Hubconfigurationlabel as HubConfigurationLabelFields;
 
-class HubConfigurationLabelBehavior extends \SmartStructure\Hubconfiguration
+class HubConfigurationLabelBehavior extends \SmartStructure\Hubconfigurationslot
 {
 
     public function registerHooks()
     {
         parent::registerHooks();
+    }
+
+    /**
+     * @return array
+     * @throws \Anakeen\Exception
+     */
+    protected function getComponentConfiguration()
+    {
+        return [
+            // Name of the Vue.js component
+            "name" => "hub-label",
+
+            // Properties to use for the components
+            "props" => ["label" => $this->getAttributeValue("label")]
+        ];
     }
 }
