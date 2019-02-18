b6f5f9d57318766566cd850419d59e92a849fd14
Aurélien Brugalières <aurelien.brugalieres@anakeen.com>
2019-02-18 15:21:07 +0100

    fix: add external asset support for component refs #6

--- src/vendor/Anakeen/Hub/IHM/Components/HelloWorld/MyComponent.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <i class="fa fa-edit"></i>
-</template>
--- src/vendor/Anakeen/Hub/IHM/Components/HelloWorld/index.js
+++
@@ -1,7 +0,0 @@
-import HelloWorld from "./HelloWorld.vue";
-import MyComponent from "./MyComponent.vue";
-
-export default function install(Vue) {
-  Vue.component("hello-world", HelloWorld);
-  Vue.component("hello-icon", MyComponent);
-}
