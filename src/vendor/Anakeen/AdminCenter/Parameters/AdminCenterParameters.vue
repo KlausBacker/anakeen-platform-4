680ccd855700cf3e47ab16ffe7f54782aa067d79
david <david.dang@anakeen.com>
2019-02-26 16:03:44 +0100

    fix(component): remove colors and add switch button refs #18

--- src/vendor/Anakeen/AdminCenter/Parameters/AdminCenterParameters.vue
+++ src/vendor/Anakeen/AdminCenter/Parameters/AdminCenterParameters.vue
@@ -1,13 +1,20 @@
 <template>
-    <div class="parameter-plugin">
-        <admin-center-global-parameters v-if="globalParameters" @switchParameters="switchParameters"></admin-center-global-parameters>
-        <admin-center-user-parameters v-else @switchParameters="switchParameters"></admin-center-user-parameters>
-    </div>
+  <div class="parameter-plugin">
+    <admin-center-global-parameters
+      v-if="globalParameters"
+      @switchParameters="switchParameters"
+      :switch="globalParameters"
+    ></admin-center-global-parameters>
+    <admin-center-user-parameters
+      v-else
+      @switchParameters="switchParameters"
+      :switch="globalParameters"
+    ></admin-center-user-parameters>
+  </div>
 </template>
 
 <style>
 @import "./AdminCenterParameters.css";
 </style>
 
-<script src="./AdminCenterParameters.controller.ts" lang="ts">
-</script>
+<script src="./AdminCenterParameters.controller.ts" lang="ts"></script>
