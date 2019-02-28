4fd151b60e99576c89b20ba867f8a5ef6a87b5d6
Aurélien Brugalières <aurelien.brugalieres@anakeen.com>
2019-02-28 15:06:45 +0100

    fix(config): use hub station page to render admin page

--- src/vendor/Anakeen/Routes/Admin/MainPage.php
+++ src/vendor/Anakeen/Routes/Admin/MainPage.php
@@ -2,75 +2,13 @@
 
 namespace Anakeen\Routes\Admin;
 
-use Anakeen\Ui\UIGetAssetPath;
-
-class MainPage
+class MainPage extends \Anakeen\Hub\Routes\Hub
 {
-
-    /**
-     * @param \Slim\Http\request $request
-     * @param \Slim\Http\response $response
-     * @param $args
-     * @return \Slim\Http\Response
-     * @throws \Anakeen\Ui\Exception
-     */
-    public function __invoke(\Slim\Http\request $request, \Slim\Http\response $response, $args)
-    {
-        $page=__DIR__."/../Layout/adminCenterMainPage.html.mustache";
-        $mustache = new \Mustache_Engine();
-        $data = [
-            "JS_DEPS" => [
-                [
-                    "key" => "jquery",
-                    "path" => UIGetAssetPath::getJSJqueryPath()
-                ],
-                [
-                    "key" => "kendo",
-                    "path" => UIGetAssetPath::getJSKendoPath()
-                ]
-            ],
-            "JS" => [
-                [
-                    "key" => "adminCenter",
-                    "path" => UIGetAssetPath::getElementAssets(
-                        "adminCenter",
-                        UIGetAssetPath::isInDebug() ? "dev" : "prod"
-                    )["adminCenter"]["js"]
-                ]
-            ],
-            "JS_LEGACY" => [
-                [
-                    "key" => "polyfill",
-                    "path" => UIGetAssetPath::getPolyfill()
-                ],
-                [
-                    "key" => "adminCenter",
-                    "path" => UIGetAssetPath::getElementAssets(
-                        "adminCenter",
-                        "legacy"
-                    )["adminCenter"]["js"]
-                ]
-            ],
-            "CSS" => [
-                [
-                    "key" => "bootstrap",
-                    "path" => UIGetAssetPath::getCssBootstrap()
-                ],
-                [
-                    "key" => "kendo",
-                    "path" => UIGetAssetPath::getCssKendo()
-                ],
-                [
-                    "key" => "component",
-                    "path" => UIGetAssetPath::getCssSmartWebComponents()
-                ],
-                [
-                    "key" => "admincenter",
-                    "path" => "/Anakeen/adminCenter/assets/adminCenter.css"
-                ]
-            ]
-        ];
-        $template = file_get_contents($page);
-        return $response->write($mustache->render($template, $data));
+    protected function getHubInstanceId(
+        \Slim\Http\request $request,
+        \Slim\Http\response $response,
+        $args
+    ) {
+        return "ADMINCENTER";
     }
 }
