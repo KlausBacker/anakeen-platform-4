dbf0e83b84bb043d0636c6483315a9b99782bb59
david <david.dang@anakeen.com>
2019-03-06 17:14:28 +0100

    fix(component): fix grids and splitters refs #17

--- src/vendor/Anakeen/AdminCenter/Account/AdminCenterAccount.vue
+++ src/vendor/Anakeen/AdminCenter/Account/AdminCenterAccount.vue
@@ -1,77 +1,145 @@
 <template>
-    <div class="accountManager" >
-        <div class="accountManager_wrapper" ref="accountManager_wrapper">
-            <div v-show="!userModeSelected" class="accountManager_gridAndTreePart" ref="gridAndTreePart">
-                <div ref="treeViewPart">
-                    <div class="accountManager_treeViewPart_wrapper">
-                        <div class="accountManager_treeViewPart_wrapper_topPart">
-                            <div class="accountManager_treeViewPart_wrapper_topPart_btnPart">
-                                <kendo-toolbar>
-                                    <kendo-toolbar-item type="button" icon="refresh" @click="() => updateTreeData(true)"></kendo-toolbar-item>
-                                    <kendo-toolbar-item type="button" icon="sort-asc-sm" @click="collapseAll"></kendo-toolbar-item>
-                                    <kendo-toolbar-item type="button" icon="sort-desc-sm" @click="expandAll"></kendo-toolbar-item>
-                                </kendo-toolbar>
-                            </div>
-                            <div class="accountManager_treeViewPart_wrapper_searchPart">
-                                <form @submit="filterGroup">
-                                    <input type="text" ref="filterTree" class="form-control" placeholder="Search..."/>
-                                </form>
-                            </div>
-                        </div>
-                        <div class="accountManager_treeViewPart_wrapper_content">
-                            <kendo-treeview ref="groupTreeView"
-                                            :data-source="groupTree"
-                                            :template="'#= item.title # (#= item.nbUser #)'"
-                                            @change="onGroupSelect"
-                                            @expand="registerTreeState"
-                                            @collapse="registerTreeState">
-                            </kendo-treeview>
-                        </div>
-                    </div>
+  <div class="accountManager">
+    <div class="accountManager_wrapper" ref="accountManager_wrapper">
+      <ank-splitter
+        ref="accountTreeSplitter"
+        class="account-manager-splitter"
+        :panes="panes"
+        localStorageKey="account-manager-tree-splitter"
+      >
+        <template slot="left">
+          <div ref="treeViewPart">
+            <div class="accountManager_treeViewPart_wrapper">
+              <div class="accountManager_treeViewPart_wrapper_topPart">
+                <div
+                  class="accountManager_treeViewPart_wrapper_topPart_btnPart"
+                >
+                  <kendo-toolbar>
+                    <kendo-toolbar-item
+                      type="button"
+                      icon="refresh"
+                      @click="() => updateTreeData(true)"
+                    ></kendo-toolbar-item>
+                    <kendo-toolbar-item
+                      type="button"
+                      icon="sort-asc-sm"
+                      @click="collapseAll"
+                    ></kendo-toolbar-item>
+                    <kendo-toolbar-item
+                      type="button"
+                      icon="sort-desc-sm"
+                      @click="expandAll"
+                    ></kendo-toolbar-item>
+                  </kendo-toolbar>
                 </div>
-                <div class="accountManager_contentPart" ref="centerPart">
-                    <div  class="accountManager_contentPart_groupPart" ref="documentPart">
-                        <div v-show="displayGroupDocument" class="accountManager_contentPart_groupPart_wrapper">
-                            <kendo-toolbar ref="groupToolbar">
-                                <kendo-toolbar-item type="button" text="Change group" @click="openChangeGroup"></kendo-toolbar-item>
-                            </kendo-toolbar>
-                            <ank-smart-element ref="groupDoc"></ank-smart-element>
-                        </div>
-                        <div v-show="!displayGroupDocument" class="accountManager_logo">
-                            <div><img class="anakeen-logo" src="/Images/anakeen-logo.svg" alt="Anakeen Logo"/></div>
-                        </div>
-                    </div>
-                    <div class="accountManager_contentPart_gridPart">
-                        <kendo-toolbar ref="userToolbar">
-                        </kendo-toolbar>
-                        <kendo-grid ref="grid"
-                                    :data-source="gridContent"
-                                    :pageable="{'alwaysVisible': true, pageSizes: [10, 20, 100]}"
-                                    :sortable='true'
-                                    :filterable="{ extra: false, operators: { string: { contains: 'contains' } } }"
-                                    :persistSelection="true"
-                                    :autoBind="false">
-                            <kendo-grid-column :selectable="true" width="50px"></kendo-grid-column>
-                            <kendo-grid-column field="id" template='<a class="btn btn-light material-icons openButton" data-initid="#= id #" href="/api/v2/smart-elements/#= id #.html" >open_in_new</a>' width="50px" title=" " :filterable=false ></kendo-grid-column>
-                            <kendo-grid-column field="login" title="Login" type="string"></kendo-grid-column>
-                            <kendo-grid-column field="firstname" title="First name" type="string"></kendo-grid-column>
-                            <kendo-grid-column field="lastname" title="Last name" type="string"></kendo-grid-column>
-                            <kendo-grid-column field="mail" title="E-mail" type="string"></kendo-grid-column>
-                        </kendo-grid>
-                    </div>
+                <div class="accountManager_treeViewPart_wrapper_searchPart">
+                  <form @submit="filterGroup">
+                    <input
+                      type="text"
+                      ref="filterTree"
+                      class="form-control"
+                      placeholder="Search..."
+                    />
+                  </form>
                 </div>
+              </div>
+              <div class="accountManager_treeViewPart_wrapper_content">
+                <kendo-treeview
+                  ref="groupTreeView"
+                  :data-source="groupTree"
+                  :template="'#= item.title # (#= item.nbUser #)'"
+                  @change="onGroupSelect"
+                  @expand="registerTreeState"
+                  @collapse="registerTreeState"
+                >
+                </kendo-treeview>
+              </div>
             </div>
-            <div v-show="userModeSelected" class="accountManager_userPart">
-                <kendo-toolbar>
-                    <kendo-toolbar-item type="button" icon="arrow-double-60-left" @click="toggleUserMode"></kendo-toolbar-item>
+          </div>
+        </template>
+        <template slot="right">
+          <ank-splitter
+            ref="accountSplitter"
+            class="account-manager-splitter"
+            :panes="panes"
+            localStorageKey="account-manager-splitter"
+          >
+            <template slot="left">
+              <div class="accountManager_contentPart_gridPart">
+                <kendo-toolbar ref="userToolbar">
+                  <kendo-toolbar-item
+                    type="button"
+                    text="Change group"
+                    @click="openChangeGroup"
+                  ></kendo-toolbar-item>
+                  <kendo-toolbar-item
+                    type="button"
+                    text="Open group"
+                    @click="openGroup"
+                  ></kendo-toolbar-item>
                 </kendo-toolbar>
-                <ank-smart-element ref="openDoc"></ank-smart-element>
-            </div>
-        </div>
+                <kendo-grid
+                  ref="grid"
+                  class="user-grid"
+                  :data-source="gridContent"
+                  :pageable="{ alwaysVisible: true, pageSizes: [10, 20, 100] }"
+                  :sortable="true"
+                  :filterable="{
+                    extra: false,
+                    operators: { string: { contains: 'contains' } }
+                  }"
+                  :persistSelection="true"
+                  :autoBind="false"
+                >
+                  <kendo-grid-column
+                    :selectable="true"
+                    width="50px"
+                  ></kendo-grid-column>
+                  <kendo-grid-column
+                    field="id"
+                    :hidden="true"
+                  ></kendo-grid-column>
+                  <kendo-grid-column
+                    field="login"
+                    title="Login"
+                    type="string"
+                  ></kendo-grid-column>
+                  <kendo-grid-column
+                    field="firstname"
+                    title="First name"
+                    type="string"
+                  ></kendo-grid-column>
+                  <kendo-grid-column
+                    field="lastname"
+                    title="Last name"
+                    type="string"
+                  ></kendo-grid-column>
+                  <kendo-grid-column
+                    field="mail"
+                    title="E-mail"
+                    type="string"
+                  ></kendo-grid-column>
+                  <kendo-grid-column
+                    :command="{ text: 'Consult', click: openUser }"
+                    :filterable="false"
+                    :width="'10rem'"
+                  ></kendo-grid-column>
+                </kendo-grid>
+              </div>
+            </template>
+            <template slot="right">
+              <ank-smart-element
+                ref="openDoc"
+                class="open-element-content"
+              ></ank-smart-element>
+            </template>
+          </ank-splitter>
+        </template>
+      </ank-splitter>
     </div>
+  </div>
 </template>
 <style>
 @import "./AdminCenterAccount.css";
 </style>
-<script src="./AdminCenterAccount.controller.ts" lang="ts">
-</script>
+<script src="./AdminCenterAccount.controller.ts" lang="ts"></script>
