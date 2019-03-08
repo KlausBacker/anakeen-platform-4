a4350f67d73789d257cac4d06b8157390c54a949
Éric Brison <eric.brison@anakeen.com>
2019-03-08 10:37:07 +0100

    build: update webpack dependances

---
+++ stubs/HUBADMINCENTERVAULTVUE__STUB.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace SmartStructure {
+
+    class Hubadmincentervaultvue extends \Anakeen\AdminCenter\SmartStructures\HubAdminVaultManager\HubAdminVaultManagerVueBehavior
+    {
+        const familyName = "HUBADMINCENTERVAULTVUE";
+    }
+}
+
+namespace SmartStructure\Fields {
+
+    class Hubadmincentervaultvue extends Hubconfigurationvue
+    {
+
+    }
+}
