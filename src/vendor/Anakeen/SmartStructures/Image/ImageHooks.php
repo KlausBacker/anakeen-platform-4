cff67b5099a199dbbb55795ed1b7348319b134cd
Éric Brison <eric.brison@anakeen.com>
2019-03-19 17:30:51 +0100

    fix(internal): correct url for icon recorded in vault (refs #9)

--- src/vendor/Anakeen/SmartStructures/Image/ImageHooks.php
+++ src/vendor/Anakeen/SmartStructures/Image/ImageHooks.php
@@ -12,6 +12,7 @@
 namespace Anakeen\SmartStructures\Image;
 
 use Anakeen\SmartHooks;
+use SmartStructure\Fields\Image as ImageFields;
 
 class ImageHooks extends \Anakeen\SmartElement
 {
@@ -19,7 +20,13 @@ class ImageHooks extends \Anakeen\SmartElement
     {
         parent::registerHooks();
         $this->getHooks()->addListener(SmartHooks::POSTSTORE, function () {
-            return $this->setValue("IMG_TITLE", $this->vaultFilename("IMG_FILE"));
+            return $this->setValue("IMG_TITLE", $this->vaultFilename(ImageFields::img_file));
         });
+
+        /*
+        $this->getHooks()->addListener(SmartHooks::PRESTORE, function () {
+            $this->icon = $this->getRawValue(ImageFields::img_file);
+        });
+        */
     }
 }
