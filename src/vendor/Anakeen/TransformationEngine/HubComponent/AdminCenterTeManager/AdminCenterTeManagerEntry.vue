6e35ba0266343a664ef87e28f7b4faedbc0404c1
Éric Brison <eric.brison@anakeen.com>
2019-04-04 17:18:53 +0200

    fix(internal): async load component

--- src/vendor/Anakeen/TransformationEngine/HubComponent/AdminCenterTeManager/AdminCenterTeManagerEntry.vue
+++ src/vendor/Anakeen/TransformationEngine/HubComponent/AdminCenterTeManager/AdminCenterTeManagerEntry.vue
@@ -1,30 +1,38 @@
 <template>
-    <div>
-        <nav v-if="isDockCollapsed">
-            <i class="material-icons hub-icon">file_copy</i>
-        </nav>
-        <nav v-else-if="isDockExpanded">
-            <i class="material-icons hub-icon">file_copy</i> <span> Transformation File Manager</span>
-        </nav>
-        <div v-else-if="isHubContent" class="hub-te-manager">
-            <admin-center-vault></admin-center-vault>
-        </div>
+  <div>
+    <nav v-if="isDockCollapsed">
+      <i class="material-icons hub-icon">file_copy</i>
+    </nav>
+    <nav v-else-if="isDockExpanded">
+      <i class="material-icons hub-icon">file_copy</i>
+      <span> Transformation File Manager</span>
+    </nav>
+    <div v-else-if="isHubContent" class="hub-te-manager">
+      <admin-center-te></admin-center-te>
     </div>
+  </div>
 </template>
 <script>
-  import TeManager from "../TeManager/TeManager.vue";
-  import HubElement  from "@anakeen/hub-components/components/lib/HubElement";
+import HubElement from "@anakeen/hub-components/components/lib/HubElement";
+import Vue from "vue";
 
-  export default {
-    name: "ank-admin-te-manager",
-    extends: HubElement, // ou mixins: [ HubElementMixins ],
-    components: {
-      "admin-center-vault": TeManager
+export default {
+  name: "ank-admin-te-manager",
+  extends: HubElement, // ou mixins: [ HubElementMixins ],
+
+  created() {
+    if (this.isHubContent) {
+      Vue.component("admin-center-te", resolve => {
+        import("../TeManager/TeManager.vue").then(Component => {
+          resolve(Component.default);
+        });
+      });
     }
   }
+};
 </script>
 <style>
-    .hub-te-manager {
-        height:100%;
-    }
+.hub-te-manager {
+  height: 100%;
+}
 </style>
