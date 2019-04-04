432f9e52485032509c3efdcae47479d6ca24e8b2
Éric Brison <eric.brison@anakeen.com>
2019-04-04 17:12:49 +0200

    fix(i18n): correct msg for privilges

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,7 +1,7 @@
 const Configuration = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'scope-enum': [2, 'always', ['config', 'route', 'internal', 'script']]
+    'scope-enum': [2, 'always', ['config', 'i18n', 'route', 'internal', 'script']]
   }
 };
 
