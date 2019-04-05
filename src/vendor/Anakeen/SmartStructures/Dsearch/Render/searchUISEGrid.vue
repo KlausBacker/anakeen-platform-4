dd6abdd7447aee4bcdc9352eb229e7b8de92e2f4
David Dang <david.dang@anakeen.com>
2019-04-05 12:07:40 +0200

    fix(component): fix grid display on reports element

--- src/vendor/Anakeen/SmartStructures/Dsearch/Render/searchUISEGrid.vue
+++ src/vendor/Anakeen/SmartStructures/Dsearch/Render/searchUISEGrid.vue
@@ -1,18 +1,21 @@
 <template>
     <div class="grid-parent">
-        <ank-se-grid v-if="searchId" url-config="/api/v2/smartstructures/dsearch/gridConfig/<collection>" :server-paging="true"
-                     :server-sorting="true" :server-filtering="true" :collection="searchId" class="search-grid" :contextTitles="false" ref="searchGrid">
+        <ank-se-grid v-if="searchId" url-config="/api/v2/smartstructures/dsearch/gridConfig/<collection>"
+                     :server-paging="true"
+                     :server-sorting="true" :server-filtering="true" :collection="searchId" class="search-grid"
+                     :contextTitles="false" ref="searchGrid">
         </ank-se-grid>
     </div>
 </template>
 <script>
   import AnkSEGrid from "../../../../../../components/lib/AnkSEGrid";
+
   export default {
     name: "SearchUISEGrid",
     components: {
       "ank-se-grid": AnkSEGrid
     },
-    mounted() {
+    created() {
       const that = this;
       window.dcp.document.documentController(
         "addEventListener",
@@ -20,20 +23,35 @@
         {
           name: "getTmpSearchId",
           documentCheck: function isDsearch(document) {
-            return document.renderMode === "edit" && document.type === "search";
+            return document.type === "search";
           }
         },
         function prepareResultEditEvents(event, data) {
-            that.searchId = data.id.toString();
-            if (that.$refs.searchGrid) {
-              that.$refs.searchGrid.privateScope.initGrid();
-            }
+          that.searchId = data.id.toString();
+          if (that.$refs.searchGrid) {
+            that.$refs.searchGrid.privateScope.initGrid();
+          }
+        });
+      window.dcp.document.documentController(
+        "addEventListener",
+        "custom:content:view",
+        {
+          name: "getTmpViewId",
+          documentCheck: function isReport(document) {
+            return document.renderMode === "view" && document.type === "search";
+          }
+        },
+        function prepareResultViewEvents(event, data) {
+          that.searchId = data.id.toString();
+          if (that.$refs.searchGrid) {
+            that.$refs.searchGrid.privateScope.initGrid();
+          }
         });
     },
     data() {
       return {
         searchId: null
-      }
+      };
     }
   };
 </script>
@@ -41,6 +59,5 @@
     .grid-parent {
         height: 100%;
         width: 100%;
-        background-color: red;
     }
 </style>
