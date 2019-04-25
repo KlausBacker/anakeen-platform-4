728669ff453cc731dafe8af88c46ebd6b4527b0c
Éric Brison <eric.brison@anakeen.com>
2019-04-25 11:23:40 +0200

    build: update generate file

--- kendo-shims.d.ts
+++ kendo-shims.d.ts
@@ -1,10 +1,10 @@
 declare namespace kendo {
-  export const jQuery: JQueryStatic
+  export const jQuery: JQueryStatic;
 }
 
 declare namespace kendo.ui {
   class KendoPopup extends kendo.ui.Popup {
-      toggle(): void;
+    toggle(): void;
   }
 }
 
