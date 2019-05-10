dc41f15c43299711d118d658efdc74e99a4091cf
Aurélien Brugalières <aurelien.brugalieres@anakeen.com>
2019-05-10 10:36:47 +0200

    fix(component): lint files

--- src/vendor/Anakeen/Hub/IHM/Components/Hub/HubComponentStatus/HubComponentError.vue
+++ src/vendor/Anakeen/Hub/IHM/Components/Hub/HubComponentStatus/HubComponentError.vue
@@ -1,63 +1,71 @@
 <template>
-    <div :class="`ank-hub-component-error ank-hub-component-error--${displayType}`">
-        <i class="ank-hub-component-error-icon fa fa-warning" :title="titleMessage"></i>
-        <span class="ank-hub-component-error-message" v-if="displayType==='CONTENT'">{{errorMessage}}</span>
-    </div>
+  <div
+    :class="`ank-hub-component-error ank-hub-component-error--${displayType}`"
+  >
+    <i
+      class="ank-hub-component-error-icon fa fa-warning"
+      :title="titleMessage"
+    ></i>
+    <span
+      class="ank-hub-component-error-message"
+      v-if="displayType === 'CONTENT'"
+      >{{ errorMessage }}</span
+    >
+  </div>
 </template>
 <!-- CSS to this component only -->
 <style scoped lang="scss">
-    .ank-hub-component-error {
-        width: 100%;
-        height: 100%;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        .ank-hub-component-error-icon {
-            font-size: 2rem;
-            color: red;
-        }
+.ank-hub-component-error {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .ank-hub-component-error-icon {
+    font-size: 2rem;
+    color: red;
+  }
 
-        &.ank-hub-component-error--CONTENT {
-            .ank-hub-component-error-icon {
-                font-size: 5rem;
-            }
-            .ank-hub-component-error-message {
-                font-size: 2rem;
-            }
-        }
+  &.ank-hub-component-error--CONTENT {
+    .ank-hub-component-error-icon {
+      font-size: 5rem;
     }
+    .ank-hub-component-error-message {
+      font-size: 2rem;
+    }
+  }
+}
 </style>
 <!-- Global CSS -->
-<style lang="scss">
-</style>
+<style lang="scss"></style>
 <script>
-    export default {
-      props: {
-        title: {
-          default: "Le composant n'a pas pu être chargé"
-        },
-        displayType: {
-          type: String
-        },
-        entryOptions: {
-          type: Object,
-          default: () => null
-        }
-      },
-      data() {
-        return {
-          titleMessage: this.title,
-          errorMessage: this.title
-        }
-      },
-      mounted() {
-        if (this.entryOptions && this.entryOptions.name) {
-          this.errorMessage = `${this.entryOptions.name} n'a pas pu être chargé`
-        }
-      },
-      errorCaptured(err, vm, info) {
-        console.log(err, vm, info);
-      }
+export default {
+  props: {
+    title: {
+      default: "Le composant n'a pas pu être chargé"
+    },
+    displayType: {
+      type: String
+    },
+    entryOptions: {
+      type: Object,
+      default: () => null
     }
+  },
+  data() {
+    return {
+      titleMessage: this.title,
+      errorMessage: this.title
+    };
+  },
+  mounted() {
+    if (this.entryOptions && this.entryOptions.name) {
+      this.errorMessage = `${this.entryOptions.name} n'a pas pu être chargé`;
+    }
+  },
+  errorCaptured(err, vm, info) {
+    console.log(err, vm, info);
+  }
+};
 </script>
--- src/vendor/Anakeen/Hub/IHM/Components/Hub/HubComponentStatus/HubComponentLoading.vue
+++ src/vendor/Anakeen/Hub/IHM/Components/Hub/HubComponentStatus/HubComponentLoading.vue
@@ -1,26 +1,25 @@
 <template>
   <div class="ank-hub-component-loading">
-      <div ref="loadingWidget" class="ank-hub-component-loading-widget"></div>
+    <div ref="loadingWidget" class="ank-hub-component-loading-widget"></div>
   </div>
 </template>
 <!-- CSS to this component only -->
 <style scoped lang="scss">
-    .ank-hub-component-loading {
-        position: relative;
-        width: 100%;
-        height: 100%;
-    }
+.ank-hub-component-loading {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
 </style>
 <!-- Global CSS -->
 <style lang="scss"></style>
 <script>
-  export default {
-    mounted() {
-      kendo.ui.progress($(this.$refs.loadingWidget), true);
-    },
-    beforeDestroy() {
-      kendo.ui.progress($(this.$refs.loadingWidget), false);
-    }
-  };
-
+export default {
+  mounted() {
+    kendo.ui.progress($(this.$refs.loadingWidget), true);
+  },
+  beforeDestroy() {
+    kendo.ui.progress($(this.$refs.loadingWidget), false);
+  }
+};
 </script>
