192c70fc583e9af2a9ccc2066cfa6bf6ccb47125
Éric Brison <eric.brison@anakeen.com>
2019-05-22 14:23:34 +0200

    fix(internal): correct reconfigure for mkdir sub dir in var

--- src/programs/reconfigure_config.php
+++ src/programs/reconfigure_config.php
@@ -9,17 +9,18 @@ $paramName = "Core::CORE_TMPDIR";
 \Anakeen\LogManager::warning(sprintf("Restoring default value for parameter '%s'.", $paramName));
 \Anakeen\Core\DbManager::query(sprintf('UPDATE paramv SET val = %s WHERE name = %s', pg_escape_literal('./var/tmp'), pg_escape_literal($paramName)));
 
-printf('UPDATE paramv SET val = %s WHERE name = %s', pg_escape_literal('./var/tmp'), pg_escape_literal($paramName));
-
 
 $tmpDir=__DIR__.'/../var/tmp';
 if (! is_dir($tmpDir)) {
     mkdir($tmpDir);
 }
 
+
 $cacheDir=__DIR__.'/../'.\Anakeen\Core\Settings::CacheDir;
 if (! is_dir($cacheDir)) {
     mkdir($cacheDir);
+    mkdir($cacheDir."/image");
+    mkdir($cacheDir."/file");
 }
 
 $sessionDir=__DIR__.'/../'.\Anakeen\Core\Internal\Session::SESSION_SUBDIR;
