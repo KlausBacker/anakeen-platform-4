762ff2c4e414487bcb44b93c1452a7282996da3e
David Dang <david.dang@anakeen.com>
2019-06-13 15:49:31 +0200

    feat(config): i18n hub element entry refs #29

---
+++ src/vendor/Anakeen/AdminCenter/IHM/HubComponent/AdminCenterI18n/AdminCenterI18nEntry.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <nav v-if="isDockCollapsed">
+      <i class="fa fa-flag" aria-hidden="true"></i>
+    </nav>
+    <nav v-else-if="isDockExpanded">
+      <i class="fa fa-flag" aria-hidden="true"></i> <span>I18n</span>
+    </nav>
+    <div v-else-if="isHubContent" class="i18n-station">
+      <admin-center-i18n
+        @changeLocaleWrongArgument="handleLocaleWrongArgumentError"
+      ></admin-center-i18n>
+    </div>
+  </div>
+</template>
+<script>
+import Vue from "vue";
+import HubElement from "@anakeen/hub-components/components/lib/HubElement";
+
+export default {
+  name: "ank-admin-i18n",
+  extends: HubElement, // ou mixins: [ HubElementMixins ],
+  components: {
+    "admin-center-i18n": () =>
+      new Promise(resolve => {
+        import("../../I18n/AdminCenterI18n.vue").then(Component => {
+          resolve(Component.default);
+        });
+      })
+  },
+  methods: {
+    handleLocaleWrongArgumentError(message) {
+      this.hubNotify({
+        type: "error",
+        content: {
+          textContent: message, // ou htmlContent: "<em>Un message d'information important</em>"
+          title: "Wrong locale argument"
+        }
+      });
+    }
+  }
+};
+</script>
+<style>
+.i18n-station {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  min-height: 0;
+}
+</style>
