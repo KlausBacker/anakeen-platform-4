8a1f84e22fdaddc54d07858a1241c6fe2afbea1a
David Dang <david.dang@anakeen.com>
2019-06-19 17:11:23 +0200

    fix(component): prettier refs #29

--- src/vendor/Anakeen/AdminCenter/IHM/I18n/AdminCenterI18n.controller.ts
+++ src/vendor/Anakeen/AdminCenter/IHM/I18n/AdminCenterI18n.controller.ts
@@ -121,10 +121,10 @@ export default class I18nManagerController extends Vue {
             if (rowData.plural) {
               let cellData = "";
               // tslint:disable-next-line:prefer-for-of
-              for (let i = 0; i < rowData.plural.length-1 ; i++) {
-                cellData += rowData.plural[i]+"<hr>";
+              for (let i = 0; i < rowData.plural.length - 1; i++) {
+                cellData += rowData.plural[i] + "<hr>";
               }
-              cellData += rowData.plural[rowData.plural.length-1];
+              cellData += rowData.plural[rowData.plural.length - 1];
               return cellData;
             } else {
               return rowData.msgstr;
