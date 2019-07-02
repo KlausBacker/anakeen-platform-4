48b1c88091dac29840acbed95fb4e662e08a8c44
Charles Bonnissent <charles.bonnissent@anakeen.com>
2019-07-02 09:45:10 +0200

    build: correct dectetion of version and artifacts collection

--- .devtool/ci/utils/getVersionFromApp.js
+++ .devtool/ci/utils/getVersionFromApp.js
@@ -5,6 +5,9 @@ const appPath = process.argv[2];
 const appName = process.argv[3];
 
 findApp(appName, appPath).then(fileName => {
+  if (!fileName) {
+    throw new Error(`No app found : ${appPath} ${appName}`);
+  }
   getInfoFromApp(path.join(appPath, fileName)).then(appInfo => {
     console.log(appInfo.version);
   });
