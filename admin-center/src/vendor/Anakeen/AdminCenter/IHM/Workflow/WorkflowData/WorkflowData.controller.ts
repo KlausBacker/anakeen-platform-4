f9160c69d3fd4f6a6851e08e2dc29394adf793be
David Dang <david.dang@anakeen.com>
2019-07-18 15:29:11 +0200

    feat(admin): fix workflow grid and links refs #45

--- admin-center/src/vendor/Anakeen/AdminCenter/IHM/Workflow/WorkflowData/WorkflowData.controller.ts
+++ admin-center/src/vendor/Anakeen/AdminCenter/IHM/Workflow/WorkflowData/WorkflowData.controller.ts
@@ -1,9 +1,11 @@
 import { DataSourceInstaller } from "@progress/kendo-datasource-vue-wrapper";
 import { Grid, GridInstaller } from "@progress/kendo-grid-vue-wrapper";
+import "@progress/kendo-ui/js/kendo.color.js";
+import "@progress/kendo-ui/js/kendo.colorpicker.js";
 import "@progress/kendo-ui/js/kendo.filtercell.js";
 import "@progress/kendo-ui/js/kendo.grid.js";
 import Vue from "vue";
-import { Component, Prop } from "vue-property-decorator";
+import { Component, Prop, Watch } from "vue-property-decorator";
 
 declare var kendo;
 
@@ -12,9 +14,21 @@ Vue.use(DataSourceInstaller);
 @Component
 export default class WorkflowDataController extends Vue {
   public tabData = [];
-  public language: string = (navigator.language === 'fr-FR') ? 'fr' : 'en';
+  public language: string = "";
   @Prop({ type: String, default: "" }) public wflName;
-
+  @Watch("wflName")
+  public watchWflName() {
+    const grid = $(".wfl-grid-content");
+    grid.data("kendoGrid").dataSource.read();
+  }
+  public mounted() {
+    this.$http
+      .get(`/api/v2/ui/users/current`)
+      .then(
+        response =>
+          (this.language = response.data.locale === "fr_FR.UTF-8" ? "fr" : "en")
+      );
+  }
   public getWfl(options) {
     this.$http
       .get(`/api/v2/admin/workflow/data/${this.wflName}`, {
@@ -29,14 +43,17 @@ export default class WorkflowDataController extends Vue {
       });
     return [];
   }
-
+  public parseWflTotal(response) {
+    return response.data.data.requestParameters.total;
+  }
   public parseWflData(response) {
-    if (response && response.data && response.data.data) {
-      response.data.data.steps.forEach(item => {
+    this.tabData = [];
+    if (response && response.data && response.data.data &&response.data.data.data) {
+      response.data.data.data.steps.forEach(item => {
         item.type = "steps";
         this.tabData.push(item);
       });
-      response.data.data.transitions.forEach(item => {
+      response.data.data.data.transitions.forEach(item => {
         item.type = "transitions";
         this.tabData.push(item);
       });
@@ -49,17 +66,71 @@ export default class WorkflowDataController extends Vue {
       switch (colId) {
         case "type":
           return `<ul><li><b>Id:&nbsp</b>${dataItem.id}</li><li><b>Type:&nbsp</b>${dataItem.type}</li></ul>`;
-          break;
         case "info":
           if (dataItem.type === "steps") {
-            return `<ul><li><b>Label:&nbsp</b><a href="/admin/i18n/en?msgstr=${dataItem.label}">${dataItem.label}</a></li><li><b>Timer:&nbsp</b>${dataItem.timer}</li><li><b>Mail template:&nbsp</b>${dataItem.mailtemplates}</li><li><b>Color:&nbsp</b></li>`;
+            return `<ul><li><b>Label:&nbsp</b><a href="/admin/i18n/${
+              this.language
+            }?msgstr=${dataItem.label}&msgid=${dataItem.id}">${
+              dataItem.label
+            }</a></li><li><b>Timer:&nbsp</b>${this.displayMultiple(
+              dataItem.timer,
+              "timer"
+            )}</li><li><b>Mail template:&nbsp</b>${this.displayMultiple(
+              dataItem.mailtemplates,
+              "mail"
+            )}</li><li><b>Color:&nbsp</b><input data-role="colorpicker" type="color" class="wfl-step__color" value="${
+              dataItem.color
+            }"/></li>`;
           } else {
-            return `<ul><li><b>Label:&nbsp</b>${dataItem.label}</li><li><b>Timer:&nbsp</b>${dataItem.volatileTimers}</li><li><b>Mail template:&nbsp</b>${dataItem.mailtemplates}</li>`;
+            return `<ul><li><b>Label:&nbsp</b>${
+              dataItem.label
+            }</li><li><b>Timer:&nbsp</b>${this.displayMultiple(
+              dataItem.persistentTimers,
+              "timer"
+            )}</li><li><b>Mail template:&nbsp</b>${this.displayMultiple(
+              dataItem.mailtemplates,
+              "mail"
+            )}</li>`;
           }
-          break;
         default:
           break;
       }
     };
+  }
+  public displayMultiple(data, type) {
+    if (data instanceof Object) {
+      const str = "";
+      return this.recursiveData(data, str, type);
+    } else if (data !== null && data !== undefined) {
+      return data;
+    } else {
+      return "";
+    }
+  }
+  public recursiveData(items, str, type) {
+    if (items instanceof Object) {
+      Object.keys(items.toJSON()).forEach(item => {
+        if (items[item] instanceof Object) {
+          this.recursiveData(items[item], str, type);
+        } else {
+          if (items[item]) {
+            switch (type) {
+              case "mail":
+                str += `<li><a href="/admin/mail/?name=${items[item]}">${items[item]}</a></li>`;
+                break;
+              case "timer":
+                str += `<li><a href="/admin/timer/?name=${items[item]}">${items[item]}</a></li>`;
+                break;
+              default:
+                break;
+            }
+          }
+        }
+      });
+    }
+    return `<ul>${str}</ul>`;
+  }
+  public autoFilterCol(e) {
+    e.element.addClass("k-textbox filter-input");
   }
 }
--- admin-center/src/vendor/Anakeen/AdminCenter/IHM/Workflow/WorkflowData/WorkflowData.vue
+++ admin-center/src/vendor/Anakeen/AdminCenter/IHM/Workflow/WorkflowData/WorkflowData.vue
@@ -4,6 +4,8 @@
       ref="wflGrid"
       :transport-read="getWfl"
       :schema-data="parseWflData"
+      :schema-total="parseWflTotal"
+      :server-filtering="true"
     >
     </kendo-datasource>
     <kendo-grid
@@ -12,14 +14,25 @@
       :data-source-ref="'wflGrid'"
       :sortable="false"
       :resizable="true"
+      :dataBound="gridDataBound"
+      :filterable-mode="'row'"
+      :filterable-extra="false"
     >
       <kendo-grid-column
+        :field="'type'"
         :title="'<b>Type</b>'"
         :template="displayData('type')"
+        :filterable-cell-operator="'contains'"
+        :filterable-cell-show-operators="false"
+        :filterable-cell-template="autoFilterCol"
       ></kendo-grid-column>
       <kendo-grid-column
+        :field="'info'"
         :title="'<b>Informations</b>'"
         :template="displayData('info')"
+        :filterable-cell-operator="'contains'"
+        :filterable-cell-show-operators="false"
+        :filterable-cell-template="autoFilterCol"
       ></kendo-grid-column>
     </kendo-grid>
   </div>
