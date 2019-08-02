4c6765b38eaa2d43d0cd7fecad53f8fec56131df
Leopold Desert-Legendre <leopold.desertlegendre@anakeen.com>
2019-08-02 15:57:56 +0200

    fix(ui): migration Mask (refs #93)

--- user-interfaces/src/vendor/Anakeen/SmartStructures/Mask/Render/MaskView.js
+++ user-interfaces/src/vendor/Anakeen/SmartStructures/Mask/Render/MaskView.js
@@ -3,7 +3,7 @@ import $ from "jquery";
 import "@progress/kendo-ui/js/kendo.treelist";
 import "@progress/kendo-ui/js/kendo.columnmenu";
 
-{
+window.ank.smartElement.globalController.registerFunction("mask", controller => {
   const fitMaskGridToBottom = $tree => {
     if ($tree.length === 1) {
       let kTree = $tree.data("kendoTreeList");
@@ -33,150 +33,142 @@ import "@progress/kendo-ui/js/kendo.columnmenu";
     }, 1);
   };
 
-  window.dcp.document.documentController(
-    "addEventListener",
-    "ready",
-    {
-      name: "maskReady",
-      documentCheck: documentObject => documentObject.renderMode === "view" && documentObject.family.name === "MASK"
-    },
-    function maskViewReady(event, documentObject) {
+  controller.addEventListener("ready", function maskViewReady(event, smartElementObject) {
+    let $tree = $("#maskGrid");
+
+    if ($tree.length === 0) {
+      return;
+    }
+
+    $(window).on("resize.mask", () => {
       let $tree = $("#maskGrid");
 
-      if ($tree.length === 0) {
-        return;
+      if ($tree.length === 1) {
+        fitMaskGridToBottom($tree);
+      } else {
+        $(window).off("resize.mask");
       }
+    });
 
-      $(window).on("resize.mask", () => {
-        let $tree = $("#maskGrid");
-
-        if ($tree.length === 1) {
-          fitMaskGridToBottom($tree);
-        } else {
-          $(window).off("resize.mask");
+    $tree.kendoTreeList({
+      dataSource: {
+        transport: {
+          read: {
+            url: "/api/v2/admin/mask/" + smartElementObject.id + "/visibilities/"
+          }
+        },
+        schema: {
+          model: {
+            id: "attrid",
+            parentId: "parentId",
+            expanded: true
+          },
+          data: response => {
+            response.data.forEach(item => {
+              item.mNeededLabel = item.mNeeded ? "Mandatory" : "Optional";
+              item.neededLabel = item.needed ? "Mandatory" : "Optional";
+            });
+            return response.data;
+          }
         }
-      });
+      },
+      columnMenu: { columns: true },
+      filterable: {
+        extra: false,
+        operators: {
+          string: {
+            contains: "Contains...",
+            startswith: "Starts with..."
+          }
+        }
+      },
+      dataBound: e => {
+        let grid = e.sender;
+        addRowClassName(grid);
+      },
 
-      $tree.kendoTreeList({
-        dataSource: {
-          transport: {
-            read: {
-              url: "/api/v2/admin/mask/" + documentObject.id + "/visibilities/"
-            }
+      expand: e => {
+        let grid = e.sender;
+        addRowClassName(grid);
+      },
+
+      collapse: e => {
+        let grid = e.sender;
+        addRowClassName(grid);
+      },
+
+      sortable: false,
+      columns: [
+        {
+          field: "label",
+          title: "Label"
+        },
+        {
+          field: "mVisibilityLabel",
+          width: "12rem",
+          title: "Altered visibility",
+          attributes: {
+            class: "cell--m-visibility"
           },
-          schema: {
-            model: {
-              id: "attrid",
-              parentId: "parentId",
-              expanded: true
-            },
-            data: response => {
-              response.data.forEach(item => {
-                item.mNeededLabel = item.mNeeded ? "Mandatory" : "Optional";
-                item.neededLabel = item.needed ? "Mandatory" : "Optional";
-              });
-              return response.data;
-            }
+          template:
+            "#if (data.setVisibility === true) {#" +
+            '<div class="mask-visibility mask-visibility-set">#:mVisibilityLabel#</div>' +
+            "# } else if (data.setVisibility === false && data.visibility !== data.mVisibility)" +
+            ' {#<div class="mask-visibility mask-visibility-modified">#:mVisibilityLabel#</div>#}' +
+            'else if (data.mVisibility) {# <div class="mask-visibility">#:mVisibilityLabel#</div>  #}#'
+        },
+        {
+          field: "mNeededLabel",
+          title: "Altered need",
+          width: "10rem",
+          filterable: true,
+          attributes: {
+            class: "cell--m-needed"
+          },
+          template:
+            "#if (data.needed == undefined) {# #} " +
+            "else if (data.needed !== data.mNeeded) {#" +
+            '<div class="mask-needed mask-needed-set">#:mNeededLabel#</div>' +
+            '# } else if (data.needed !== undefined) {# <div class="mask-needed">#:mNeededLabel#</div>' +
+            "#}#"
+        },
+        {
+          field: "visibilityLabel",
+          type: "text",
+          width: "12rem",
+          title: "Original visibility",
+          attributes: {
+            class: "cell--visibility"
           }
         },
-        columnMenu: { columns: true },
-        filterable: {
-          extra: false,
-          operators: {
-            string: {
-              contains: "Contains...",
-              startswith: "Starts with..."
-            }
+        {
+          field: "neededLabel",
+          title: "Original need",
+          attributes: {
+            class: "cell--needed"
+          },
+          width: "10rem",
+          filterable: true,
+          template: '<div class="mask-needed">#:neededLabel#</div>'
+        },
+        {
+          field: "type",
+          title: "Type",
+          width: "8rem",
+          attributes: {
+            class: "cell--type"
           }
         },
-        dataBound: e => {
-          let grid = e.sender;
-          addRowClassName(grid);
-        },
-
-        expand: e => {
-          let grid = e.sender;
-          addRowClassName(grid);
-        },
-
-        collapse: e => {
-          let grid = e.sender;
-          addRowClassName(grid);
-        },
-
-        sortable: false,
-        columns: [
-          {
-            field: "label",
-            title: "Label"
-          },
-          {
-            field: "mVisibilityLabel",
-            width: "12rem",
-            title: "Altered visibility",
-            attributes: {
-              class: "cell--m-visibility"
-            },
-            template:
-              "#if (data.setVisibility === true) {#" +
-              '<div class="mask-visibility mask-visibility-set">#:mVisibilityLabel#</div>' +
-              "# } else if (data.setVisibility === false && data.visibility !== data.mVisibility)" +
-              ' {#<div class="mask-visibility mask-visibility-modified">#:mVisibilityLabel#</div>#}' +
-              'else if (data.mVisibility) {# <div class="mask-visibility">#:mVisibilityLabel#</div>  #}#'
-          },
-          {
-            field: "mNeededLabel",
-            title: "Altered need",
-            width: "10rem",
-            filterable: true,
-            attributes: {
-              class: "cell--m-needed"
-            },
-            template:
-              "#if (data.needed == undefined) {# #} " +
-              "else if (data.needed !== data.mNeeded) {#" +
-              '<div class="mask-needed mask-needed-set">#:mNeededLabel#</div>' +
-              '# } else if (data.needed !== undefined) {# <div class="mask-needed">#:mNeededLabel#</div>' +
-              "#}#"
-          },
-          {
-            field: "visibilityLabel",
-            type: "text",
-            width: "12rem",
-            title: "Original visibility",
-            attributes: {
-              class: "cell--visibility"
-            }
-          },
-          {
-            field: "neededLabel",
-            title: "Original need",
-            attributes: {
-              class: "cell--needed"
-            },
-            width: "10rem",
-            filterable: true,
-            template: '<div class="mask-needed">#:neededLabel#</div>'
-          },
-          {
-            field: "type",
-            title: "Type",
-            width: "8rem",
-            attributes: {
-              class: "cell--type"
-            }
-          },
-          {
-            field: "attrid",
-            title: "Attribute identifier",
-            attributes: {
-              class: "cell--attrid"
-            }
+        {
+          field: "attrid",
+          title: "Attribute identifier",
+          attributes: {
+            class: "cell--attrid"
           }
-        ]
-      });
+        }
+      ]
+    });
 
-      fitMaskGridToBottom($tree);
-    }
-  );
-}
+    fitMaskGridToBottom($tree);
+  });
+});
