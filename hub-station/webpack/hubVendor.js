0e945e82bf93c2007e60e9067bf53dbf9db2ad6e
Charles Bonnissent <charles.bonnissent@anakeen.com>
2019-09-20 12:02:30 +0200

    build: bump dev

--- hub-station/webpack/hubVendor.js
+++ hub-station/webpack/hubVendor.js
@@ -4,20 +4,21 @@ const { deps } = require("@anakeen/webpack-conf");
 
 const BASE_DIR = path.resolve(__dirname, "../");
 const PUBLIC_PATH = path.join(BASE_DIR, "src/public");
-const USER_INTERFACES = path.resolve(
-  BASE_DIR,
-  "..",
-  "node_modules",
-  "@anakeen",
-  "user-interfaces"
-);
+const USER_INTERFACES = path.resolve(BASE_DIR, "..", "node_modules", "@anakeen", "user-interfaces");
 const { vueLoader, addDll, addFalseKendoGlobal } = require("@anakeen/webpack-conf/parts");
 
 module.exports = () => {
   const conf = {
     moduleName: "hubVendor",
     entry: {
-      hubVendor: ["@anakeen/user-interfaces/components/lib/AnkSEGrid","@anakeen/user-interfaces/components/lib/AnkLogout","@anakeen/user-interfaces/components/lib/AnkIdentity","@anakeen/user-interfaces/components/lib/AnkSmartElement", "tslib", "vue-property-decorator"]
+      hubVendor: [
+        "@anakeen/user-interfaces/components/lib/AnkSEGrid",
+        "@anakeen/user-interfaces/components/lib/AnkLogout",
+        "@anakeen/user-interfaces/components/lib/AnkIdentity",
+        "@anakeen/user-interfaces/components/lib/AnkSmartElement",
+        "tslib",
+        "vue-property-decorator"
+      ]
     },
     excludeBabel: [
       /node_modules\/axios/,
@@ -32,27 +33,11 @@ module.exports = () => {
       addFalseKendoGlobal([/kendo.pdf/, /kendo.excel/]),
       addDll({
         context: BASE_DIR,
-        manifest: path.join(
-          USER_INTERFACES,
-          "src",
-          "public",
-          "Anakeen",
-          "assets",
-          "legacy",
-          "KendoUI-manifest.json"
-        )
+        manifest: path.join(USER_INTERFACES, "src", "public", "Anakeen", "assets", "legacy", "KendoUI-manifest.json")
       }),
       addDll({
         context: BASE_DIR,
-        manifest: path.join(
-          USER_INTERFACES,
-          "src",
-          "public",
-          "Anakeen",
-          "ankDll",
-          "legacy",
-          "vueDll-manifest.json"
-        )
+        manifest: path.join(USER_INTERFACES, "src", "public", "Anakeen", "ankDll", "legacy", "vueDll-manifest.json")
       }),
       addDll({
         context: BASE_DIR,
@@ -69,10 +54,10 @@ module.exports = () => {
     ]
   };
   if (process.env.conf === "DEV") {
-    return deps({...conf, ...{mode: "dev"}});
+    return deps({ ...conf, ...{ mode: "dev" } });
   }
   if (process.env.conf === "LEGACY") {
     return deps(conf);
   }
-  return [deps(conf), deps({...conf, ...{mode: "dev"}})];
+  return [deps(conf), deps({ ...conf, ...{ mode: "dev" } })];
 };
