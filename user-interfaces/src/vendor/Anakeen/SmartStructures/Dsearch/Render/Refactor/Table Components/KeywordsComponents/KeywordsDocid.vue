b1d396990ab6640822f2e07ed276e64a5b6a71da
Leopold Desert-Legendre <leopold.desertlegendre@anakeen.com>
2019-09-24 16:14:03 +0200

    fix(ui): added field reactivity for docid, enum, wid (refs #84)

--- user-interfaces/src/vendor/Anakeen/SmartStructures/Dsearch/Render/Refactor/Table Components/KeywordsComponents/KeywordsDocid.vue
+++ user-interfaces/src/vendor/Anakeen/SmartStructures/Dsearch/Render/Refactor/Table Components/KeywordsComponents/KeywordsDocid.vue
@@ -27,7 +27,9 @@ export default {
   data() {
     return {
       textBoxOperators: ["~*", "!~*", "=~*"],
-      comboBox: null
+      comboBox: null,
+      initHtmlTitle: "",
+      myInitValue: ""
     };
   },
   computed: {
@@ -41,7 +43,7 @@ export default {
       if (this.isTextBox) {
         valid = this.$refs.keywordsDocidTextBoxWrapper.value !== "";
       } else {
-        valid = !!this.comboBox.value();
+        valid = this.comboBox ? !!this.comboBox.value() : false;
       }
       return valid;
     },
@@ -60,78 +62,114 @@ export default {
       });
     },
     initData() {
-      if (this.initValue) {
+      if (this.myInitValue) {
         if (!this.isTextBox) {
           let that = this;
           this.comboBox.select(function(item) {
-            return item.id === that.initValue;
+            return item.id === that.myInitValue;
           });
+          this.onComboBoxChange();
         } else {
-          $(this.$refs.keywordsDocidTextBoxWrapper).val(this.initValue);
+          $(this.$refs.keywordsDocidTextBoxWrapper).val(this.myInitValue);
         }
       }
+      this.myInitValue = "";
+      this.initHtmlTitle = "";
     },
     clearData() {
       this.comboBox.value("");
       $(this.$refs.keywordsDocidTextBoxWrapper).val("");
+    },
+    fetchData() {
+      let that = this;
+      let dataSource = {
+        serverFiltering: true,
+        transport: {
+          read: function(options) {
+            let filter = "";
+            if (options.data.filter !== undefined) {
+              if (options.data.filter.filters[0] !== undefined) {
+                filter = options.data.filter.filters[0].value;
+              }
+            }
+            $.ajax({
+              type: "GET",
+              url:
+                "/api/v2/smartstructures/dsearch/relations/" +
+                that.famid +
+                "/" +
+                that.field.id +
+                "?slice=25&offset=0&keyword=" +
+                filter,
+              dataType: "json",
+              success: function succesRequestRelationsIRelation(result) {
+                let info = [];
+
+                let isInData = false;
+                result.data.forEach(function eachResultRelationsIRelation(item) {
+                  info.push({
+                    id: item.id,
+                    htmlTitle: item.htmlTitle
+                  });
+                  if (item.id === that.myInitValue) {
+                    isInData = true;
+                  }
+                });
+                if (that.myInitValue && !isInData) {
+                  info.unshift({
+                    id: that.myInitValue,
+                    htmlTitle: that.initHtmlTitle
+                  });
+                }
+                options.success(info);
+                that.initData();
+              },
+              error: function errorRequestRelationsIRelation(result) {
+                options.error(result);
+              }
+            });
+          }
+        }
+      };
+      this.comboBox.setDataSource(dataSource);
     }
   },
   mounted() {
-    let that = this;
-    this.$nextTick(() => {
-      this.comboBox = $(this.$refs.keywordsDocidComboBoxWrapper)
-        .kendoComboBox({
-          width: 200,
-          filter: "contains",
-          dataValueField: "id",
-          dataTextField: "htmlTitle",
-          clearButton: false,
-          change: this.onComboBoxChange,
-          dataSource: {
-            serverFiltering: true,
-            transport: {
-              /**
-               * function to get data
-               * @param options param to return success or error data
-               */
-              read: function readDatasIRelation(options) {
-                let filter = "";
-                if (options.data.filter !== undefined) {
-                  if (options.data.filter.filters[0] !== undefined) {
-                    filter = options.data.filter.filters[0].value;
-                  }
-                }
-                $.ajax({
-                  type: "GET",
-                  url:
-                    "/api/v2/smartstructures/dsearch/relations/" +
-                    that.famid +
-                    "/" +
-                    that.field.id +
-                    "?slice=25&offset=0&keyword=" +
-                    filter,
-                  dataType: "json",
-                  success: function succesRequestRelationsIRelation(result) {
-                    let info = [];
-                    result.data.forEach(function eachResultRelationsIRelation(item) {
-                      info.push({
-                        id: item.id,
-                        htmlTitle: item.htmlTitle
-                      });
-                    });
-                    options.success(info);
-                    that.initData();
-                  },
-                  error: function errorRequestRelationsIRelation(result) {
-                    options.error(result);
-                  }
-                });
-              }
-            }
+    this.myInitValue = this.initValue
+    this.comboBox = $(this.$refs.keywordsDocidComboBoxWrapper)
+      .kendoComboBox({
+        width: 200,
+        filter: "contains",
+        dataValueField: "id",
+        dataTextField: "htmlTitle",
+        clearButton: false,
+        change: this.onComboBoxChange
+      })
+      .data("kendoComboBox");
+
+    if (this.myInitValue) {
+      let that = this;
+      $.ajax({
+        type: "GET",
+        url: "/api/v2/smart-elements/" + this.myInitValue + ".json?fields=document.properties.title",
+        dataType: "json",
+        success: function(result) {
+          that.initHtmlTitle = result.data.document.properties.title;
+          if (that.initHtmlTitle) {
+            that.fetchData();
+          } else {
+            that.comboBox.one("open", that.fetchData);
           }
-        })
-        .data("kendoComboBox");
-    });
+        }
+      });
+    } else {
+      this.comboBox.one("open", this.fetchData);
+    }
+  },
+  watch: {
+    field: function() {
+      this.$nextTick(this.fetchData);
+    }
   }
 };
 </script>
--- user-interfaces/src/vendor/Anakeen/SmartStructures/Dsearch/Render/Refactor/Table Components/KeywordsComponents/KeywordsWid.vue
+++ user-interfaces/src/vendor/Anakeen/SmartStructures/Dsearch/Render/Refactor/Table Components/KeywordsComponents/KeywordsWid.vue
@@ -36,10 +36,63 @@ export default {
         this.comboBox.select(function(item) {
           return item.id === that.initValue;
         });
+        this.initValue = "";
       }
     },
     clearData() {
       this.comboBox.value("");
+    },
+    fetchData() {
+      let that = this;
+      this.workflows = [];
+      this.dataWorkflow = [];
+
+      $.getJSON("/api/v2/smart-elements/" + this.famid + "/workflows/states/?allStates=1", function requestWorkflows(
+        data
+      ) {
+        $.each(data.data.states, function eachStatesSReady(key, value) {
+          that.workflows.push(value);
+        });
+      }).done(function() {
+        const typeRevision = that.controller.getValue("se_latest").value;
+        that.workflows.forEach(item => {
+          let myObject;
+          if (typeRevision === "fixed" || typeRevision === "allfixed" || typeRevision === "lastfixed") {
+            myObject = {
+              id: item.id,
+              label: item.label
+            };
+          } else if (item.activity !== "") {
+            if (typeRevision === "yes") {
+              myObject = {
+                id: item.id,
+                label: item.activity
+              };
+            } else {
+              myObject = {
+                id: item.id,
+                label: item.label + "/" + item.activity
+              };
+            }
+          } else {
+            if (typeRevision === "yes") {
+              myObject = {
+                id: item.id,
+                label: item.label
+              };
+            } else {
+              myObject = {
+                id: item.id,
+                label: item.label
+              };
+            }
+          }
+          myObject.color = item.color;
+          that.dataWorkflow.push(myObject);
+        });
+        that.comboBox.setDataSource(that.dataWorkflow);
+        that.initData();
+      });
     }
   },
   mounted() {
@@ -55,53 +108,12 @@ export default {
         change: this.onComboBoxChange
       })
       .data("kendoComboBox");
-    let that = this;
-    $.getJSON("/api/v2/smart-elements/" + this.famid + "/workflows/states/?allStates=1", function requestWorkflows(
-      data
-    ) {
-      $.each(data.data.states, function eachStatesSReady(key, value) {
-        that.workflows.push(value);
-      });
-    }).done(function() {
-      const typeRevision = that.controller.getValue("se_latest").value;
-      that.workflows.forEach(item => {
-        let myObject;
-        if (typeRevision === "fixed" || typeRevision === "allfixed" || typeRevision === "lastfixed") {
-          myObject = {
-            id: item.id,
-            label: item.label
-          };
-        } else if (item.activity !== "") {
-          if (typeRevision === "yes") {
-            myObject = {
-              id: item.id,
-              label: item.activity
-            };
-          } else {
-            myObject = {
-              id: item.id,
-              label: item.label + "/" + item.activity
-            };
-          }
-        } else {
-          if (typeRevision === "yes") {
-            myObject = {
-              id: item.id,
-              label: item.label
-            };
-          } else {
-            myObject = {
-              id: item.id,
-              label: item.label
-            };
-          }
-        }
-        myObject.color = item.color;
-        that.dataWorkflow.push(myObject);
-      });
-      that.comboBox.setDataSource(that.dataWorkflow);
-      that.initData();
-    });
+    this.comboBox.one("open", this.fetchData);
+  },
+  watch: {
+    famid: function() {
+      this.$nextTick(this.fetchData);
+    }
   }
 };
 </script>
