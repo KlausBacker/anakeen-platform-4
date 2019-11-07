203e1c4c0be27a92b823e64017ec80099a26756f
Éric Brison <eric.brison@anakeen.com>
2019-11-07 08:55:12 +0100

    fix(tools): add warning on stug gen if no config set (refs #181)

--- anakeen-cli/tasks/stub.js
+++ anakeen-cli/tasks/stub.js
@@ -3,6 +3,7 @@ const { parseStub, parseWorkflowContants, parseEnumContants } = require("../util
 const { getModuleInfo } = require("../utils/moduleInfo");
 const { Signale } = require("signale");
 const { analyzeXML } = require("../utils/globAnalyze");
+const path = require("path");
 
 exports.stub = ({ sourcePath, targetPath = "./stubs", verbose }) => {
   gulp.task("stubFields", async () => {
@@ -11,15 +12,22 @@ exports.stub = ({ sourcePath, targetPath = "./stubs", verbose }) => {
     }
     try {
       const interactive = new Signale({ scope: "stub struct" });
-      const log = message => {
-        interactive.info(message);
+      const log = (message, type = "info") => {
+        if (type === "warning") {
+          interactive.warn(message);
+        } else {
+          interactive.info(message);
+        }
       };
+
       const info = await getModuleInfo(sourcePath);
       const stub = info.buildInfo.build.config["stub-config"];
       let globXML = [];
 
       if (stub) {
         globXML = stub[0]["stub-struct"];
+      } else {
+        log(`No stub configuration in ${path.resolve(sourcePath)}/build.xml`, "warning");
       }
 
       const globFile = analyzeXML(globXML);
