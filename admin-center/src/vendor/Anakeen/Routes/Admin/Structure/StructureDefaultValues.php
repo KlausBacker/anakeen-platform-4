86b0c8b43002f0349f60d00cacb7edcc8c30d069
Éric Brison <eric.brison@anakeen.com>
2019-11-18 14:57:07 +0100

    feat(admin): new default values data (refs #43)

--- admin-center/src/vendor/Anakeen/Routes/Admin/Structure/StructureDefaultValues.php
+++ admin-center/src/vendor/Anakeen/Routes/Admin/Structure/StructureDefaultValues.php
@@ -2,6 +2,7 @@
 
 namespace Anakeen\Routes\Admin\Structure;
 
+use Anakeen\Core\Internal\FormatCollection;
 use Anakeen\Core\SEManager;
 use Anakeen\Core\Settings;
 use Anakeen\Core\SmartStructure;
@@ -39,21 +40,41 @@ class StructureDefaultValues extends StructureFields
     protected static function getConfigParameterValues(SmartStructure $structure)
     {
         $configDefValues = $structure->getOwnDefValues();
+        if ($structure->fromid) {
+            $parentStruct=SEManager::getFamily($structure->fromid);
+            $configParentValues = $parentStruct->getDefValues();
+        } else {
+            $configParentValues=[];
+        }
         $data = [];
-
         $element = SEManager::createTemporaryDocument($structure->name, true);
 
-        $fields = $structure->getAttributes();
-        foreach ($fields as $field => $oa) {
-            if ($field === SmartStructure\Attributes::HIDDENFIELD) {
+        $formater=new FormatCollection($element);
+
+
+        $fields = $structure->getNormalAttributes();
+        foreach ($fields as $oa) {
+            if ($oa->id === SmartStructure\Attributes::HIDDENFIELD) {
                 continue;
             }
             $isMultiple = $oa->isMultiple();
-            $data[$field] = [
-                "config" => $configDefValues[$field]??"",
+
+
+            $data[$oa->id]["configurationValue"]=$configDefValues[$oa->id]??null;
+            if ($structure->fromid) {
+                $data[$oa->id]["parentConfigurationValue"] = $configParentValues[$oa->id] ?? null;
+            }
+
+
+            $data[$oa->id]["result"]= json_decode(json_encode($formater->getInfo($oa, $element->getRawValue($oa->id), $element)), true);
+
+            $data[$oa->id]["configurationValue"]=$configDefValues[$oa->id]??null;
+            /*
+                "configurationValue" => ,
                 "type" => $oa->type,
-                "value" => $isMultiple ? $element->getRawValue($field) : $element->getMultipleRawValues($field)
-            ];
+                "value" => $isMultiple ? $element->getMultipleRawValues($field) : $element->getRawValue($field),
+                "displayValue" => $formater->getInfo($oa, $element->getRawValue($field), $element)
+            ];*/
         }
         return $data;
     }
