edcf9acd6ba4ab5a3471ba4034b5693030a34aa0
Éric Brison <eric.brison@anakeen.com>
2019-11-29 17:33:03 +0100

    feat(sde): parameters defaults for column (refs #256)

--- smart-data-engine/src/vendor/Root/CheckEnd.php
+++ smart-data-engine/src/vendor/Root/CheckEnd.php
@@ -229,7 +229,7 @@ class CheckEnd extends CheckData
             if (!$oa) {
                 $this->addError(ErrorCode::getError('DFLT0005', $attrid, $this->doc->name));
             } else {
-                if ($oa->usefor === "Q") {
+                if ($oa->usefor === "Q" && !$oa->inArray()) {
                     $this->addError(ErrorCode::getError('DFLT0010', $attrid, $this->doc->name));
                 } elseif (is_string($def)) {
                     $oParse = new \Anakeen\Core\SmartStructure\Callables\ParseFamilyMethod();
