11bbb6e5d64eb069302391edec53af3235825831
Charles Bonnissent <charles.bonnissent@anakeen.com>
2019-12-05 10:35:37 +0100

    feat(hub): rollup hub station (refs #168)

---
+++ hub-station/components/rollup/rollup.config.js
@@ -0,0 +1,73 @@
+const path = require("path");
+const Scss = require("rollup-plugin-scss");
+const typescript = require("rollup-plugin-typescript2");
+const VuePlugin = require("rollup-plugin-vue");
+const magicImporter = require("node-sass-magic-importer");
+const progress = require("rollup-plugin-progress");
+const { string } = require("rollup-plugin-string");
+const cleaner = require("rollup-plugin-cleaner");
+const json = require("rollup-plugin-json");
+const NodeExternals = require("@yelo/rollup-node-external");
+
+// Use commonjs plugin for vuejs vue-runtime-helpers/dist/normalize-component.js
+const commonjs = require("rollup-plugin-commonjs");
+
+// eslint-disable-next-line no-undef
+const BASE_PATH = path.resolve(__dirname, "../");
+const OUTPUT_DIR = path.resolve(BASE_PATH, "lib");
+const MONOREPO_PATH = path.resolve(BASE_PATH, "../..");
+
+// rollup.config.js
+const conf = {
+  input: {
+    AnkHub: path.resolve(BASE_PATH, "src/index.ts")
+  },
+  output: {
+    dir: OUTPUT_DIR,
+    entryFileNames: "[name].[format].js",
+    name: "[name]",
+    format: "esm",
+    sourcemap: true
+  },
+  external: NodeExternals({
+    importType: "commonjs",
+    modulesDir: path.resolve(MONOREPO_PATH, "node_modules")
+  }),
+  plugins: [
+    progress(),
+    json(),
+    Scss(),
+    string({
+      include: "**/*.template.kd"
+    }),
+    commonjs(),
+    typescript({
+      objectHashIgnoreUnknownHack: true,
+      tsconfigDefaults: {
+        sourceMap: true
+      }
+    }),
+    VuePlugin({
+      css: true, // Dynamically inject css as a <style> tag
+      compileTemplate: true, // Explicitly convert template to render function
+      style: {
+        preprocessOptions: {
+          scss: {
+            importer: magicImporter()
+          }
+        }
+      }
+    })
+  ]
+};
+
+// eslint-disable-next-line no-undef
+if (process.env.CLEAN) {
+  conf.plugins.push(
+    cleaner({
+      targets: [OUTPUT_DIR]
+    })
+  );
+}
+
+export default conf;
--- hub-station/components/src/shims-ank-components.d.ts
+++
@@ -1,1 +0,0 @@
-declare module "@anakeen/user-interfaces";
