f4922126593e06bc3fb8ae1139217c09a7c609e6
Charles Bonnissent <charles.bonnissent@anakeen.com>
2019-12-11 12:32:00 +0100

    fix(cli): add a handler for unknown compose command (refs #188)
    Avant
    
    on fait npx @anakeen/anakeen-cli compose trololo et rien pas d'erreur
    
    Après
    
    Message d'erreur + redirect vers le -h

--- anakeen-cli/commands/compose.js
+++ anakeen-cli/commands/compose.js
@@ -1,7 +1,23 @@
+const signale = require("signale");
+
 exports.command = "compose <command> [<options>]";
 
 exports.description = "Compose a repository";
 
 exports.builder = yargs => {
-  return yargs.commandDir("compose");
+  return yargs
+    .commandDir("compose")
+    .command({
+      command: "*",
+      handler: argv => {
+        if (argv._[0]) {
+          signale.error("Unknown commmand compose", argv._[1]);
+        } else {
+          signale.error("You need to specify a command");
+        }
+        yargs.showHelp();
+      }
+    })
+    .alias("h", "help")
+    .showHelpOnFail(true);
 };
