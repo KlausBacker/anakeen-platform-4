8f60f9307996bb53cd225c4cf714d0302eb790b1
Charles Bonnissent <charles.bonnissent@anakeen.com>
2020-02-13 18:55:01 +0100

    fix(ui): correct view de search element (refs #454)
    Avant
    
    L'iframe d'affichage n'avait pas été migrée au component vue
    
    Après
    
    C'est migré

--- user-interfaces/src/vendor/Anakeen/SmartStructures/Dsearch/Render/searchViewGrid.js
+++ user-interfaces/src/vendor/Anakeen/SmartStructures/Dsearch/Render/searchViewGrid.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import SearchViewGrid from "./searchViewGrid.vue";
 
+import setup from "@anakeen/user-interfaces/components/lib/setup.esm";
+Vue.use(setup);
+
 new Vue({
   el: ".search-ui-view",
   components: {
