b46e779d2f074a1b3b99aec77a9ffbba642bbac9
David Dang <david.dang@anakeen.com>
2020-02-19 17:08:42 +0100

    refactor(admin): remove commentend grid
    refs
    https://gitlab.anakeen.com/Anakeen/Platform-4/anakeen-platform-4/issues/251

--- admin-center/src/vendor/Anakeen/AdminCenter/IHM/Mail/AdminCenterMail.vue
+++ admin-center/src/vendor/Anakeen/AdminCenter/IHM/Mail/AdminCenterMail.vue
@@ -7,23 +7,6 @@
       class="mail-splitter"
       local-storage-key="admin-center-mail-splitter"
     >
-      <!--      <ank-se-grid-->
-      <!--        class="mail-grid"-->
-      <!--        :pageSizes="[50, 100, 200]"-->
-      <!--        ref="grid"-->
-      <!--        collection="MAILTEMPLATE"-->
-      <!--        :contextTitles="false"-->
-      <!--        filterable="inline"-->
-      <!--        @action-click="selectMailTemplate"-->
-      <!--      >-->
-
-      <!--        <ank-se-grid-column title="Identification" field="id" hidden="true"></ank-se-grid-column>-->
-      <!--        <ank-se-grid-column title="Title" field="tmail_title"></ank-se-grid-column>-->
-      <!--        <ank-se-grid-column title="Subject" field="tmail_subject"></ank-se-grid-column>-->
-      <!--        <ank-se-grid-actions>-->
-      <!--          <ank-se-grid-action action="consultMailTemplate" title="Display"></ank-se-grid-action>-->
-      <!--        </ank-se-grid-actions>-->
-      <!--      </ank-se-grid>-->
       <ank-se-vue-grid
         ref="grid"
         class="mail-grid"
