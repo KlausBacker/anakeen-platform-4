21a987732ef479d453defa19eab4660634d49904
David Dang <david.dang@anakeen.com>
2020-02-19 17:00:55 +0100

    refactor(admin): native vue grid usage
    refs
    https://gitlab.anakeen.com/Anakeen/Platform-4/anakeen-platform-4/issues/251

--- admin-center/src/vendor/Anakeen/AdminCenter/IHM/Mail/AdminCenterMail.controller.ts
+++ admin-center/src/vendor/Anakeen/AdminCenter/IHM/Mail/AdminCenterMail.controller.ts
@@ -1,5 +1,6 @@
 import AnkPaneSplitter from "@anakeen/internal-components/lib/PaneSplitter";
 import SmartElement from "@anakeen/user-interfaces/components/lib/AnkSmartElement.esm";
+import AnkSEVueGrid from "@anakeen/user-interfaces/components/lib/AnkSmartElementVueGrid.esm";
 import AnkSEGrid from "@anakeen/user-interfaces/components/lib/AnkSmartElementGrid.esm";
 import { Component, Vue } from "vue-property-decorator";
 
@@ -7,15 +8,23 @@ import { Component, Vue } from "vue-property-decorator";
   components: {
     "ank-se-grid": AnkSEGrid,
     "ank-smart-element": () => SmartElement,
-    "ank-split-panes": AnkPaneSplitter
+    "ank-split-panes": AnkPaneSplitter,
+    "ank-se-vue-grid": AnkSEVueGrid
   }
 })
 export default class AdminCenterMailController extends Vue {
-  public selectedMail: string = "";
+  public actions: object[] = [{ action: "consultMailTemplate", title: "Display" }];
+  public columns: object[] = [
+    { field: "id", property: true, hidden: true, title: "Identification", withContext: false },
+    { field: "tmail_title", title: "Title", withContext: false },
+    { field: "tmail_subject", title: "Subject", withContext: false }
+  ];
+  public selectedMail = "";
+
   public selectMailTemplate(e) {
     switch (e.data.type) {
       case "consultMailTemplate":
-        this.selectedMail = e.data.row.name || e.data.row.id.toString();
+        this.selectedMail = e.data.row.properties.id.toString();
         this.$nextTick(() => {
           // @ts-ignore
           this.$refs.mailSmartElement.fetchSmartElement({
--- admin-center/src/vendor/Anakeen/AdminCenter/IHM/Mail/AdminCenterMail.vue
+++ admin-center/src/vendor/Anakeen/AdminCenter/IHM/Mail/AdminCenterMail.vue
@@ -1,34 +1,46 @@
 <template>
   <div class="mail-parent">
     <ank-split-panes
+      ref="mailSplitter"
       watch-slots
       vertical
-      ref="mailSplitter"
       class="mail-splitter"
-      localStorageKey="admin-center-mail-splitter"
+      local-storage-key="admin-center-mail-splitter"
     >
-      <ank-se-grid
-        class="mail-grid"
-        :pageSizes="[50, 100, 200]"
+      <!--      <ank-se-grid-->
+      <!--        class="mail-grid"-->
+      <!--        :pageSizes="[50, 100, 200]"-->
+      <!--        ref="grid"-->
+      <!--        collection="MAILTEMPLATE"-->
+      <!--        :contextTitles="false"-->
+      <!--        filterable="inline"-->
+      <!--        @action-click="selectMailTemplate"-->
+      <!--      >-->
+
+      <!--        <ank-se-grid-column title="Identification" field="id" hidden="true"></ank-se-grid-column>-->
+      <!--        <ank-se-grid-column title="Title" field="tmail_title"></ank-se-grid-column>-->
+      <!--        <ank-se-grid-column title="Subject" field="tmail_subject"></ank-se-grid-column>-->
+      <!--        <ank-se-grid-actions>-->
+      <!--          <ank-se-grid-action action="consultMailTemplate" title="Display"></ank-se-grid-action>-->
+      <!--        </ank-se-grid-actions>-->
+      <!--      </ank-se-grid>-->
+      <ank-se-vue-grid
         ref="grid"
+        class="mail-grid"
         collection="MAILTEMPLATE"
-        :contextTitles="false"
-        filterable="inline"
-        @action-click="selectMailTemplate"
-      >
-        <ank-se-grid-column title="Identification" field="id" hidden="true"></ank-se-grid-column>
-        <ank-se-grid-column title="Title" field="tmail_title"></ank-se-grid-column>
-        <ank-se-grid-column title="Subject" field="tmail_subject"></ank-se-grid-column>
-        <ank-se-grid-actions>
-          <ank-se-grid-action action="consultMailTemplate" title="Display"></ank-se-grid-action>
-        </ank-se-grid-actions>
-      </ank-se-grid>
+        controller="DEFAULT_GRID_CONTROLLER"
+        :actions="actions"
+        :columns="columns"
+        @rowActionClick="selectMailTemplate"
+        filterable
+        defaultExpandable
+      />
       <div class="mail-form-wrapper">
         <div v-show="!selectedMail" class="mail-empty">
           <span class="material-icons">email</span>
           <p>Select mail template</p>
         </div>
-        <ank-smart-element v-show="selectedMail" class="mail-smart-element" ref="mailSmartElement"></ank-smart-element>
+        <ank-smart-element v-show="selectedMail" class="mail-smart-element" ref="mailSmartElement" />
       </div>
     </ank-split-panes>
   </div>
