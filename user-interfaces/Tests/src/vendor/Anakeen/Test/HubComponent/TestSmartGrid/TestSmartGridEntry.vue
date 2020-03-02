163e6ed5973c0c45df4e22ef47c085ed41b6f894
Aurélien Brugalières <aurelien.brugalieres@anakeen.com>
2020-03-02 17:08:21 +0100

    fix(ui): fix test smart grid with hot properties change fixes #251

--- user-interfaces/Tests/src/vendor/Anakeen/Test/HubComponent/TestSmartGrid/TestSmartGridEntry.vue
+++ user-interfaces/Tests/src/vendor/Anakeen/Test/HubComponent/TestSmartGrid/TestSmartGridEntry.vue
@@ -6,21 +6,14 @@
     </nav>
     <template v-slot:hubContent="">
       <div class="test-smart-grid">
+        <div class="grid-section grid-left">
+          <form>
+            <textarea v-model="gridProps" cols="100" rows="30"></textarea>
+          </form>
+        </div>
         <div class="grid-section grid-right">
           <!--          <h1>New grid</h1>-->
-          <ank-smart-element-grid
-            @rowActionClick="onRowActionClick"
-            collection="TST_DDUI_ALLTYPE"
-            :columns="columns"
-            :actions="actions"
-            defaultExportButton
-            defaultExpandable
-            defaultShownColumns
-            checkable
-            :reorderable="true"
-            :resizable="true"
-          >
-          </ank-smart-element-grid>
+          <ank-smart-element-grid @rowActionClick="onRowActionClick" v-bind="gridConfig"> </ank-smart-element-grid>
         </div>
       </div>
     </template>
@@ -59,36 +52,64 @@ export default {
     "export-button": GridExportButton,
     "ank-se-grid-expand-button": AnkGridExpandButtonController
   },
-
-  created() {},
   data() {
     return {
+      gridProps: "",
       routeUrl: () => {
         return this.entryOptions.completeRoute;
       },
-      columns: [
-        { field: "id", property: true },
-        { field: "title", property: true },
-        // { field: "test_ddui_all__title" },
-        { field: "test_ddui_all__longtext" },
-        // { field: "test_ddui_all__htmltext" },
-        { field: "test_ddui_all__account_multiple" },
-        { field: "test_ddui_all__account_multiple_array" },
-        // { field: "test_ddui_all__enumlist" },
-        { field: "test_ddui_all__docid" },
-        // { field: "test_ddui_all__account" },
-        { field: "test_ddui_all__date" },
-        { field: "test_ddui_all__color" },
-        // { field: "test_ddui_all__image" },
-        { field: "test_ddui_all__timestamp" },
-        { field: "test_ddui_all__money" }
-      ],
-      actions: [
-        { action: "display", title: "Display" },
-        { action: "modify", title: "Modify", iconClass: "fa fa-edit" },
-        { action: "delete", title: "Delete", iconClass: "fa fa-trash" }
-      ]
+      gridConfig: {
+        collection: "TST_DDUI_ALLTYPE",
+        columns: [
+          { field: "initid", property: true },
+          { field: "state", property: true },
+          { field: "title", property: true },
+          // { field: "test_ddui_all__title" },
+          { field: "test_ddui_all__longtext" },
+          // { field: "test_ddui_all__htmltext" },
+          { field: "test_ddui_all__account_multiple" },
+          { field: "test_ddui_all__account_multiple_array" },
+          // { field: "test_ddui_all__enumlist" },
+          { field: "test_ddui_all__docid" },
+          // { field: "test_ddui_all__account" },
+          { field: "test_ddui_all__date" },
+          { field: "test_ddui_all__color" },
+          // { field: "test_ddui_all__image" },
+          { field: "test_ddui_all__timestamp" },
+          { field: "test_ddui_all__money" }
+        ],
+        actions: [
+          { action: "display", title: "Display" },
+          { action: "modify", title: "Modify", iconClass: "fa fa-edit" },
+          { action: "delete", title: "Delete", iconClass: "fa fa-trash" }
+        ],
+        defaultExportButton: true,
+        defaultExpandable: true,
+        defaultShownColumns: true,
+        checkable: true,
+        reorderable: true,
+        resizable: true
+      }
     };
+  },
+  watch: {
+    gridProps(newValue, oldValue) {
+      if (newValue !== oldValue) {
+        try {
+          const config = JSON.parse(newValue);
+          Object.keys(config).forEach((key) => {
+            if (this.gridConfig[key] !== config[key]) {
+              this.gridConfig[key] = config[key];
+            }
+          });
+        } catch (err) {
+          console.warn("normal error:", err);
+        }
+      }
+    }
+  },
+  created() {
+    this.gridProps = JSON.stringify(this.gridConfig, null, 2);
   },
   methods: {
     onRowActionClick(evt) {}
