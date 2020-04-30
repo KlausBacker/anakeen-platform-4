0a349f641824447b5c78886fd010eda8d2c02cce
Éric Brison <eric.brison@anakeen.com>
2020-04-30 11:46:20 +0200

    test(ui): add test for htmltext (refs #318)

---
+++ user-interfaces/Tests/src/vendor/Anakeen/SmartStructures/UiTest/TstUiAllType/Renders/HtmltextToolbarFull.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Anakeen\SmartStructures\UiTest\TstUiAllType\Renders;
+
+use Anakeen\Ui\HtmltextRenderOptions;
+use Anakeen\Ui\RenderOptions;
+use SmartStructure\Fields\Tst_ddui_alltype as myAttributes;
+
+class HtmltextToolbarFull extends \Anakeen\Ui\DefaultEdit
+{
+
+
+    public function getOptions(\Anakeen\Core\Internal\SmartElement $document):RenderOptions
+    {
+        $options = parent::getOptions($document);
+
+
+        $options->frame()->setCollapse(true);
+        $options->frame(myAttributes::test_ddui_all__fr_text)->setCollapse(false);
+        $options->htmltext()->setToolbar(HtmltextRenderOptions::fullToolbar);
+
+
+        return $options;
+
+    }
+}
