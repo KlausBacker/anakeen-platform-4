a2551a55678a485a8ec87a555c74a6a63ef3fc30
David Dang <david.dang@anakeen.com>
2020-05-28 16:13:14 +0200

    fix(ui): display reload button when lock is impossible refs #643

--- user-interfaces/src/vendor/Anakeen/DOCUMENT/IHM/models/mDocument.js
+++ user-interfaces/src/vendor/Anakeen/DOCUMENT/IHM/models/mDocument.js
@@ -296,11 +296,18 @@ export default Backbone.Model.extend({
           docModel.get("properties").trigger("change");
         },
         error: function mDocumentLockDocumentError(theModel, HttpResponse) {
-          var response = JSON.parse(HttpResponse.responseText);
-
-          docModel.trigger("showError", {
-            title: response.exceptionMessage
-          });
+          if (HttpResponse && HttpResponse.status === 0) {
+            docModel.trigger("showError", {
+              errorCode: "offline",
+              title: i18n.___("Your navigator seems offline, try later", "ddui")
+            });
+          } else {
+            docModel.trigger("showError", {
+              errorCode: "unableToParseJson",
+              title: i18n.___("Unable to lock", "ddui")
+            });
+          }
+          docModel.trigger("displayNetworkError");
         }
       }
     );
