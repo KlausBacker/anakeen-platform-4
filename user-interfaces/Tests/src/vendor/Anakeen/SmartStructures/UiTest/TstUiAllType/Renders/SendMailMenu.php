3201a4bc7dda9f34acc998cf016a3a92f84f7d5c
Éric Brison <eric.brison@anakeen.com>
2020-06-17 15:28:58 +0200

    test(ui): add test for send menu (refs #389)

--- user-interfaces/Tests/src/vendor/Anakeen/SmartStructures/UiTest/TstUiAllType/Renders/SendMailMenu.php
+++ user-interfaces/Tests/src/vendor/Anakeen/SmartStructures/UiTest/TstUiAllType/Renders/SendMailMenu.php
@@ -5,66 +5,75 @@ namespace Anakeen\SmartStructures\UiTest\TstUiAllType\Renders;
 use Anakeen\Core\ContextManager;
 use Anakeen\Core\Internal\SmartElement;
 use Anakeen\Ui\BarMenu;
-use Anakeen\Ui\HtmltextRenderOptions;
 use Anakeen\Ui\ItemMenu;
 use Anakeen\Ui\MenuTargetOptions;
-use Anakeen\Ui\RenderOptions;
-use SmartStructure\Fields\Tst_ddui_alltype as myAttributes;
 
 class SendMailMenu extends \Anakeen\Ui\DefaultView
 {
-
-
-    public function getOptions(\Anakeen\Core\Internal\SmartElement $document): RenderOptions
+    public function getMenu(SmartElement $se): BarMenu
     {
-        $options = parent::getOptions($document);
-
-
-        $options->frame()->setCollapse(true);
-        $options->frame(myAttributes::test_ddui_all__fr_text)->setCollapse(false);
-        $options->htmltext()->setToolbar(HtmltextRenderOptions::basicToolbar);
-
-
-        return $options;
-    }
-
-
-    public function getMenu(SmartElement $document): BarMenu
-    {
-        $menu = parent::getMenu($document);
-        if (!$document->control("send")) {
+        $menu = parent::getMenu($se);
+        if (!$se->control("send")) {
+            // ---------------------------------------
             // Mail standard (par défaut)
             $mail = new ItemMenu("zou-mail");
-            $url = "/api/v2/smart-elements/MAIL/views/!defaultCreation.html";
-            $url .= sprintf("?customClientData=%s", urlencode(json_encode(["targetDocument" => $document->initid])));
+
+            $url = sprintf(
+                "/api/v2/ui/mail/form/%d.html",
+                $se->id
+            );
             $mail->setUrl($url);
 
             $option = new MenuTargetOptions();
             $option->windowHeight = 400;
             $option->windowWidth = 700;
             $mail->setTarget("_dialog", $option);
-            $mail->setTextLabel("Mail par défaut");
+            $mail->setTextLabel("Envoyer");
             $mail->setBeforeContent('<i class="fa fa-envelope" />');
-
             $menu->appendElement($mail);
 
-            // Mail de changement de mot de passe
-            $mail = new ItemMenu("zou-pass");
-            $url = "/api/v2/smart-elements/MAIL/views/!defaultCreation.html";
-            $url .= sprintf("?customClientData=%s", urlencode(json_encode([
-                "targetDocument" => ContextManager::getCurrentUser()->fid,
-                "mailTemplate" => [
-                    "name" => "AUTH_TPLMAILASKPWD",
-                    "keys" => [
-                        "LINK_CHANGE_PASSWORD" => "<h1>HOHO</h1>"
-                    ]
-                ]
-            ])));
+            // ---------------------------------------
+            // Mail custom (par défaut)
+            $mail = new ItemMenu("zou-perso");
+            $url = sprintf(
+                "/api/v2/ui/mail/form/%d.html?mailTemplate=%s&keys=%s",
+                $se->id,
+                "MY_MESSAGE",
+                urlencode(json_encode([
+                    "my_you" => ContextManager::getCurrentUser()->getAccountName(),
+                    "my_name" => "John",
+                    "my_now" => strftime("%d %B %Y")
+                ]))
+            );
             $mail->setUrl($url);
 
             $option = new MenuTargetOptions();
             $option->windowHeight = 500;
             $option->windowWidth = 700;
+
+            $mail->setTarget("_dialog", $option);
+            $mail->setTextLabel("Message personnel");
+            $mail->setBeforeContent('<i class="fa fa-envelope" />');
+
+            $menu->appendElement($mail);
+
+            // ---------------------------------------
+            // Mail de changement de mot de passe
+            $mail = new ItemMenu("zou-pass");
+            $url = sprintf(
+                "/api/v2/ui/mail/form/%d.html?mailTemplate=%s&keys=%s",
+                ContextManager::getCurrentUser()->fid,
+                "AUTH_TPLMAILASKPWD",
+                urlencode(json_encode([
+                    "LINK_CHANGE_PASSWORD" => "<h1>HOHO</h1>"
+                ]))
+            );
+            $mail->setUrl($url);
+
+            $option = new MenuTargetOptions();
+            $option->windowHeight = 500;
+            $option->windowWidth = 700;
+
             $mail->setTarget("_dialog", $option);
             $mail->setTextLabel("Mot de passe oublié");
             $mail->setBeforeContent('<i class="fa fa-envelope" />');
