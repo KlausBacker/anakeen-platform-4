4117cb9418f6aa8e0ca8e0ecaab9aad7dc39dab4
Lucas Surowy <lucas.surowy@anakeen.com>
2020-06-25 12:03:58 +0200

    fix(cli): fresh stubs generation (refs #753)

--- admin-center/stubs/ADMINPARAMETERSHUBCONFIGURATION__STUB.php
+++ admin-center/stubs/ADMINPARAMETERSHUBCONFIGURATION__STUB.php
@@ -27,25 +27,35 @@ namespace SmartStructure\Fields {
         */ 
         const admin_hub_configuration_options='admin_hub_configuration_options';
         /**
-        * Paramètres généraux
+        * Global parameters
         * <ul>
-        * <li> <i>relation</i> Hub_YesNo </li>
+        * <li> <i>relation</i> Admin_Param_TrueFalse </li>
         * <li> <i>access</i> ReadWrite </li>
         * <li> <i>type</i> enum </li>
         * </ul>
         */ 
         const admin_hub_configuration_global='admin_hub_configuration_global';
         /**
-        * Paramètres utilisateurs
+        * User parameters
         * <ul>
-        * <li> <i>relation</i> Hub_YesNo </li>
+        * <li> <i>relation</i> Admin_Param_TrueFalse </li>
         * <li> <i>access</i> ReadWrite </li>
         * <li> <i>type</i> enum </li>
         * </ul>
         */ 
         const admin_hub_configuration_user='admin_hub_configuration_user';
         /**
-        * Utilisateur spécifique
+        * Namespace
+        * <ul>
+        * <li> <i>access</i> ReadWrite </li>
+        * <li> <i>multiple</i> true </li>
+        * <li> <i>relation</i> Admin_Param_NameSpace </li>
+        * <li> <i>type</i> enum </li>
+        * </ul>
+        */ 
+        const admin_hub_configuration_namespace='admin_hub_configuration_namespace';
+        /**
+        * Specific user
         * <ul>
         * <li> <i>access</i> ReadWrite </li>
         * <li> <i>type</i> account </li>
@@ -53,13 +63,21 @@ namespace SmartStructure\Fields {
         */ 
         const admin_hub_configuration_account='admin_hub_configuration_account';
         /**
-        * Namespace
+        * Material-icon
         * <ul>
         * <li> <i>access</i> ReadWrite </li>
         * <li> <i>type</i> text </li>
         * </ul>
         */ 
-        const admin_hub_configuration_namespace='admin_hub_configuration_namespace';
+        const admin_hub_configuration_icon='admin_hub_configuration_icon';
+        /**
+        * Label
+        * <ul>
+        * <li> <i>access</i> ReadWrite </li>
+        * <li> <i>type</i> text </li>
+        * </ul>
+        */ 
+        const admin_hub_configuration_label='admin_hub_configuration_label';
 
     }
 }
