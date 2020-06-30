bb308eb867e5b27bce30e2211dd8602bdf4efbe6
Leopold Desert-Legendre <leopold.desertlegendre@anakeen.com>
2020-06-30 14:31:37 +0200

    fix(fulltext): vue component template refactor (refs #640)

--- fulltext-search/components/src/AnkFulltextSmartCriteria/AnkFulltextSmartCriteria.component.ts
+++ fulltext-search/components/src/AnkFulltextSmartCriteria/AnkFulltextSmartCriteria.component.ts
@@ -1,13 +1,16 @@
 import { Component } from "vue-property-decorator";
 import AnkSmartCriteria from "@anakeen/user-interfaces/components/src/AnkSmartCriteria/AnkSmartCriteria.component";
+import AnkSmartCriteriaVueComponent from "@anakeen/user-interfaces/components/lib/AnkSmartCriteria.esm";
 import ISmartCriteriaConfiguration from "@anakeen/user-interfaces/components/src/AnkSmartCriteria/Types/ISmartCriteriaConfiguration";
 import SmartCriteriaFulltextConfigurationLoader from "./SmartCriteriaFulltextConfigurationLoader";
 import SmartFormFulltextConfigurationBuilder from "./SmartFormFulltextConfigurationBuilder";
 import ISmartFilter from "@anakeen/user-interfaces/components/src/AnkSmartCriteria/Types/ISmartFilter";
 import IConfigurationCriteria from "@anakeen/user-interfaces/components/src/AnkSmartCriteria/Types/IConfigurationCriteria";
+import * as $ from "jquery";
 
 @Component({
-  name: "ank-fulltext-smart-criteria"
+  name: "ank-fulltext-smart-criteria",
+  extends: AnkSmartCriteriaVueComponent
 })
 export default class AnkFulltextSmartCriteria extends AnkSmartCriteria {
   protected getConfigurationLoader(config: ISmartCriteriaConfiguration) {
