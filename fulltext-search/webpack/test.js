13063eb4c0f131544c1a1d33123114f6f78e7287
Leopold Desert-Legendre <leopold.desertlegendre@anakeen.com>
2020-06-30 14:32:25 +0200

    fix(fulltext): webpack conf cleanup (refs #640)

--- fulltext-search/webpack/test.js
+++ fulltext-search/webpack/test.js
@@ -1,34 +1,8 @@
-const path = require("path");
-const { prod, dev, lib } = require("@anakeen/webpack-conf");
-const {
-  vueLoader,
-  typeScriptLoader,
-  cssLoader,
-  addKendoGlobal,
-  addJqueryGlobal,
-  addVueGlobal
-} = require("@anakeen/webpack-conf/parts");
+const { lib } = require("@anakeen/webpack-conf");
 const testFulltextSmartCriteria = require("./testFulltextSmartCriteria");
 const testFulltextSmartElementGrid = require("./testFulltextSmartElementGrid");
 
-const BASE_DIR = path.resolve(__dirname, "../");
-const PUBLIC_PATH = path.join(BASE_DIR, "Tests/src/public");
-
 module.exports = () => {
-  const conf = {
-    context: BASE_DIR,
-    moduleName: "fulltextTest",
-    entry: {},
-    buildPath: PUBLIC_PATH,
-    customParts: [
-      vueLoader(),
-      typeScriptLoader(),
-      addKendoGlobal([/kendo.pdf/, /kendo.excel/], true),
-      addJqueryGlobal(),
-      addVueGlobal(),
-      cssLoader()
-    ]
-  };
   if (process.env.conf === "PROD") {
     return [lib(testFulltextSmartCriteria), lib(testFulltextSmartElementGrid)];
   }
