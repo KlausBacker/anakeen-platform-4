57a306cb6ab57801fb2d030c1d26c70d6b42d471
joaquim <joaquim.nallar@anakeen.com>
2020-07-09 15:04:04 +0200

    feat(ui): click on view more button from the error notif ok (refs #791)

--- user-interfaces/src/vendor/Anakeen/DOCUMENT/IHM/widgets/globalController/utils/templates/SEError.mustache.js
+++ user-interfaces/src/vendor/Anakeen/DOCUMENT/IHM/widgets/globalController/utils/templates/SEError.mustache.js
@@ -2,12 +2,6 @@ export default `<div class="dcpStaticErrorMessage" hidden>
     <style>
         .dcpStaticErrorMessage {
             text-align: center;
-            z-index: 10000;
-            position: absolute;
-            top: 0;
-            bottom: 0;
-            left: 0;
-            right: 0;
         }
     </style>
     <div class="alert alert-danger" role="alert">
