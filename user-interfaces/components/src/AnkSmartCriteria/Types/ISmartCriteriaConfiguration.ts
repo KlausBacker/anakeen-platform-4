90729cd61673f5860ff517ca07bf783286b26053
Leopold Desert-Legendre <leopold.desertlegendre@anakeen.com>
2020-07-15 08:59:57 +0200

    feat(ui): added standalone option to criteria (refs #805)

--- user-interfaces/components/src/AnkSmartCriteria/Types/ISmartCriteriaConfiguration.ts
+++ user-interfaces/components/src/AnkSmartCriteria/Types/ISmartCriteriaConfiguration.ts
@@ -1,6 +1,7 @@
 import IConfigurationCriteria from "./IConfigurationCriteria";
 
 export default interface ISmartCriteriaConfiguration {
+  standalone?: boolean;
   title?: string;
   defaultStructure?: string | number;
   criterias: Array<IConfigurationCriteria>;
