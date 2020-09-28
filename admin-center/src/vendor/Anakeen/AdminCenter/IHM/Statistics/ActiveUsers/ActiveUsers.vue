89e7abc8328b33a20ac4e453fdb9ac3ab09bffd1
David Dang <david.dang@anakeen.com>
2020-09-29 01:00:46 +0200

    feat(admin): statistics css refs #250

--- admin-center/src/vendor/Anakeen/AdminCenter/IHM/Statistics/ActiveUsers/ActiveUsers.vue
+++ admin-center/src/vendor/Anakeen/AdminCenter/IHM/Statistics/ActiveUsers/ActiveUsers.vue
@@ -2,12 +2,16 @@
   <div class="statistics">
     <h2 class="statistics-title">{{ $t("AdminCenterStatistics.Active users over the period") }}</h2>
     <div class="statistics-period">
-      <span>{{ $t("AdminCenterStatistics.From") }}</span>
-      <input ref="monthPickerFrom" title="month from" />
-      <span>{{ $t("AdminCenterStatistics.To") }}</span>
-      <input ref="monthPickerTo" title="month to" />
+      <div class="statistics-date">
+        <span>{{ $t("AdminCenterStatistics.From") }}:</span>
+        <input ref="monthPickerFrom" title="month from" />
+      </div>
+      <div class="statistics-date">
+        <span>{{ $t("AdminCenterStatistics.To") }}:</span>
+        <input ref="monthPickerTo" title="month to" />
+      </div>
       <span class="statistics-total">{{ $t("AdminCenterStatistics.Total") }} : {{ activeUsersTotal }}</span>
-      <kendo-button class="k-primary" @click="exportActiveUsers">{{ $t("AdminCenterStatistics.Export") }}</kendo-button>
+      <kendo-button class="k-primary statistics-export" @click="exportActiveUsers">{{ $t("AdminCenterStatistics.Export") }}</kendo-button>
     </div>
   </div>
 </template>
