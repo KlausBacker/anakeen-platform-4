5b490c3aadfb5261cea7218ced8f0fe9365341cb
David Dang <david.dang@anakeen.com>
2020-10-08 18:48:16 +0200

    fix(dev-center): correct number of json translations refs #969

--- development-center/src/vendor/Anakeen/Routes/Devel/I18n.php
+++ development-center/src/vendor/Anakeen/Routes/Devel/I18n.php
@@ -25,6 +25,8 @@ require_once "vendor/Anakeen/Routes/Devel/Lib/vendor/autoload.php";
  */
 class I18n
 {
+    const langs = ["fr", "en"];
+    const COMPONENT_SECTION="Component";
 
     public function __invoke(\Slim\Http\request $request, \Slim\Http\response $response, $args)
     {
@@ -58,26 +60,25 @@ class I18n
     protected function addJsonTranslations(&$data)
     {
         $rootPathLength = strlen(ContextManager::getRootDirectory());
-        $langs = ["fr", "en"];
-        foreach ($langs as $lang) {
-            $jsonPattern = sprintf("%s/locale/%s/**/*.json", ContextManager::getRootDirectory(), $lang);
-            $jsonFiles = Glob::glob($jsonPattern);
-            foreach ($jsonFiles as $jsonFile) {
-                $jsonCtxt = basename($jsonFile, "." . pathinfo($jsonFile)["extension"]);
-                $jsonContent = file_get_contents($jsonFile);
-                $items = json_decode($jsonContent, true);
-                foreach ($items as $key => $val) {
-                    $key = sprintf("%s-%s", $jsonCtxt, $key);
-                    if (!isset($data[$key])) {
-                        $data[$key] = [
-                            "msgctxt" => $jsonCtxt,
-                            "msgid" => $key,
-                        ];
+        foreach (self::langs as $lang) {
+            $vueGlob = sprintf("%s/locale/%s/vuejs/src/*.json", ContextManager::getRootDirectory(), $lang);
+            foreach (glob($vueGlob) as $filename) {
+                $vueData = json_decode(file_get_contents($filename), true);
+
+                $ctx = strtok(basename($filename), ".");
+                foreach ($vueData as $entryID => $entryMsg) {
+                    $key = self::COMPONENT_SECTION . $entryID;
+                    $entry = [
+                        "msgctxt" => $ctx,
+                        "msgid" => $entryID,
+                    ];
+                    if (empty($this->filters) || $this->filterContainsEntry($entry, $this->filters)) {
+                        $data[$key] = $entry;
                     }
                     if (isset($data[$key])) {
-                        $data[$key]["files"][] = "." . substr($jsonFile, $rootPathLength);
+                        $data[$key]["files"][] = "." . substr($filename, $rootPathLength);
                     }
-                    $data[$key]["$lang"] = $val;
+                    $data[$key]["$lang"] = $entryMsg;
                 }
             }
         }
@@ -85,10 +86,9 @@ class I18n
 
     protected function addSmartStructureLocale(&$data)
     {
-        $langs = ["fr", "en"];
         $s = new SearchElements(-1);
         $structures = $s->search()->getResults();
-        foreach ($langs as $lang) {
+        foreach (self::langs as $lang) {
             foreach ($structures as $structure) {
                 $fields = $structure->getAttributes();
                 foreach ($fields as $field) {
@@ -118,11 +118,10 @@ class I18n
 
     protected function addWorkflowLocale(&$data)
     {
-        $langs = ["fr", "en"];
         $s = new SearchElements(-1);
         $s->addFilter("usefor ~ 'W'");
         $structures = $s->search()->getResults();
-        foreach ($langs as $lang) {
+        foreach (self::langs as $lang) {
             foreach ($structures as $structure) {
                 $workflow = SEManager::createDocument($structure->name);
                 /** @var WDocHooks $workflow */
@@ -172,9 +171,8 @@ class I18n
 
     protected function addEnumLocale(&$data)
     {
-        $langs = ["fr", "en"];
         DbManager::query(sprintf("select * from docenum where key != '%s'", EnumManager::EXTENDABLEKEY), $enums);
-        foreach ($langs as $lang) {
+        foreach (self::langs as $lang) {
             foreach ($enums as $enum) {
                 $entry = [
                     "msgid" => $enum["key"],
@@ -239,48 +237,81 @@ class I18n
             }
         }
     }
-
     protected function getRecordedTranslations()
     {
         $data = [];
-
-        $langs = ["fr", "en"];
-
-        $tmpPo = sprintf("%s/%s.po", ContextManager::getTmpDir(), uniqid("i18n"));
-        foreach ($langs as $lang) {
-            $cmd = sprintf("msgunfmt %s/locale/%s/LC_MESSAGES/main-catalog.mo > %s", escapeshellarg(ContextManager::getRootDirectory()), $lang, escapeshellarg($tmpPo));
-
-            exec($cmd, $output, $status);
-            if ($status !== 0) {
-                throw new Exception("Fail retrieve locale");
+        foreach (self::langs as $lang) {
+            $originPoFile = sprintf(
+                "%s/locale/%s/LC_MESSAGES/main-catalog.po",
+                ContextManager::getRootDirectory(),
+                $lang
+            );
+            if (!file_exists($originPoFile)) {
+                throw new Exception("Fail retrieve origin locale results");
             }
-            if (!file_exists($tmpPo)) {
-                throw new Exception("Fail retrieve locale results");
-            }
-            $fileHandler = new \Sepia\PoParser\SourceHandler\FileSystem($tmpPo);
-            $poParser = new \Sepia\PoParser\Parser($fileHandler);
-            $catalog = $poParser->parse();
 
-            $entries = $catalog->getEntries();
+            $originFileHandler = new \Sepia\PoParser\SourceHandler\FileSystem($originPoFile);
+            $originParser = new \Sepia\PoParser\Parser($originFileHandler);
+            $originalCatalog = $originParser->parse();
+
+
+            $entries = $originalCatalog->getEntries();
+            $i = 0;
             foreach ($entries as $entry) {
-                $key = sprintf("%s-%s", $entry->getMsgCtxt(), $entry->getMsgId());
-
-                if (!isset($data[$key])) {
-                    $data[$key] = [
-                        "msgctxt" => $entry->getMsgCtxt(),
-                        "msgid" => $entry->getMsgId()
-                    ];
+                if ($entry->isFuzzy()) {
+                    //continue;
                 }
-
-                if (($entry->getMsgIdPlural())) {
-                    $data[$key]["$lang"] = implode("\n", $entry->getMsgStrPlurals());
-                } else {
-                    $data[$key]["$lang"] = $entry->getMsgStr();
+                if ($entry->isObsolete() === false) {
+                    $key = sprintf("%s-%s", $entry->getMsgCtxt(), $entry->getMsgId());
+                    if (empty($this->filters) || $this->filterContainsTranslations($entry, $this->filters)) {
+                        if (!isset($data[$key])) {
+                            $data[$key] = [
+                                "msgctxt" => $entry->getMsgCtxt(),
+                                "msgid" => $entry->getMsgId(),
+                            ];
+                        }
+                        $data[$key]["$lang"] = $entry->getMsgStr();
+                        if (($entry->getMsgIdPlural())) {
+                            $data[$key]["$lang"] = implode("\n", $entry->getMsgStrPlurals());
+                        }
+                    }
                 }
             }
         }
-
-
         return $data;
+    }
+    private function filterContainsTranslations(Entry $entry, $filters)
+    {
+        $filterPassed = true;
+        if (!empty($filters)) {
+            foreach ($filters as $filter) {
+                $filterField = $filter["field"];
+                $filterValue = $filter["value"];
+                $entryValue = null;
+                switch ($filterField) {
+                    case "msgctxt":
+                        $entryValue = $entry->getMsgCtxt();
+                        break;
+                    case "msgid":
+                        $entryValue = $entry->getMsgId();
+                        break;
+                    case "msgstr":
+                        $entryValue = $entry->getMsgStr();
+                        break;
+                    case "section":
+                        $entryValue = "";
+                        break;
+                    default:
+                        break;
+                }
+                if (strpos(
+                    Strings::unaccent(strtolower($entryValue)),
+                    Strings::unaccent(strtolower($filterValue))
+                ) === false) {
+                    return false;
+                }
+            }
+        }
+        return $filterPassed;
     }
 }
