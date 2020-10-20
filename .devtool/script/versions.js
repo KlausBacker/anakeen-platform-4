e38aff9c704ee53bd6fba51f34823fe01baad70b
Charles Bonnissent <charles.bonnissent@anakeen.com>
2020-10-20 17:29:40 +0200

    build: correct deps of fulltext

--- .devtool/script/versions.js
+++ .devtool/script/versions.js
@@ -1,115 +1,115 @@
 exports.versions = [
+  // {
+  //   package: "@anakeen/anakeen-cli",
+  //   version: "1.2.3",
+  //   npmOnly: true
+  // },
+  // {
+  //   package: "@anakeen/admin-center",
+  //   version: "2.2.1",
+  //   modulePath: "admin-center",
+  //   moduleName: "admin-center"
+  // },
+  // {
+  //   package: "@anakeen/business-app",
+  //   version: "2.0.1",
+  //   modulePath: "business-app",
+  //   moduleName: "anakeen-hub-business-app"
+  // },
+  // {
+  //   package: "anakeen-control",
+  //   version: "3.0.2",
+  //   npmOnly: true
+  // },
+  // {
+  //   package: "transformation-server",
+  //   version: "1.4.3",
+  //   npmOnly: true
+  // },
+  // {
+  //   package: "development-center",
+  //   version: "2.2.4",
+  //   modulePath: "development-center",
+  //   moduleName: "development-center"
+  // },
+  // {
+  //   package: "@anakeen/hub-components",
+  //   version: "2.0.1",
+  //   modulePath: "hub-station",
+  //   moduleName: "anakeen-hub"
+  // },
+  // {
+  //   package: "@anakeen/internal-components",
+  //   version: "1.0.4",
+  //   npmOnly: true
+  // },
+  // {
+  //   package: "migration-tools",
+  //   version: "2.1.1",
+  //   modulePath: "migration-tools",
+  //   moduleName: "migration-tools"
+  // },
+  // {
+  //   package: "@anakeen/test-tools",
+  //   version: "1.0.0",
+  //   modulePath: "test-tools",
+  //   moduleName: "test-tools"
+  // },
+  // {
+  //   package: "@anakeen/security",
+  //   version: "2.0.5",
+  //   modulePath: "security",
+  //   moduleName: "security"
+  // },
+  // {
+  //   package: "@anakeen/theme-variables",
+  //   version: "1.0.1",
+  //   npmOnly: true
+  // },
+  // {
+  //   package: "transformation",
+  //   version: "2.0.5",
+  //   modulePath: "transformation",
+  //   moduleName: "transformation"
+  // },
+  // {
+  //   package: "@anakeen/user-interfaces",
+  //   version: "3.1.1",
+  //   modulePath: "user-interfaces",
+  //   moduleName: "user-interfaces"
+  // },
+  // {
+  //   package: "@anakeen/workflow",
+  //   version: "2.0.4",
+  //   modulePath: "workflow",
+  //   moduleName: "workflow"
+  // },
+  // {
+  //   package: "@anakeen/smart-data-engine",
+  //   version: "4.0.0",
+  //   modulePath: "smart-data-engine",
+  //   moduleName: "smart-data-engine"
+  // },
+  // {
+  //   package: "dev-data",
+  //   version: "2.0.3",
+  //   modulePath: "dev-data",
+  //   moduleName: "dev-data"
+  // },
   {
-    package: "@anakeen/anakeen-cli",
-    version: "1.2.3",
-    npmOnly: true
-  },
-  {
-    package: "@anakeen/admin-center",
-    version: "2.2.1",
-    modulePath: "admin-center",
-    moduleName: "admin-center"
-  },
-  {
-    package: "@anakeen/business-app",
-    version: "2.0.1",
-    modulePath: "business-app",
-    moduleName: "anakeen-hub-business-app"
-  },
-  {
-    package: "anakeen-control",
-    version: "3.0.2",
-    npmOnly: true
-  },
-  {
-    package: "transformation-server",
-    version: "1.4.3",
-    npmOnly: true
-  },
-  {
-    package: "development-center",
-    version: "2.2.4",
-    modulePath: "development-center",
-    moduleName: "development-center"
-  },
-  {
-    package: "@anakeen/hub-components",
-    version: "2.0.1",
-    modulePath: "hub-station",
-    moduleName: "anakeen-hub"
-  },
-  {
-    package: "@anakeen/internal-components",
+    package: "@anakeen/fulltext-search",
     version: "1.0.4",
-    npmOnly: true
+    modulePath: "fulltext-search",
+    moduleName: "fulltext-search"
   },
-  {
-    package: "migration-tools",
-    version: "2.1.1",
-    modulePath: "migration-tools",
-    moduleName: "migration-tools"
-  },
-  {
-    package: "@anakeen/test-tools",
-    version: "1.0.0",
-    modulePath: "test-tools",
-    moduleName: "test-tools"
-  },
-  {
-    package: "@anakeen/security",
-    version: "2.0.5",
-    modulePath: "security",
-    moduleName: "security"
-  },
-  {
-    package: "@anakeen/theme-variables",
-    version: "1.0.1",
-    npmOnly: true
-  },
-  {
-    package: "transformation",
-    version: "2.0.5",
-    modulePath: "transformation",
-    moduleName: "transformation"
-  },
-  {
-    package: "@anakeen/user-interfaces",
-    version: "3.1.1",
-    modulePath: "user-interfaces",
-    moduleName: "user-interfaces"
-  },
-  {
-    package: "@anakeen/workflow",
-    version: "2.0.4",
-    modulePath: "workflow",
-    moduleName: "workflow"
-  },
-  {
-    package: "@anakeen/smart-data-engine",
-    version: "4.0.0",
-    modulePath: "smart-data-engine",
-    moduleName: "smart-data-engine"
-  },
-  {
-    package: "dev-data",
-    version: "2.0.3",
-    modulePath: "dev-data",
-    moduleName: "dev-data"
-  },
-   {
-     package: "@anakeen/fulltext-search",
-     version: "1.0.3",
-     modulePath: "fulltext-search",
-     moduleName: "fulltext-search"
-   },
-  {
-    package: "@anakeen/anakeen-module-validation",
-    version: "2.2.0",
-    npmOnly: true
-  }
+  // {
+  //   package: "@anakeen/anakeen-module-validation",
+  //   version: "2.2.0",
+  //   npmOnly: true
+  // }
 ];
 exports.RC = "";
-exports.release = "2020.2";
+exports.release = "2020.2.2";
 exports.modulePath = [
   "admin-center",
   "business-app",
@@ -125,5 +125,6 @@ exports.modulePath = [
   "dev-data",
   "smart-data-engine/Tests",
   "workflow/Tests",
-  "user-interfaces/Tests"
+  "user-interfaces/Tests",
+  "fulltext-search"
 ];
