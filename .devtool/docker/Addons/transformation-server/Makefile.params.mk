1ee4da0c4b0f2999a5a228b17dbcfce54b431eb2
Éric Brison <eric.brison@anakeen.com>
2020-12-15 16:27:47 +0100

    build: add possibility to override PROJECT_TE_PORT

--- .devtool/docker/Addons/transformation-server/Makefile.params.mk
+++ .devtool/docker/Addons/transformation-server/Makefile.params.mk
@@ -1,9 +1,11 @@
 PROJECT_TRANSFORMATION_SERVER_PHP_VERSION ?= 7.3-2
+PROJECT_TE_PORT ?= 51968
 
 DOCKER_COMPOSE_SERVICES += transformation-server
 DOCKER_COMPOSE_SERVICES_WAIT_LIST += transformation-server:51968
 DOCKER_COMPOSE_OVERRIDES += -f $(DOCKER_DIR)/Addons/transformation-server/docker-compose.yml
 DOCKER_COMPOSE_ENV += PROJECT_TRANSFORMATION_SERVER_PHP_VERSION=$(PROJECT_TRANSFORMATION_SERVER_PHP_VERSION)
+DOCKER_COMPOSE_ENV += PROJECT_TE_PORT=$(PROJECT_TE_PORT)
 
 docker-prompt-transformation-server: ## open bash prompt in PHP-FPM container
 	$(DOCKER_COMPOSE_CMD) exec transformation-server /bin/bash
